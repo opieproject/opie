354f91046c01155d3fdad5de24fd10f962265cd8
cvs2git <opie-devel@handhelds.org>
2002-01-31 18:35:54 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.3.6'.

--- i18n/de/mountmonapplet.ts
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE TS><TS>
-<context>
-    <name>MountMon</name>
-    <message>
-        <source>Eject SD-card</source>
-        <translation>SD-Karte auswerfen</translation>
-    </message>
-</context>
-</TS>
