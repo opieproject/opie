067d938f39c4dcaf9e6d290e340011d3eac8a3ef
Simon Hausmann <simon@handhelds.org>
2002-03-14 16:50:51 +0000

    - moved encodeAttr out of XMLElement into an anonymous namespace - private copy ctor and assignment operator - changed license to LGPL

--- libopie2/opiecore/xmltree.cc
+++ libopie2/opiecore/xmltree.cc
@@ -1,24 +1,20 @@
 /*  This file is part of the KDE project
     Copyright (C) 2001 Simon Hausmann <hausmann@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-    As a special exception this program may be linked with Qt non-commercial 
-    edition, the resulting executable be distributed, without including the 
-    source code for the Qt non-commercial edition in the source distribution.
-
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
 */
 
 
@@ -27,6 +23,28 @@
 #include <qxml.h>
 
 #include <assert.h>
+
+namespace
+{
+
+/**
+  Encode an attribute value upon saving.
+  replaces '"' with "&quot"
+  replaces '<' with "&lt"
+  replaces '&' with "&amp"
+  replaces '>' with "&gt"
+*/
+QString encodeAttr( const QString& str ) 
+{
+    QString encAttr( str ); // cause of the const parameter
+    encAttr = encAttr.replace( QRegExp( "[<]"  ), "&lt"   );
+    encAttr = encAttr.replace( QRegExp( "[>]"  ), "&gt"   );
+    encAttr = encAttr.replace( QRegExp( "[\"]" ), "&quot" );
+    encAttr = encAttr.replace( QRegExp( "[&]"  ), "&amp"  );   
+    return encAttr;
+}
+
+}
 
 XMLElement::XMLElement()
     : m_parent( 0 ), m_next( 0 ), m_prev( 0 ), m_first( 0 ), m_last( 0 )
@@ -193,22 +211,6 @@ void XMLElement::save( QTextStream &s, uint indent )
     }
     else
         s << "/>" << endl;
-}
-/**
-  Encode an attribute value upon saving.
-  replaces '"' with "&quot"
-  replaces '<' with "&lt"
-  replaces '&' with "&amp"
-  replaces '>' with "&gt"
-*/
-QString XMLElement::encodeAttr( const QString& str )
-{
-    QString encAttr( str ); // cause of the const parameter
-    encAttr = encAttr.replace( QRegExp( "[<]"  ), "&lt"   );
-    encAttr = encAttr.replace( QRegExp( "[>]"  ), "&gt"   );
-    encAttr = encAttr.replace( QRegExp( "[\"]" ), "&quot" );
-    encAttr = encAttr.replace( QRegExp( "[&]"  ), "&amp"  );   
-    return encAttr;
 }
 
 class Handler : public QXmlDefaultHandler
