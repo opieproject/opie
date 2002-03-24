1cd2c1473e59975204b1b0d8677b31cdd58135dc
Lorn Potter <lpotter@trolltech.com>
2002-03-24 15:39:37 +0000

    cleared developers define

--- noncore/net/opieftp/opieftp.cpp
+++ noncore/net/opieftp/opieftp.cpp
@@ -9,7 +9,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  ***************************************************************************/
-#define DEVELOPERS_VERSION
+//#define DEVELOPERS_VERSION
 
 #include "opieftp.h"
 #include "ftplib.h"
