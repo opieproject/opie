f7983c52e968a6d21c69b307da850dc20941ba91
Carsten Niehaus <cniehaus@handhelds.org>
2002-04-06 22:15:43 +0000

    this regards the konsole-changes

--- i18n/de/embeddedkonsole.ts
+++ i18n/de/embeddedkonsole.ts
@@ -82,6 +82,106 @@
         <source>Paste</source>
         <translation>Einfügen</translation>
     </message>
+    <message>
+        <source>Show command list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide command list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tabs on Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Green on Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Black on White</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>White on Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Black on Transparent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Black on Red</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Red on Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Green on Yellow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Blue on Magenta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Magenta on Blue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cyan on White</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>White on Cyan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Blue on Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Amber on Black</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quick Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ScrollBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show Command List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide Command List</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>PlayListSelection</name>
