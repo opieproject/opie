83afae90a7ee2fc9429132d7452ef725b842ba3a
David Woodhouse <dwmw2@handhelds.org>
2002-04-21 10:45:43 +0000

    Allow building as real Qt app too.

--- noncore/unsupported/gsmtool/main.cpp
+++ noncore/unsupported/gsmtool/main.cpp
@@ -1,12 +1,23 @@
 #include "gsmtool.h"
+#ifdef QWS
 #include <qpe/qpeapplication.h>
+#else
+#include <qapplication.h>
+#endif
 
 int main( int argc, char ** argv )
 {
+#ifdef QWS
     QPEApplication a( argc, argv );
 
     GSMTool mw;
     a.showMainWidget( &mw );
+#else
+    QApplication a( argc, argv );
 
+    GSMTool mw;
+    a.setMainWidget( &mw );
+    mw.show();
+#endif
     return a.exec();
 }
