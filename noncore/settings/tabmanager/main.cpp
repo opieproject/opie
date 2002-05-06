4532883c7f72083fc1187042b9fb161eb63b3927
Lorn Potter <lpotter@trolltech.com>
2002-05-06 20:54:54 +0000

    removed license info as per request from benmeyer..... it's still GPL, tho

--- noncore/settings/tabmanager/main.cpp
+++ noncore/settings/tabmanager/main.cpp
@@ -1,31 +1,3 @@
-/*
-
-               =.            This file is part of the OPIE Project
-             .=l.            Copyright (c)  2002 Benjamin Meyer <meyerb@sharpsec.com>
-           .>+-=
- _;:,     .>    :=|.         This library is free software; you can
-.> <`_,   >  .   <=          redistribute it and/or  modify it under
-:`=1 )Y*s>-.--   :           the terms of the GNU Library General Public
-.="- .-=="i,     .._         License as published by the Free Software
- - .   .-<_>     .<>         Foundation; either version 2 of the License,
-     ._= =}       :          or (at your option) any later version.
-    .%`+i>       _;_.
-    .i_,=:_.      -<s.       This library is distributed in the hope that
-     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
-    : ..    .:,     . . .    without even the implied warranty of
-    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
-  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
-..}^=.=       =       ;      Library General Public License for more
-++=   -.     .`     .:       details.
- :     =  ...= . :.=-
- -.   .:....=;==+<;          You should have received a copy of the GNU
-  -_. . .   )=.  =           Library General Public License along with
-    --        :-=`           this library; see the file COPYING.LIB.
-                             If not, write to the Free Software Foundation,
-                             Inc., 59 Temple Place - Suite 330,
-                             Boston, MA 02111-1307, USA.
-
-*/
 #include "tabmainwindow.h"
 #include <qpe/qpeapplication.h>
 
