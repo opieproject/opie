f21ea8822ca852823f54f466b078c1f8dc501b77
Thomas Stephens <spiralman@gmail.com>
2002-05-27 01:01:10 +0000

    script run by opie-sh dektop file

---
+++ bin/opie-sh-howto.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# opie-sh-howto.sh - a demonstration of opie-sh
+#
+# Copyright (C) 2002 gonz@directbox.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+cd  $QPEDIR/help/opie-sh/
+opie-sh -t "opie-sh-howto" -f index.html
+rm -f /tmp/qcop-opie-sh-howto.sh
