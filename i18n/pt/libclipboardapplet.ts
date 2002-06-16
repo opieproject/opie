d14f5e72e9896c0c7c073da7f68c08845010921d
Carsten Niehaus <cniehaus@handhelds.org>
2002-06-16 22:10:55 +0000

    cvs add is a nice thing ;)

---
+++ i18n/pt/libclipboardapplet.ts
@@ -0,0 +1,17 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>ClipboardApplet</name>
+    <message>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Colar</translation>
+    </message>
+</context>
+</TS>
