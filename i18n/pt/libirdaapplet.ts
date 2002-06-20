15eb4ce309e8120a860fa7b1df56a27691b93956
Bruno David Rodrigues <davidpt@handhelds.org>
2002-06-20 18:22:36 +0000

    Added translations to irdaapplet

---
+++ i18n/pt/libirdaapplet.ts
@@ -0,0 +1,37 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>IrdaApplet</name>
+    <message>
+        <source>Discovered Device:</source>
+        <translation type="unfinished">Devices Descobertos:</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Nenhum</translation>
+    </message>
+    <message>
+        <source>Disable IrDA</source>
+        <translation>Ligar IrDA</translation>
+    </message>
+    <message>
+        <source>Enable IrDA</source>
+        <translation>Desligar IrDA</translation>
+    </message>
+    <message>
+        <source>Disable Discovery</source>
+        <translation type="unfinished">Desligar Discovery</translation>
+    </message>
+    <message>
+        <source>Enable Discovery</source>
+        <translation type="unfinished">Ligar Discovery</translation>
+    </message>
+    <message>
+        <source>Disable Receive</source>
+        <translation type="unfinished">Desligar Receber</translation>
+    </message>
+    <message>
+        <source>Enable Receive</source>
+        <translation type="unfinished">Ligar Receber</translation>
+    </message>
+</context>
+</TS>
