21c0db533076e3a230f38b91de79887eb912562e
Holger Freyther <zecke@handhelds.org>
2002-06-26 19:23:12 +0000

    Adjust mimetype handling LJP and Max look at it.

--- core/multimedia/opieplayer/inputDialog.cpp
+++ core/multimedia/opieplayer/inputDialog.cpp
@@ -23,7 +23,7 @@ InputDialog::InputDialog( QWidget* parent,  const char* name, bool modal, WFlags
 {
     if ( !name )
   setName( "InputDialog" );
-    resize( 234, 115); 
+    resize( 234, 115);
     setMaximumSize( QSize( 240, 40));
     setCaption( tr(name ) );
 
@@ -35,18 +35,33 @@ InputDialog::InputDialog( QWidget* parent,  const char* name, bool modal, WFlags
     LineEdit1->setGeometry( QRect( 4, 10, 190, 22 ) );
 }
 
-/*  
+/*
  *  Destroys the object and frees any allocated resources
  */
 InputDialog::~InputDialog()
 {
     inputText= LineEdit1->text();
-  
+
 }
 
 void InputDialog::browse() {
 
-    QString str = OFileDialog::getOpenFileName( 1,"/","", "audio/*;video/*;playlist/plain;audio/x-mpegurl", 0 );
+    MimeTypes types;
+    QStringList audio, video, all;
+    audio << "audio/*";
+    audio << "playlist/plain";
+    audio << "audio/x-mpegurl";
+
+    video << "video/*";
+    video << "playlist/plain";
+
+    all += audio;
+    all += video;
+    types.insert("All Media Files", all );
+    types.insert("Audio",  audio );
+    types.insert("Video", video );
+
+    QString str = OFileDialog::getOpenFileName( 1,"/","", types, 0 );
     LineEdit1->setText(str);
 }
 
