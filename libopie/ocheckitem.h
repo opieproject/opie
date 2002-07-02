7b95fce497eaba5fd80ef75d5722c1925f843dac
Stefan Eilers <stefan@eilers-online.net>
2002-07-02 16:41:22 +0000

    no comment

--- libopie/ocheckitem.h
+++ libopie/ocheckitem.h
@@ -1,11 +1,8 @@
 /**********************************************************************
 ** Copyright (C) 2002 Stefan Eilers (se, eilers.stefan@epost.de
-** Copyright (C) 2000 Trolltech AS.  All rights reserved.
-**
-** This file is part of Qtopia Environment.
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software
+** GNU Lesser General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file LICENSE.GPL included in the
 ** packaging of this file.
 **
