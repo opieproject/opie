2a3c4ae6fb69ef9c8c295dba7765e23e079d08e5
Jose Luis Fernandez Barros <fbarros@handhelds.org>
2002-07-17 01:53:46 +0000

    Initial translation

--- i18n/es/libliquid.ts
+++ i18n/es/libliquid.ts
@@ -3,59 +3,59 @@
     <name>LiquidSettings</name>
     <message>
         <source>Liquid Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Estilo Líquido</translation>
     </message>
     <message>
         <source>No translucency</source>
-        <translation type="unfinished"></translation>
+        <translation>Sin transparencias</translation>
     </message>
     <message>
         <source>Stippled, background color</source>
-        <translation type="unfinished"></translation>
+        <translation>Punteado, color de fondo</translation>
     </message>
     <message>
         <source>Stippled, button color</source>
-        <translation type="unfinished"></translation>
+        <translation>Punteado, color de botón</translation>
     </message>
     <message>
         <source>Translucent stippled, background color</source>
-        <translation type="unfinished"></translation>
+        <translation>Punteado translúcido, color de fondo</translation>
     </message>
     <message>
         <source>Translucent stippled, button color</source>
-        <translation type="unfinished"></translation>
+        <translation>Punteado translúcido, color de botón</translation>
     </message>
     <message>
         <source>Custom translucency</source>
-        <translation type="unfinished"></translation>
+        <translation>Transparencia propia</translation>
     </message>
     <message>
         <source>Menu color</source>
-        <translation type="unfinished"></translation>
+        <translation>Color del menú</translation>
     </message>
     <message>
         <source>Text color</source>
-        <translation type="unfinished"></translation>
+        <translation>Color del texto</translation>
     </message>
     <message>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Opacidad</translation>
     </message>
     <message>
         <source>Use shadowed menu text</source>
-        <translation type="unfinished"></translation>
+        <translation>Usar texto del menú sombreado</translation>
     </message>
     <message>
         <source>Draw liquid window title bars</source>
-        <translation type="unfinished"></translation>
+        <translation>Barra título ventana líquida</translation>
     </message>
     <message>
         <source>Make toolbar buttons appear flat</source>
-        <translation type="unfinished"></translation>
+        <translation>Botones planos en barra herramientas</translation>
     </message>
     <message>
         <source>Stipple contrast</source>
-        <translation type="unfinished"></translation>
+        <translation>Puntear contraste</translation>
     </message>
 </context>
 <context>
@@ -63,12 +63,12 @@
     <message>
         <source>Liquid</source>
         <comment>name</comment>
-        <translation type="unfinished"></translation>
+        <translation>Líquido</translation>
     </message>
     <message>
         <source>High Performance Liquid style by Mosfet</source>
         <comment>description</comment>
-        <translation type="unfinished"></translation>
+        <translation>Estilo Líquido de alto rendimiento por Mosfet</translation>
     </message>
 </context>
 </TS>
