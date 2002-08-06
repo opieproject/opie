43774490da2a9e91ef10baadeead0d358ec6e614
Lorn Potter <lpotter@trolltech.com>
2002-08-06 23:19:32 +0000

    doesnt compile without this file

---
+++ noncore/unsupported/oipkg/debug.h
@@ -0,0 +1,21 @@
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#include <stdio.h>
+
+extern int debugLevel;
+
+#define HACK
+#define PACKAGELISTLOCAL
+#define NEWLAYOUT
+
+#define pvDebug(I, S) \
+if ( debugLevel < 3 ) \
+{ \
+	if ( I <= debugLevel ) qDebug(S);\
+}else{\
+	if ( I <= debugLevel ) \
+		qDebug("#%s:%i: %s \t\t(Level: %i)",__FILE__,__LINE__,QString(S).latin1(),I);\
+}
+
+#endif
