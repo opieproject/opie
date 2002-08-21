93423f2c2cec38cc17579dc98a214a1cbf467871
Lorn Potter <lpotter@trolltech.com>
2002-08-21 11:06:40 +0000

    patch from drw, adds otabwidget

--- libopie/libopie.pro
+++ libopie/libopie.pro
@@ -1,8 +1,8 @@
 TEMPLATE        = lib
 CONFIG          += qte warn_on release
-HEADERS 	= ofontmenu.h ofileselector.h ofiledialog.h ofileview.h tododb.h todoevent.h todoresource.h todovcalresource.h xmltree.h  colordialog.h colorpopupmenu.h oclickablelabel.h oprocctrl.h oprocess.h odevice.h otimepicker.h
-SOURCES		= ofontmenu.cc ofileselector.cc ofiledialog.cc xmltree.cc tododb.cpp todoevent.cpp todovcalresource.cpp  colordialog.cpp colorpopupmenu.cpp oclickablelabel.cpp oprocctrl.cpp oprocess.cpp odevice.cpp otimepicker.cpp
-TARGET		= opie
+HEADERS   = ofontmenu.h ofileselector.h ofiledialog.h ofileview.h tododb.h todoevent.h todoresource.h todovcalresource.h xmltree.h  colordialog.h colorpopupmenu.h oclickablelabel.h oprocctrl.h oprocess.h odevice.h otimepicker.h otabwidget.h otabinfo.h
+SOURCES   = ofontmenu.cc ofileselector.cc ofiledialog.cc xmltree.cc tododb.cpp todoevent.cpp todovcalresource.cpp  colordialog.cpp colorpopupmenu.cpp oclickablelabel.cpp oprocctrl.cpp oprocess.cpp odevice.cpp otimepicker.cpp otabwidget.cpp
+TARGET    = opie
 INCLUDEPATH += $(OPIEDIR)/include
 DESTDIR         = $(QTDIR)/lib$(PROJMAK)
 #VERSION = 1.0.0
@@ -10,16 +10,16 @@ DESTDIR         = $(QTDIR)/lib$(PROJMAK)
 INTERFACES      = otimepickerbase.ui
 
 TRANSLATIONS = ../i18n/de/libopie.ts \
-	 ../i18n/en/libopie.ts \
-	 ../i18n/es/libopie.ts \
-	 ../i18n/fr/libopie.ts \
-	 ../i18n/hu/libopie.ts \
-	 ../i18n/ja/libopie.ts \
-	 ../i18n/ko/libopie.ts \
-	 ../i18n/no/libopie.ts \
-	 ../i18n/pl/libopie.ts \
-	 ../i18n/pt/libopie.ts \
-	 ../i18n/pt_BR/libopie.ts \
-	 ../i18n/sl/libopie.ts \
-	 ../i18n/zh_CN/libopie.ts \
-	 ../i18n/zh_TW/libopie.ts
+   ../i18n/en/libopie.ts \
+   ../i18n/es/libopie.ts \
+   ../i18n/fr/libopie.ts \
+   ../i18n/hu/libopie.ts \
+   ../i18n/ja/libopie.ts \
+   ../i18n/ko/libopie.ts \
+   ../i18n/no/libopie.ts \
+   ../i18n/pl/libopie.ts \
+   ../i18n/pt/libopie.ts \
+   ../i18n/pt_BR/libopie.ts \
+   ../i18n/sl/libopie.ts \
+   ../i18n/zh_CN/libopie.ts \
+   ../i18n/zh_TW/libopie.ts
---
+++ libopie/otabwidget.cpp
@@ -0,0 +1,267 @@
+/*
+                            This file is part of the Opie Project
+
+                             Copyright (c)  2002 Dan Williams <williamsdr@acm.org>
+              =.
+            .=l.
+           .>+-=
+ _;:,     .>    :=|.         This program is free software; you can
+.> <`_,   >  .   <=          redistribute it and/or  modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU General Public
+.="- .-=="i,     .._         License as published by the Free Software
+ - .   .-<_>     .<>         Foundation; either version 2 of the License,
+     ._= =}       :          or (at your option) any later version.
+    .%`+i>       _;_.
+    .i_,=:_.      -<s.       This program is distributed in the hope that
+     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    : ..    .:,     . . .    without even the implied warranty of
+    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=       =       ;      Library General Public License for more
+++=   -.     .`     .:       details.
+ :     =  ...= . :.=-
+ -.   .:....=;==+<;          You should have received a copy of the GNU
+  -_. . .   )=.  =           Library General Public License along with
+    --        :-=`           this library; see the file COPYING.LIB.
+                             If not, write to the Free Software Foundation,
+                             Inc., 59 Temple Place - Suite 330,
+                             Boston, MA 02111-1307, USA.
+
+*/
+
+#include "otabwidget.h"
+
+#include <qpe/config.h>
+#include <qpe/resource.h>
+
+#include <qcombobox.h>
+#include <qtabbar.h>
+#include <qwidgetstack.h>
+
+OTabWidget::OTabWidget( QWidget *parent, const char *name, TabStyle s, TabPosition p )
+    : QWidget( parent, name )
+{
+    if ( s == Global )
+    {
+        Config config( "qpe" );
+        config.setGroup( "Appearance" );
+        tabBarStyle = ( TabStyle ) config.readNumEntry( "TabStyle", (int) IconTab );
+        if ( tabBarStyle <= Global || tabBarStyle > IconList)
+        {
+            tabBarStyle = IconTab;
+        }
+        QString pos = config.readEntry( "TabPosition", "Top");
+        if ( pos == "Bottom" )
+        {
+            tabBarPosition = Bottom;
+        }
+        else
+        {
+            tabBarPosition = Top;
+        }
+    }
+    else
+    {
+        tabBarStyle = s;
+        tabBarPosition = p;
+    }
+
+    widgetStack = new QWidgetStack( this, "widgetstack" );
+    widgetStack->setFrameStyle( QFrame::StyledPanel | QFrame::Raised );
+    widgetStack->setLineWidth( style().defaultFrameWidth() );
+
+    tabBarStack = new QWidgetStack( this, "tabbarstack" );
+
+    tabBar = new QTabBar( tabBarStack, "tabbar" );
+    tabBarStack->addWidget( tabBar, 0 );
+    connect( tabBar, SIGNAL( selected( int ) ), this, SLOT( slotTabBarSelected( int ) ) );
+
+    tabList = new QComboBox( false, tabBarStack, "tablist" );
+    tabBarStack->addWidget( tabList, 1 );
+    connect( tabList, SIGNAL( activated( int ) ), this, SLOT( slotTabListSelected( int ) ) );
+
+    if ( tabBarStyle == TextTab || tabBarStyle == IconTab )
+    {
+        tabBarStack->raiseWidget( tabBar );
+    }
+    else if ( tabBarStyle == TextList || tabBarStyle == IconList )
+    {
+        tabBarStack->raiseWidget( tabList );
+    }
+
+    if ( tabBarPosition == Bottom )
+    {
+        tabBar->setShape( QTabBar::RoundedBelow );
+    }
+
+    currentTab= 0x0;
+}
+
+OTabWidget::~OTabWidget()
+{
+}
+
+void OTabWidget::addTab( QWidget *child, const QString &icon, const QString &label )
+{
+    QPixmap iconset = loadSmooth( icon );
+
+    QTab * tab = new QTab();
+    if ( tabBarStyle == IconTab )
+    {
+        tab->label = QString::null;
+    }
+    else
+    {
+        tab->label = label;
+    }
+    if ( tabBarStyle == IconTab || tabBarStyle == IconList)
+    {
+        tab->iconset = new QIconSet( iconset );
+    }
+    int tabid = tabBar->addTab( tab );
+
+    if ( tabBarStyle == IconTab || tabBarStyle == IconList )
+    {
+        tabList->insertItem( iconset, label, -1 );
+    }
+    else
+    {
+        tabList->insertItem( label );
+    }
+
+    widgetStack->addWidget( child, tabid );
+    widgetStack->raiseWidget( child );
+
+    OTabInfo *tabinfo = new OTabInfo( tabid, child, icon, label );
+    tabs.append( tabinfo );
+    selectTab( tabinfo );
+}
+
+void OTabWidget::setCurrentTab( QWidget *childwidget )
+{
+    OTabInfo *newtab = tabs.first();
+    while ( newtab && newtab->control() != childwidget )
+    {
+        newtab = tabs.next();
+    }
+    if ( newtab && newtab->control() == childwidget )
+    {
+        selectTab( newtab );
+    }
+}
+
+void OTabWidget::setCurrentTab( const QString &tabname )
+{
+    OTabInfo *newtab = tabs.first();
+    while ( newtab && newtab->label() != tabname )
+    {
+        newtab = tabs.next();
+    }
+    if ( newtab && newtab->label() == tabname )
+    {
+        selectTab( newtab );
+    }
+}
+
+OTabWidget::TabStyle OTabWidget::tabStyle() const
+{
+    return tabBarStyle;
+}
+
+void OTabWidget::setTabStyle( TabStyle s )
+{
+    tabBarStyle = s;
+}
+
+OTabWidget::TabPosition OTabWidget::tabPosition() const
+{
+    return tabBarPosition;
+}
+
+void OTabWidget::setTabPosition( TabPosition p )
+{
+    tabBarPosition = p;
+}
+
+void OTabWidget::slotTabBarSelected( int id )
+{
+    OTabInfo *newtab = tabs.first();
+    while ( newtab && newtab->id() != id )
+    {
+        newtab = tabs.next();
+    }
+    if ( newtab && newtab->id() == id )
+    {
+        selectTab( newtab );
+    }
+}
+
+void OTabWidget::slotTabListSelected( int index )
+{
+    OTabInfo *newtab = tabs.at( index );
+    if ( newtab )
+    {
+        selectTab( newtab );
+    }
+}
+
+QPixmap OTabWidget::loadSmooth( const QString &name )
+{
+    QImage image = Resource::loadImage( name );
+    QPixmap pixmap;
+    pixmap.convertFromImage( image.smoothScale( 16, 16 ) );
+    return pixmap;
+}
+
+void OTabWidget::selectTab( OTabInfo *tab )
+{
+    if ( tabBarStyle == IconTab )
+    {
+        if ( currentTab )
+        {
+            tabBar->tab( currentTab->id() )->setText( QString::null );
+            setUpLayout();
+        }
+        tabBar->tab( tab->id() )->setText( tab->label() );
+        currentTab = tab;
+    }
+    tabBar->setCurrentTab( tab->id() );
+    setUpLayout();
+    tabBar->update();
+
+    widgetStack->raiseWidget( tab->control() );
+}
+
+void OTabWidget::setUpLayout()
+{
+    tabBar->layoutTabs();
+    QSize t( tabBarStack->sizeHint() );
+    if ( t.width() > width() )
+        t.setWidth( width() );
+    int lw = widgetStack->lineWidth();
+    if ( tabBarPosition == Bottom )
+    {
+        tabBarStack->setGeometry( QMAX(0, lw-2), height() - t.height() - lw, t.width(), t.height() );
+        widgetStack->setGeometry( 0, 0, width(), height()-t.height()+QMAX(0, lw-2) );
+    }
+    else
+    {
+        tabBarStack->setGeometry( QMAX(0, lw-2), 0, t.width(), t.height() );
+        widgetStack->setGeometry( 0, t.height()-lw, width(), height()-t.height()+QMAX(0, lw-2));
+    }
+
+    if ( autoMask() )
+        updateMask();
+}
+
+QSize OTabWidget::sizeHint() const
+{
+    QSize s( widgetStack->sizeHint() );
+    QSize t( tabBarStack->sizeHint() );
+    return QSize( QMAX( s.width(), t.width()), s.height() + t.height() );
+}
+
+void OTabWidget::resizeEvent( QResizeEvent * )
+{
+    setUpLayout();
+}
