f7dcbe0fe2c277125cae5d245c1f1b5e97074942
Carsten Niehaus <cniehaus@handhelds.org>
2002-08-27 19:37:30 +0000

    updates for italien

--- i18n/it/today.ts
+++ i18n/it/today.ts
@@ -1,117 +1,129 @@
 <!DOCTYPE TS><TS>
+<codec>iso8859-1</codec>
 <context>
     <name>Today</name>
     <message>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Oggi</translation>
     </message>
     <message>
         <source>Owned by </source>
-        <translation type="unfinished"></translation>
+        <translation>Proprietà di</translation>
     </message>
     <message>
         <source>Please fill out the business card</source>
-        <translation type="unfinished"></translation>
+        <translation>Inserisci i tuoi dati personali</translation>
     </message>
     <message>
         <source>No more appointments today</source>
-        <translation type="unfinished"></translation>
+        <translation>Nessun altro appuntamento per oggi</translation>
     </message>
     <message>
         <source>No appointments today</source>
-        <translation type="unfinished"></translation>
+        <translation>Nessun appuntamento per oggi</translation>
     </message>
     <message>
         <source>&lt;b&gt;%1&lt;/b&gt; new mail(s), &lt;b&gt;%2&lt;/b&gt; outgoing</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; nuove e-mail, &lt;b&gt;%2&lt;/b&gt; in uscita</translation>
     </message>
     <message>
         <source>There is &lt;b&gt; 1&lt;/b&gt; active task:  &lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>C&apos;è &lt;b&gt; 1&lt;/b&gt; promemoria attivo:  &lt;br&gt;</translation>
     </message>
     <message>
         <source>There are &lt;b&gt; %1&lt;/b&gt; active tasks: &lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Ci sono&lt;b&gt; %1&lt;/b&gt; promemoria attivi: &lt;br&gt;</translation>
     </message>
     <message>
         <source>No active tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Non ci sono promemoria attivi</translation>
     </message>
 </context>
 <context>
     <name>TodayBase</name>
     <message>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Oggi</translation>
     </message>
     <message>
         <source>Opiemail not installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Opiemail non installato</translation>
     </message>
 </context>
 <context>
     <name>todayconfig</name>
     <message>
         <source>Today config</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurazione di &apos;Oggi&apos;</translation>
     </message>
     <message>
         <source>Should the 
 location 
 be shown?</source>
-        <translation type="unfinished"></translation>
+        <translation>Visualizzare
+la posizione?</translation>
     </message>
     <message>
         <source>Should the notes 
 be shown?</source>
-        <translation type="unfinished"></translation>
+        <translation>Visualizzare le
+note?</translation>
     </message>
     <message>
         <source>Show only later
 appointments</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrare solo gli
+app. successivi?</translation>
     </message>
     <message>
         <source>How many 
 appointment
 should
 be shown?</source>
-        <translation type="unfinished"></translation>
+        <translation>Quanti
+appuntamenti
+visualizzare?</translation>
     </message>
     <message>
         <source>Calendar</source>
-        <translation type="unfinished"></translation>
+        <translation>Rubrica</translation>
     </message>
     <message>
         <source>How many
 tasks should 
 be shown?</source>
-        <translation type="unfinished"></translation>
+        <translation>Quanti promemoria
+visualizzare?</translation>
     </message>
     <message>
         <source>Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Promemoria</translation>
     </message>
     <message>
         <source>Clip after how
 many letters</source>
-        <translation type="unfinished"></translation>
+        <translation>Quanti caratteri
+visualizzare?</translation>
     </message>
     <message>
         <source>Should today be
 autostarted on
 resume? (Opie only)</source>
-        <translation type="unfinished"></translation>
+        <translation>Avviare &apos;Oggi&apos; 
+automaticamente
+all&apos;accensione?</translation>
     </message>
     <message>
         <source>Activate the  
 autostart after how
 many minutes?</source>
-        <translation type="unfinished"></translation>
+        <translation>Dopo quanti
+minuti attivare
+l&apos;avvio automatico?</translation>
     </message>
     <message>
         <source>Misc</source>
-        <translation type="unfinished"></translation>
+        <translation>Varie</translation>
     </message>
 </context>
 </TS>
