2db412175e91b1fb5209e09c0db22f5a6e915233
Carsten Niehaus <cniehaus@handhelds.org>
2002-08-29 15:34:49 +0000

    sorry for the spamming ;) I hope in some days I will have all i18n-things sorted out

---
+++ i18n/it/qpe.ts
@@ -0,0 +1,17 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>SyncDialog</name>
+    <message>
+        <source>Syncing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Contacts&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
