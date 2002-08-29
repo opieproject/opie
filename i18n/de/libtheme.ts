47d8520dcf840c974f1d1ea04c0ea47eb1128573
Carsten Niehaus <cniehaus@handhelds.org>
2002-08-29 14:31:37 +0000

    some new files

---
+++ i18n/de/libtheme.ts
@@ -0,0 +1,39 @@
+<!DOCTYPE TS><TS>
+<defaultcodec>iso8859-1</defaultcodec>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Themed style</source>
+        <comment>name</comment>
+        <translation>Schema</translation>
+    </message>
+    <message>
+        <source>KDE2 theme compatible style engine</source>
+        <comment>description</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThemeSettings</name>
+    <message>
+        <source>Theme Style</source>
+        <translation>Schema</translation>
+    </message>
+    <message>
+        <source>Select the theme to be used</source>
+        <translation>Wählen Sie das Schema aus, welches benutzt werden soll</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Beschreibung</translation>
+    </message>
+    <message>
+        <source>[No theme]</source>
+        <translation>[keine Schema}</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/fr/libmetal.ts
@@ -0,0 +1,15 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Metal</source>
+        <comment>name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Metal style</source>
+        <comment>description</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
