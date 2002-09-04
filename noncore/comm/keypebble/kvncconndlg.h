ffeb92239744460be778e7578a4e0e3ad162b4a8
Greg Gilbert <treke@handhelds.org>
2002-09-04 18:17:42 +0000

    This does build. More missing files

---
+++ noncore/comm/keypebble/kvncconndlg.h
@@ -0,0 +1,33 @@
+// -*- c++ -*-
+
+#ifndef KVNCCONNECTION_H
+#define KVNCCONNECTION_H
+
+#include "kvncconndlgbase.h"
+#include "krfbserver.h"
+
+
+class KVNCConnDlg : public KVNCConnDlgBase
+{
+		Q_OBJECT
+
+		public:
+				KVNCConnDlg(  KRFBServer *options,
+								QWidget *parent = 0, char *name = 0, bool modal = true );
+				~KVNCConnDlg();
+
+		protected:
+				void accept();
+		private slots:
+				void save();
+
+		private:
+				KRFBServer tmpOptions;
+				KRFBServer * options;
+};
+
+#endif // KVNCCONNECTION_H
+
+
+
+
