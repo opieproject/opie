321c67c4af28d1258a0b5227e4c86220d79bfaf4
David Woodhouse <dwmw2@handhelds.org>
2002-09-20 13:38:37 +0000

    Copyright and licence noise

--- noncore/settings/sshkeys/main.cpp
+++ noncore/settings/sshkeys/main.cpp
@@ -1,3 +1,23 @@
+/* 
+ *  ssh-agent key manipulation utility
+ *
+ *  (C) 2002 David Woodhouse <dwmw2@infradead.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #include <qpe/qpeapplication.h>
 #include "sshkeys.h"
@@ -8,7 +28,6 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-
 int main(int argc, char *argv[])
 {
 	QPEApplication a(argc, argv);
@@ -16,7 +35,7 @@ int main(int argc, char *argv[])
         int fd;
 
 	/* If we had a controlling TTY, detach from it. 
-	   This is to ensure the SSH uses ssh-askpass */
+	   This is to ensure that SSH uses ssh-askpass */
 	fd = open("/dev/tty", O_RDONLY);
         if (fd != -1) {
                 ioctl(fd, TIOCNOTTY, NULL);
