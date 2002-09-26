a27d0f66958c00aefa7cdc27c8d7f6a31d80c32f
Robert Griebl <sandman@handhelds.org>
2002-09-26 01:14:30 +0000

    Corrected the copyright headers (LGPL -> GPL)

--- noncore/settings/appearance2/decolistitem.h
+++ noncore/settings/appearance2/decolistitem.h
@@ -2,29 +2,29 @@
                =.            This file is part of the OPIE Project
              .=l.            Copyright (c)  2002 Robert Griebl <sandman@handhelds.org>
            .>+-=
- _;:,     .>    :=|.         This library is free software; you can
-.> <`_,   >  .   <=          redistribute it and/or  modify it under
-:`=1 )Y*s>-.--   :           the terms of the GNU Library General Public
+ _;:,     .>    :=|.         This file is free software; you can
+.> <`_,   >  .   <=          redistribute it and/or modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU General Public
 .="- .-=="i,     .._         License as published by the Free Software
  - .   .-<_>     .<>         Foundation; either version 2 of the License,
      ._= =}       :          or (at your option) any later version.
     .%`+i>       _;_.
-    .i_,=:_.      -<s.       This library is distributed in the hope that
-     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    .i_,=:_.      -<s.       This file is distributed in the hope that
+     +  .  -:.       =       it will be useful, but WITHOUT ANY WARRANTY;
     : ..    .:,     . . .    without even the implied warranty of
     =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
-  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
-..}^=.=       =       ;      Library General Public License for more
-++=   -.     .`     .:       details.
- :     =  ...= . :.=-
- -.   .:....=;==+<;          You should have received a copy of the GNU
-  -_. . .   )=.  =           Library General Public License along with
-    --        :-=`           this library; see the file COPYING.LIB.
-                             If not, write to the Free Software Foundation,
-                             Inc., 59 Temple Place - Suite 330,
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU General
+..}^=.=       =       ;      Public License for more details.
+++=   -.     .`     .:       
+ :     =  ...= . :.=-        You should have received a copy of the GNU
+ -.   .:....=;==+<;          General Public License along with this file;
+  -_. . .   )=.  =           see the file COPYING. If not, write to the
+    --        :-=`           Free Software Foundation, Inc.,
+                             59 Temple Place - Suite 330,
                              Boston, MA 02111-1307, USA.
 
 */
+
 #ifndef DECOLISTITEM_H
 #define DECOLISTITEM_H
 
