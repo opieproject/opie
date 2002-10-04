10e7e3b075783aeedb4770a0f2d1de26c7bfd529
Carsten Niehaus <cniehaus@handhelds.org>
2002-10-04 13:38:06 +0000

    wow, I somehow forgot to cvs ci these files

---
+++ i18n/zh_TW/libsuspendapplet.ts
@@ -0,0 +1,13 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>SuspendApplet</name>
+    <message>
+        <source>Suspend shortcut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
