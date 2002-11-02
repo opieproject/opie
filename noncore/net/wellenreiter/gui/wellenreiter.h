5d32f7f61fc67c3f4df606f10c13cf49451d8a0c
Michael Lauer <mickey@vanille-media.de>
2002-11-02 15:37:48 +0000

    Started the GUI portion of Wellenreiter (no functionality yet).

---
+++ noncore/net/wellenreiter/gui/wellenreiter.h
@@ -0,0 +1,40 @@
+/**********************************************************************
+** Copyright (C) 2002 Michael 'Mickey' Lauer.  All rights reserved.
+**
+** This file is part of Opie Environment.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+**********************************************************************/
+
+#ifndef WELLENREITER_H
+#define WELLENREITER_H
+
+#include "wellenreiterbase.h"
+
+class Wellenreiter : public WellenreiterBase {
+    Q_OBJECT
+
+public:
+
+    Wellenreiter( QWidget* parent = 0, const char* name = 0, WFlags fl = 0 );
+    ~Wellenreiter();
+
+protected:
+
+
+private slots:
+    void theButton();
+
+private:
+    //void readConfig();
+    //void writeConfig();
+};
+
+#endif
---
+++ noncore/net/wellenreiter/wellenreiter.pro
@@ -0,0 +1,11 @@
+DESTDIR         = $(OPIEDIR)/bin
+TEMPLATE        = app
+CONFIG          = qt warn_on debug
+#CONFIG         = qt warn_on release
+HEADERS         = gui/mainwindowbase.h gui/wellenreiter.h
+SOURCES         = main.cpp gui/wellenreiter.cpp
+INCLUDEPATH     += $(OPIEDIR)/include gui daemon
+DEPENDPATH      += $(OPIEDIR)/include gui daemon
+LIBS            += -lqpe
+INTERFACES      = gui/wellenreiterbase.ui
+TARGET          = wellenreiter
