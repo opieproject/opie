83a1a1e34b7a68898500f59e0b2c0de924566773
Holger Freyther <zecke@handhelds.org>
2002-11-03 11:10:04 +0000

    No default arguments.... Patch by jowenn

--- inputmethods/handwriting/qimpeninput.h
+++ inputmethods/handwriting/qimpeninput.h
@@ -41,7 +41,7 @@ class QIMPenInput : public QFrame
 {
     Q_OBJECT
 public:
-    QIMPenInput( QWidget *parent = 0, const char *name = 0, WFlags f = 0 );
+    QIMPenInput( QWidget *parent = 0, const char *name = 0, WFlags wf = 0 );
     virtual ~QIMPenInput();
 
     void resetState();
