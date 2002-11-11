14c2200e104cb3ab6816979dcac70c9a167feb79
Benjamin Meyer <ben@meyerhome.net>
2002-11-11 21:30:17 +0000

    Initial framing

--- noncore/net/networksetup/ppp/pppimp.h
+++ noncore/net/networksetup/ppp/pppimp.h
@@ -5,10 +5,18 @@
 
 class PPPConfigureImp : public PPP {
 
+Q_OBJECT
+	
 public:
-  PPPConfigureImp();
-  ~PPPConfigureImp();
+  PPPConfigureImp(QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0);
+  void setPeer(const QString &peer);
 
+protected:
+  void accept();
+
+private:
+  QString peer;
+  
 };
 
 #endif
