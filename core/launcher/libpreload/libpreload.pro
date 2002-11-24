677d05664937e7be1e1f8e0ddc89f9a1a3704c94
Robert Griebl <sandman@handhelds.org>
2002-11-24 18:18:35 +0000

    A small library that can be LD_PRELOADed to other applications by the launcher to force these applications to use the Opie style/font/window­ decoration settings.
    Could also be used for other things in the future...

---
+++ core/launcher/libpreload/libpreload.pro
@@ -0,0 +1,14 @@
+TEMPLATE    =  lib
+
+CONFIG       = release warn_on
+
+SOURCES     =  preload.cpp
+               
+HEADERS     =  
+
+INCLUDEPATH += $(QTDIR)/include
+
+DESTDIR      = $(OPIEDIR)/lib
+TARGET       = preload
+
+VERSION      = 1.0.0
---
+++ core/launcher/libpreload/preload.cpp
@@ -0,0 +1,91 @@
+
+#define _GNU_SOURCE
+//#define private public
+//#define protected public
+
+#include <qwsdecoration_qws.h>
+#include <qcommonstyle.h>
+//#include <qpe/qpeapplication.h>
+
+class QStyle;
+class QApplication;
+class QFont;
+class QWSDecoration;
+
+#include <dlfcn.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef void ( *qapp_setstyle_t ) ( QStyle * );
+typedef void ( *qapp_setdeco_t ) ( QApplication *, QWSDecoration * ); 
+typedef void ( *qapp_setfont_t ) ( QApplication *, const QFont &, bool, const char * ); 
+
+
+static bool *opie_block_style_p = 0;
+
+extern "C" {
+
+static void *resolve_symbol ( const char *sym )
+{
+	void *adr = ::dlsym ( RTLD_NEXT, sym );
+
+	if ( !adr )
+		::fprintf ( stderr, "PANIC: Could not resolve symbol \"%s\"\n", sym );
+		
+	return adr;
+}
+
+extern void setStyle__12QApplicationP6QStyle ( QStyle *style )
+{
+	static qapp_setstyle_t qsetstyle = 0;
+
+	if ( !qsetstyle )
+		qsetstyle = (qapp_setstyle_t) resolve_symbol ( "setStyle__12QApplicationP6QStyle" );
+
+	if ( !opie_block_style_p )
+		opie_block_style_p = (bool *) resolve_symbol ( "opie_block_style" );
+	
+	if ( !qsetstyle || ( opie_block_style_p && *opie_block_style_p )) {
+		delete style;
+		return;
+	}
+	else
+		( *qsetstyle ) ( style );
+}
+
+extern void setFont__12QApplicationRC5QFontbPCc ( QApplication *app, const QFont &fnt, bool informWidgets, const char * className )
+{
+	static qapp_setfont_t qsetfont = 0;
+
+	if ( !qsetfont )
+		qsetfont = (qapp_setfont_t) resolve_symbol ( "setFont__12QApplicationRC5QFontbPCc" );
+
+	if ( !opie_block_style_p )
+		opie_block_style_p = (bool *) resolve_symbol ( "opie_block_style" );
+	
+	if ( qsetfont && !( opie_block_style_p && *opie_block_style_p ))
+		( *qsetfont ) ( app, fnt, informWidgets, className );
+}
+	
+
+extern void qwsSetDecoration__12QApplicationP13QWSDecoration ( QApplication *app, QWSDecoration *deco )
+{
+	static qapp_setdeco_t qsetdeco = 0;
+
+	if ( !qsetdeco )
+		qsetdeco = (qapp_setdeco_t) resolve_symbol ( "qwsSetDecoration__12QApplicationP13QWSDecoration" );
+
+	if ( !opie_block_style_p )
+		opie_block_style_p = (bool *) resolve_symbol ( "opie_block_style" );
+	
+	if ( !qsetdeco || ( opie_block_style_p && *opie_block_style_p )) {
+		delete deco;
+		return;
+	}
+	else
+		( *qsetdeco ) ( app, deco );
+}
+
+}
