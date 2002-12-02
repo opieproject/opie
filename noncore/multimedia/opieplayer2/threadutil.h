28e80b653868e0b1d1b934ac8f8ae4a56c020cc2
Simon Hausmann <simon@handhelds.org>
2002-12-02 19:31:37 +0000

    - copyright fixlet

--- noncore/multimedia/opieplayer2/threadutil.h
+++ noncore/multimedia/opieplayer2/threadutil.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 1999 Simon Hausmann <hausmann@kde.org>
+   Copyright (C) 2002 Simon Hausmann <hausmann@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
