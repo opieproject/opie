d7794470c5365c3a2443dc371c4374f2c3aa4fc0
Simon Hausmann <simon@handhelds.org>
2002-12-11 12:34:12 +0000

    - starting with skin preloading

--- noncore/multimedia/opieplayer2/skin.cpp
+++ noncore/multimedia/opieplayer2/skin.cpp
@@ -9,26 +9,45 @@ Skin::Skin( const QString &name, const QString &fileNameInfix )
     m_skinPath = "opieplayer2/skins/" + name;
 }
 
+void Skin::preload( const MediaWidget::SkinButtonInfo *skinButtonInfo, uint buttonCount )
+{
+    backgroundImage();
+    buttonUpImage();
+    buttonDownImage();
+    for ( uint i = 0; i < buttonCount; ++i )
+        ( void )buttonMaskImage( skinButtonInfo[ i ].fileName );
+}
+
 QImage Skin::backgroundImage() const
 {
-    return QImage( Resource::findPixmap( QString( "%1/background" ).arg( m_skinPath ) ) );
+    if ( m_backgroundImage.isNull() )
+        m_backgroundImage = QImage( Resource::findPixmap( QString( "%1/background" ).arg( m_skinPath ) ) );
+    return m_backgroundImage;
 }
 
 QImage Skin::buttonUpImage() const
 {
-    return QImage( Resource::findPixmap( QString( "%1/skin%2_up" ).arg( m_skinPath ).arg( m_fileNameInfix ) ) );
+    if ( m_buttonUpImage.isNull() )
+        m_buttonUpImage = QImage( Resource::findPixmap( QString( "%1/skin%2_up" ).arg( m_skinPath ).arg( m_fileNameInfix ) ) );
+    return m_buttonUpImage;
 }
 
 QImage Skin::buttonDownImage() const
 {
-    return QImage( Resource::findPixmap( QString( "%1/skin%2_down" ).arg( m_skinPath ).arg( m_fileNameInfix ) ) );
+    if ( m_buttonDownImage.isNull() )
+        m_buttonDownImage = QImage( Resource::findPixmap( QString( "%1/skin%2_down" ).arg( m_skinPath ).arg( m_fileNameInfix ) ) );
+    return m_buttonDownImage;
 }
 
 QImage Skin::buttonMaskImage( const QString &fileName ) const
 {
-    QString prefix = m_skinPath + QString::fromLatin1( "/skin%1_mask_" ).arg( m_fileNameInfix );
-    QString path = prefix + fileName + ".png";
-    return QImage( Resource::findPixmap( path ) );
+    ButtonMaskImageMap::Iterator it = m_buttonMasks.find( fileName );
+    if ( it == m_buttonMasks.end() ) {
+        QString prefix = m_skinPath + QString::fromLatin1( "/skin%1_mask_" ).arg( m_fileNameInfix );
+        QString path = prefix + fileName + ".png";
+        it = m_buttonMasks.insert( fileName, QImage( Resource::findPixmap( path ) ) );
+    }
+    return *it;
 }
 
 /* vim: et sw=4 ts=4
--- noncore/multimedia/opieplayer2/skin.h
+++ noncore/multimedia/opieplayer2/skin.h
@@ -3,11 +3,16 @@
 
 #include <qstring.h>
 #include <qimage.h>
+#include <qmap.h>
+
+#include "mediawidget.h"
 
 class Skin
 {
 public:
     Skin( const QString &name, const QString &fileNameInfix );
+
+    void preload( const MediaWidget::SkinButtonInfo *skinButtonInfo, uint buttonCount );
 
     QImage backgroundImage() const;
     QImage buttonUpImage() const;
@@ -19,6 +24,13 @@ private:
     QString m_name;
     QString m_fileNameInfix;
     QString m_skinPath;
+
+    typedef QMap<QString, QImage> ButtonMaskImageMap;
+
+    mutable QImage m_backgroundImage;
+    mutable QImage m_buttonUpImage;
+    mutable QImage m_buttonDownImage;
+    mutable ButtonMaskImageMap m_buttonMasks;
 
     Skin( const Skin & );
     Skin &operator=( const Skin & );
