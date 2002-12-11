f7efd112ce413b26c2486bcbb1df93e59263a8ed
Tais P. Hansen <tais.hansen@osd.dk>
2002-12-11 00:44:03 +0000

    Added danish translations.

---
+++ i18n/da/security.ts
@@ -0,0 +1,102 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Security</name>
+    <message>
+        <source>Set passcode</source>
+        <translation>Vælg kode</translation>
+    </message>
+    <message>
+        <source>Change passcode</source>
+        <translation>Skift kode</translation>
+    </message>
+    <message>
+        <source>Enter passcode</source>
+        <translation>Indtast kode</translation>
+    </message>
+    <message>
+        <source>Passcode incorrect</source>
+        <translation>Koden er forkert</translation>
+    </message>
+    <message>
+        <source>The passcode entered is incorrect.
+Access denied</source>
+        <translation>Den indtastede kode er forkert.
+Adgang nægtet</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation>Vilkårlig</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <source>Enter new passcode</source>
+        <translation>Indtast ny kode</translation>
+    </message>
+    <message>
+        <source>Re-enter new passcode</source>
+        <translation>Gentag ny kode</translation>
+    </message>
+</context>
+<context>
+    <name>SecurityBase</name>
+    <message>
+        <source>Security Settings</source>
+        <translation>Sikkerheds Indstillinger</translation>
+    </message>
+    <message>
+        <source>Change passcode</source>
+        <translation>Skift kode</translation>
+    </message>
+    <message>
+        <source>Clear passcode</source>
+        <translation>Slet kode</translation>
+    </message>
+    <message>
+        <source>Require pass code at power-on</source>
+        <translation>Spørg efter kode ved opstart</translation>
+    </message>
+    <message>
+        <source>Sync</source>
+        <translation>Synkronisering</translation>
+    </message>
+    <message>
+        <source>Accept sync from network:</source>
+        <translation>Tillad synkronisering fra netværk:</translation>
+    </message>
+    <message>
+        <source>192.168.129.0/24 (default)</source>
+        <translation>192.168.129.0/24 (standard)</translation>
+    </message>
+    <message>
+        <source>192.168.1.0/24</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>192.168.0.0/16</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>172.16.0.0/12</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>10.0.0.0/8</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation>Vilkårlig</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <source>&lt;P&gt;Pass code protection provides a minimal level of protection from casual access to this device.</source>
+        <translation>&lt;P&gt;Beskyttelse med kode giver en minimal sikkerhed mod almindelig adgang til denne enhed.</translation>
+    </message>
+</context>
+</TS>
