bb952e47bea54cb275cfe9d8e1b967efc994789c
Tais P. Hansen <tais.hansen@osd.dk>
2002-12-11 00:55:44 +0000

    Added danish translations.

---
+++ i18n/da/libqmultikey.ts
@@ -0,0 +1,64 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>ConfigDlg</name>
+    <message>
+        <source>Multikey Configuration</source>
+        <translation>Multikey Indstillinger</translation>
+    </message>
+    <message>
+        <source>General Settings</source>
+        <translation>Generelle indstillinger</translation>
+    </message>
+    <message>
+        <source>Keymap File</source>
+        <translation>Keymap fil</translation>
+    </message>
+    <message>
+        <source>Current Language</source>
+        <translation>Nuværende sprog</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Tilføj</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Fjern</translation>
+    </message>
+    <message>
+        <source>Pickboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Key Repeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Colors</source>
+        <translation>Farver</translation>
+    </message>
+    <message>
+        <source>Key Color</source>
+        <translation>Tast farve</translation>
+    </message>
+    <message>
+        <source>Key Pressed Color</source>
+        <translation>Trykket tast farve</translation>
+    </message>
+    <message>
+        <source>Line Color</source>
+        <translation>Linie farve</translation>
+    </message>
+    <message>
+        <source>Text Color</source>
+        <translation>Tekst farve</translation>
+    </message>
+</context>
+<context>
+    <name>InputMethods</name>
+    <message>
+        <source>Multikey</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
