b5a847c30caa4318adc2d5ce76828897b7298d15
Michael Lauer <mickey@vanille-media.de>
2002-12-12 10:53:44 +0000

    Config Tab is shaping up

---
+++ noncore/net/wellenreiter/gui/configwindow.h
@@ -0,0 +1,30 @@
+/**********************************************************************
+** Copyright (C) 2002 Michael 'Mickey' Lauer.  All rights reserved.
+**
+** This file is part of Opie Environment.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+**********************************************************************/
+
+#ifndef WELLENREITERCONFIGWINDOW_H
+#define WELLENREITERCONFIGWINDOW_H
+
+#include "configbase.h"
+
+class WellenreiterConfigWindow : public WellenreiterConfigBase
+{
+
+  public:
+    WellenreiterConfigWindow( QWidget * parent = 0, const char * name = "WellenreiterConfigWindow", WFlags f = 0 );
+  
+};
+
+#endif
+
