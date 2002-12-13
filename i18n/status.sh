cd2458f0323da612726bcd803010715bd1004ee3
Carsten Niehaus <cniehaus@handhelds.org>
2002-12-13 15:40:41 +0000

    I hope this miniscript will help me to get an overview

--- i18n/status.sh
+++ i18n/status.sh
@@ -1,7 +1,8 @@
 #/bin/sh
+arg=$1
 echo "number of strings: "
-grep translation go.ts | wc -l
+grep translation $arg/*.ts | wc -l
 echo "unfinished: "
-grep translation go.ts | grep type=\"unfinished\" | wc -l
+grep translation $arg/*.ts | grep type=\"unfinished\" | wc -l
 echo "obsolete: "
-grep translation go.ts | grep type=\"obsolete\" | wc -l
+grep translation $arg/*.ts | grep type=\"obsolete\" | wc -l
