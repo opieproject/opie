5b4d5114fd50cc9b39c40da823997362bb015cf1
Carsten Niehaus <cniehaus@handhelds.org>
2002-12-14 20:12:47 +0000

    lets do it the way KDE does

---
+++ i18n/xx/appskey.ts
@@ -0,0 +1,29 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>AppsKeyApp</name>
+    <message>
+        <source>Set Application Key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Calendar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Address Book</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Popup Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/xx/libqhandwriting.ts
@@ -0,0 +1,162 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>HandwritingHelp</name>
+    <message>
+        <source>Handwriting Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;ul&gt;&lt;li&gt;When you start to use the handwriting recogniser write slowly, accurately and firmly.&lt;li&gt;Use the guide lines when drawing your characters.&lt;li&gt;When drawing a character with multiple strokes, each successive stroke must be drawn before the grayed strokes are erased.&lt;li&gt;Practice your handwriting using the handwriting trainer.&lt;li&gt;When adding your own character templates make sure they are sufficiently different from other characters&apos; templates.&lt;/ul&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tips</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Trainer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HandwritingTrainer</name>
+    <message>
+        <source>Select a reference character from the list.  Practice writing in the area on the right.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No match</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Matched: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Similar to: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1%</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputMethods</name>
+    <message>
+        <source>Handwriting</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QIMPenEdit</name>
+    <message>
+        <source>New...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QIMPenInput</name>
+    <message>
+        <source>Shortcut</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QIMPenInputCharDlg</name>
+    <message>
+        <source>Enter new character</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Character:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QIMPenPrefBase</name>
+    <message>
+        <source>Form1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Multi-stroke character timeout:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Input areas displayed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Upper and lower case areas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lower case (toggle Upper case)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QIMPenSetup</name>
+    <message>
+        <source>Setup Handwriting Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Character Profile:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Customize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Out of space</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to save information.
+Free up some space
+and try again.
+
+Quit anyway?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
