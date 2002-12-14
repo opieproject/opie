8cc674b21125b2ca0031aa0d395bdb108f0d34cc
Carsten Niehaus <cniehaus@handhelds.org>
2002-12-14 20:10:43 +0000

    Hi Mellen :)
    As you see I would like to give you a chance to work for OPIE ;)
    cfish made the script working and now you have a _lot_ new files.

---
+++ i18n/da/mediummount.ts
@@ -0,0 +1,87 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Global</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MediumGlobalWidget</name>
+    <message>
+        <source>Enable medium checking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use global settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Which media files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MediumMountWidget</name>
+    <message>
+        <source>Configure this medium. The changes will go into effect when the application get&apos;s closed. To update the Document Tab you need to removeand insert this medium.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Which media files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Limit search to:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Always check this medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
