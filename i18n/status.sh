51c78b311c407c8da3f8883a8d9c0dc1f4301deb
Carsten Niehaus <cniehaus@handhelds.org>
2002-12-26 13:16:41 +0000

    zh_TW is back

--- i18n/status.sh
+++ i18n/status.sh
@@ -21,8 +21,8 @@ echo "obsolete: "
 obso=`find $arg -name "*.ts" -exec cat {} \; | grep translation | grep type=\"obsolete\" | wc -l`
 echo $obso `expr \( $strs - $obso \) \* 100 / $strs`%
 
-if [ "foo" = "all" ]
-then
+#if [ "foo" = "all" ]
+#then
 	#from here on we will only look at the core-parts. All numbers have to be 0% if
 	#the specific language will be in the official release.
 	echo
@@ -37,10 +37,10 @@ then
 	_core_addressbook=`grep translation $arg/addressbook.ts | wc -l`
 	_core_datebook=`grep translation $arg/datebook.ts | wc -l`
 	_libopie=`grep translation $arg/libopie.ts | wc -l`
+#fi
 
 	echo "Today:" $core_today", that means" `expr $core_today \* 100 / $_core_today`% "needs to be done"
 	echo "Datebook:" $core_datebook", that means" `expr $core_datebook \* 100 / $_core_datebook`% "needs to be done"
 	echo "Todo:" $core_todo", that means" `expr $core_todo \* 100 / $_core_todo`% "needs to be done"
 	echo "Addressbook:" $core_addressbook", that means" `expr $core_addressbook \* 100 / $_core_addressbook`% "needs to be done"
 	echo "libopie:" $libopie", that means" `expr $libopie \* 100 / $_libopie`% "needs to be done"
-fi
