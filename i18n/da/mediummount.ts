3986305e52baa07f85e16a2f3fce17aa538070f4
Tais P. Hansen <tais.hansen@osd.dk>
2002-12-26 14:12:55 +0000

    Danish translations.

--- i18n/da/mediummount.ts
+++ i18n/da/mediummount.ts
@@ -3,85 +3,97 @@
     <name>MainWindow</name>
     <message>
         <source>Global</source>
-        <translation type="unfinished"></translation>
+        <translation>Global</translation>
+    </message>
+    <message>
+        <source>Medium Mount Settings</source>
+        <translation>Medie montering indstillinger</translation>
     </message>
 </context>
 <context>
     <name>MediumGlobalWidget</name>
     <message>
         <source>Enable medium checking</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktiver medie tjek</translation>
     </message>
     <message>
         <source>Use global settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Brug globale indstillinger</translation>
     </message>
     <message>
         <source>Which media files</source>
-        <translation type="unfinished"></translation>
+        <translation>Hvilke medie filer</translation>
     </message>
     <message>
         <source>Audio</source>
-        <translation type="unfinished"></translation>
+        <translation>Lyd</translation>
     </message>
     <message>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle</translation>
     </message>
     <message>
         <source>Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Billede</translation>
     </message>
     <message>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekst</translation>
     </message>
     <message>
         <source>Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <source>If a medium gets inserted into this device Opie tries to search the medium for Documents. On large mediums this can take some time. You can choose if Opie should scan for Documents globally or on a per medium level. You&apos;re also able to reconfigure each medium.</source>
+        <translation>Hvis et medie bliver indsat i denne enhed vil Opie prøve at søge efter dokumenter på mediet. Dette kan tage en del tid på større medier. Du kan vælge om Opie skal skanne efter dokumenter globalt eller på et &quot;pr. medie&quot;-basis. Du kan også ændre tidligere indstillinger for hvert medie.</translation>
     </message>
 </context>
 <context>
     <name>MediumMountWidget</name>
     <message>
         <source>Configure this medium. The changes will go into effect when the application get&apos;s closed. To update the Document Tab you need to removeand insert this medium.</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Opsætning af dette medie. Ændringerne træder i kræft når programmet bliver lukket. For at opdatere Dokument-fanen skal du fjerne og isætte mediet igen.</translation>
     </message>
     <message>
         <source>Which media files</source>
-        <translation type="unfinished"></translation>
+        <translation>Hvilke medie filer</translation>
     </message>
     <message>
         <source>Audio</source>
-        <translation type="unfinished"></translation>
+        <translation>Lyd</translation>
     </message>
     <message>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle</translation>
     </message>
     <message>
         <source>Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Billede</translation>
     </message>
     <message>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekst</translation>
     </message>
     <message>
         <source>Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Video</translation>
     </message>
     <message>
         <source>Limit search to:</source>
-        <translation type="unfinished"></translation>
+        <translation>Begræns søgningen til:</translation>
     </message>
     <message>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Tilføj</translation>
     </message>
     <message>
         <source>Always check this medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Tjek altid dette medie</translation>
+    </message>
+    <message>
+        <source>Configure this medium. The changes will go into effect when the application gets closed. To update the Document Tab you need to remove and insert this medium.</source>
+        <translation>Opsætning af dette medie. Ændringerne træder i kræft når programmet bliver lukket. For at opdatere Dokument-fanen skal du fjerne og isætte mediet igen.</translation>
     </message>
 </context>
 </TS>
