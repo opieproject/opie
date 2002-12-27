2f240b91342fde5aac9332c4e23103c0bb85823a
Carsten Niehaus <cniehaus@handhelds.org>
2002-12-27 08:24:37 +0000

    add 2 files

---
+++ i18n/zh_TW/libcipboardapplet.ts
@@ -0,0 +1,17 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>ClipboardApplet</name>
+    <message>
+        <source>Cut</source>
+        <translation>剪下</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation >復製</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation >貼上</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/zh_TW/sshkey.ts
@@ -0,0 +1,40 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>SSHKeysApp</name>
+    <message>
+        <source>Error running ssh-add</source>
+        <translation >錯誤 running ssh-add</translation>
+    </message>
+</context>
+<context>
+    <name>SSHKeysBase</name>
+    <message>
+        <source>SSH Keys</source>
+        <translation>SSH Keys</translation>
+    </message>
+    <message>
+        <source>Add Keys:</source>
+        <translation>新增 Keys:</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation >更新</translation>
+    </message>
+    <message>
+        <source>Clear Keys</source>
+        <translation >清除 Keys</translation>
+    </message>
+    <message>
+        <source>Key Name</source>
+        <translation>Key Name</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation >大小</translation>
+    </message>
+    <message>
+        <source>Fingerprint</source>
+        <translation >Fingerprint</translation>
+    </message>
+</context>
+</TS>
