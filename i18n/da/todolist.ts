b04a43d4e6ec48ae85ee2152292fa1a917939ecf
Tais P. Hansen <tais.hansen@osd.dk>
2003-01-01 04:37:22 +0000

    Alot of danish translations.

--- i18n/da/todolist.ts
+++ i18n/da/todolist.ts
@@ -3,63 +3,63 @@
     <name>MainWindow</name>
     <message>
         <source>New Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Ny opgave</translation>
     </message>
     <message>
         <source>Edit Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Rediger opgave</translation>
     </message>
     <message>
         <source>View Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis opgave</translation>
     </message>
     <message>
         <source>Delete...</source>
-        <translation type="unfinished"></translation>
+        <translation>Slet...</translation>
     </message>
     <message>
         <source>Delete all...</source>
-        <translation type="unfinished"></translation>
+        <translation>Slet alle...</translation>
     </message>
     <message>
         <source>Delete completed</source>
-        <translation type="unfinished"></translation>
+        <translation>Slet afsluttede</translation>
     </message>
     <message>
         <source>Duplicate</source>
-        <translation type="unfinished"></translation>
+        <translation>Duplikere</translation>
     </message>
     <message>
         <source>Beam</source>
-        <translation type="unfinished"></translation>
+        <translation>Send</translation>
     </message>
     <message>
         <source>Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Find</translation>
     </message>
     <message>
         <source>Data</source>
-        <translation type="unfinished"></translation>
+        <translation>Data</translation>
     </message>
     <message>
         <source>Category</source>
-        <translation type="unfinished"></translation>
+        <translation>Kategori</translation>
     </message>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Valgmuligheder</translation>
     </message>
     <message>
         <source>New from template</source>
-        <translation type="unfinished"></translation>
+        <translation>Ny fra skabelon</translation>
     </message>
     <message>
         <source>All Categories</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle kategorier</translation>
     </message>
     <message>
         <source>Out of space</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikke mere plads</translation>
     </message>
     <message>
         <source>Todo was unable
@@ -68,71 +68,76 @@ Free up some space
 and try again.
 
 Quit Anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>Gøremål kunne ikke
+gemme dine ændringer.
+Frigør noget lagerplads
+og prøv igen.
+
+Afslut alligevel?</translation>
     </message>
     <message>
         <source>Todo</source>
-        <translation type="unfinished"></translation>
+        <translation>Gøremål</translation>
     </message>
     <message>
         <source>Can not edit data, currently syncing</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke redigere data da en synkronisering er igang</translation>
     </message>
     <message>
         <source>all tasks?</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle opgaver?</translation>
     </message>
     <message>
         <source>all completed tasks?</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle afsluttede opgaver?</translation>
     </message>
     <message>
         <source>Unfiled</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikke-arkiverede</translation>
     </message>
     <message>
         <source>&lt;P&gt;%1 new tasks arrived.&lt;p&gt;Would you like to add them to your Todolist?</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;P&gt;%1 nye opgaver er modtaget.&lt;p&gt;Vil du tilføje dem til din opgaveliste?</translation>
     </message>
     <message>
         <source>New Tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Nye opgaver</translation>
     </message>
     <message>
         <source>Show completed tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis afsluttede opgaver</translation>
     </message>
     <message>
         <source>Show only over-due tasks</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis kun forfaldne opgaver</translation>
     </message>
     <message>
         <source>Show task deadlines</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis opgave deadlines</translation>
     </message>
     <message>
         <source>Show quick task bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis genvejspanel</translation>
     </message>
     <message>
         <source>QuickEdit</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Click here to create a new task.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at oprette en ny opgave.</translation>
     </message>
     <message>
         <source>Click here to modify the current task.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at redigere den valgte opgave.</translation>
     </message>
     <message>
         <source>Click here to remove the current task.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at fjerne den valgte opgave.</translation>
     </message>
     <message>
         <source>Click here to send the current task to another device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at sende den valgte opgave til en anden enhed.</translation>
     </message>
     <message>
         <source>This is a listing of all current tasks.
@@ -142,190 +147,196 @@ The list displays the following information:
 2. Priority - a graphical representation of task priority.  Double-click here to modify.
 3. Description - description of task.  Click here to select the task.
 4. Deadline - shows when task is due.  This column can be shown or hidden by selecting Options-&gt;&apos;Show task deadlines&apos; from the menu above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dette er en liste over alle nuværende opgaver.
+
+Listen viser følgende information:
+1. Afsluttet - Et grønt hak indikerer at opgaven er afsluttet. Klik her for at afslutte en opgave.
+2. Prioritet - En grafisk repræsentation af opgavens prioritet. Dobbelt-klik her for at ændre.
+3. Beskrivelse - Beskrivelse af opgaven. Klik her for at vælge opgaven.
+4. Deadline - Viser hvornår opgaven skal være afsluttet. Denne kolonne kan blive vist eller skjules ved at vælge Valgmuligheder-&gt;&apos;Vis opgave deadlines&apos; fra menuen ovenover.</translation>
     </message>
 </context>
 <context>
     <name>OTaskEditor</name>
     <message>
         <source>Overview</source>
-        <translation type="unfinished"></translation>
+        <translation>Oversigt</translation>
     </message>
     <message>
         <source>Advanced</source>
-        <translation type="unfinished"></translation>
+        <translation>Advanceret</translation>
     </message>
     <message>
         <source>Alarms</source>
-        <translation type="unfinished"></translation>
+        <translation>Alarmer</translation>
     </message>
     <message>
         <source>Reminders</source>
-        <translation type="unfinished"></translation>
+        <translation>Påmindelser</translation>
     </message>
     <message>
         <source>X-Ref</source>
-        <translation type="unfinished"></translation>
+        <translation>X-Ref</translation>
     </message>
     <message>
         <source>Recurrance</source>
-        <translation type="unfinished"></translation>
+        <translation>Gentagelse</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Enter Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Indtast opgave</translation>
     </message>
     <message>
         <source>Edit Task</source>
-        <translation type="unfinished"></translation>
+        <translation>Rediger opgave</translation>
     </message>
 </context>
 <context>
     <name>TableView</name>
     <message>
         <source>C.</source>
-        <translation type="unfinished"></translation>
+        <translation>C.</translation>
     </message>
     <message>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beskrivelse</translation>
     </message>
     <message>
         <source>Deadline</source>
-        <translation type="unfinished"></translation>
+        <translation>Deadline</translation>
     </message>
     <message>
         <source>Table View</source>
-        <translation type="unfinished"></translation>
+        <translation>Tabel visning</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen</translation>
     </message>
     <message>
         <source>%1 day(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 dag(e)</translation>
     </message>
     <message>
         <source>Priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Prioritet</translation>
     </message>
 </context>
 <context>
     <name>TaskEditorAdvanced</name>
     <message>
         <source>State:</source>
-        <translation type="unfinished"></translation>
+        <translation>Status:</translation>
     </message>
     <message>
         <source>Started</source>
-        <translation type="unfinished"></translation>
+        <translation>Påbegyndt</translation>
     </message>
     <message>
         <source>Postponed</source>
-        <translation type="unfinished"></translation>
+        <translation>Udsat</translation>
     </message>
     <message>
         <source>Finished</source>
-        <translation type="unfinished"></translation>
+        <translation>Afsluttet</translation>
     </message>
     <message>
         <source>Not started</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikke påbegyndt</translation>
     </message>
     <message>
         <source>Maintainer</source>
-        <translation type="unfinished"></translation>
+        <translation>Vedligeholder</translation>
     </message>
     <message>
         <source>test</source>
-        <translation type="unfinished"></translation>
+        <translation>test</translation>
     </message>
     <message>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <source>Maintain Mode:</source>
-        <translation type="unfinished"></translation>
+        <translation>Vedligeholdelses mode:</translation>
     </message>
     <message>
         <source>Nothing</source>
-        <translation type="unfinished"></translation>
+        <translation>Intet</translation>
     </message>
     <message>
         <source>Responsible</source>
-        <translation type="unfinished"></translation>
+        <translation>Ansvarlig</translation>
     </message>
     <message>
         <source>Done By</source>
-        <translation type="unfinished"></translation>
+        <translation>Afsluttet af</translation>
     </message>
     <message>
         <source>Coordinating</source>
-        <translation type="unfinished"></translation>
+        <translation>Tilrettelæggelse</translation>
     </message>
 </context>
 <context>
     <name>TaskEditorAdvancedImpl</name>
     <message>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beskrivelse</translation>
     </message>
 </context>
 <context>
     <name>TemplateDialog</name>
     <message>
         <source>Template Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Skabelon redigering</translation>
     </message>
     <message>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Tilføj</translation>
     </message>
     <message>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Rediger</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Fjern</translation>
     </message>
 </context>
 <context>
     <name>TemplateDialogImpl</name>
     <message>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Navn</translation>
     </message>
     <message>
         <source>New Template %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ny skabelon %1</translation>
     </message>
 </context>
 <context>
     <name>TemplateEditor</name>
     <message>
         <source>Configure Templates</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfigurer skabeloner</translation>
     </message>
 </context>
 <context>
     <name>Todo</name>
     <message>
         <source>More</source>
-        <translation type="unfinished"></translation>
+        <translation>Mere</translation>
     </message>
     <message>
         <source>Enter</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuller</translation>
     </message>
     <message>
         <source>Click here to set the priority of new task.
@@ -333,7 +344,11 @@ The list displays the following information:
 This area is called the quick task bar.
 
 It allows you to quickly add a new task to your list.  This area can be shown or hidden by selecting Options-&gt;&apos;Show quick task bar&apos; from the menu above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at bestemme prioriteten på den nye opgave.
+
+Dette område kaldes genvejspanelet.
+
+Det hjælper dig med hurtigt at tilføje en ny opgave til din liste. Dette område kan blive vist eller skjules ved at vælge Valgmuligheder-&gt;&apos;Vis genvejspanel&apos; fra menuen foroven.</translation>
     </message>
     <message>
         <source>Enter description of new task here.
@@ -341,7 +356,11 @@ It allows you to quickly add a new task to your list.  This area can be shown or
 This area is called the quick task bar.
 
 It allows you to quickly add a new task to your list.  This area can be shown or hidden by selecting Options-&gt;&apos;Show quick task bar&apos; from the menu above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Indtast beskrivelsen af den nye opgave her.
+
+Dette område kaldes genvejspanelet.
+
+Det hjælper dig med hurtigt at tilføje en ny opgave til din liste. Dette område kan blive vist eller skjules ved at vælge Valgmuligheder-&gt;&apos;Vis genvejspanel&apos; fra menuen foroven.</translation>
     </message>
     <message>
         <source>Click here to enter additional information for new task.
@@ -349,7 +368,11 @@ It allows you to quickly add a new task to your list.  This area can be shown or
 This area is called the quick task bar.
 
 It allows you to quickly add a new task to your list.  This area can be shown or hidden by selecting Options-&gt;&apos;Show quick task bar&apos; from the menu above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at indtaste yderligere information til den nye opgave.
+
+Dette område kaldes genvejspanelet.
+
+Det hjælper dig med hurtigt at tilføje en ny opgave til din liste. Dette område kan blive vist eller skjules ved at vælge Valgmuligheder-&gt;&apos;Vis genvejspanel&apos; fra menuen foroven.</translation>
     </message>
     <message>
         <source>Click here to add new task.
@@ -357,7 +380,11 @@ It allows you to quickly add a new task to your list.  This area can be shown or
 This area is called the quick task bar.
 
 It allows you to quickly add a new task to your list.  This area can be shown or hidden by selecting Options-&gt;&apos;Show quick task bar&apos; from the menu above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at tilføje en ny opgave.
+
+Dette område kaldes genvejspanelet.
+
+Det hjælper dig med hurtigt at tilføje en ny opgave til din liste. Dette område kan blive vist eller skjules ved at vælge Valgmuligheder-&gt;&apos;Vis genvejspanel&apos; fra menuen foroven.</translation>
     </message>
     <message>
         <source>Click here to reset new task information.
@@ -365,7 +392,11 @@ It allows you to quickly add a new task to your list.  This area can be shown or
 This area is called the quick task bar.
 
 It allows you to quickly add a new task to your list.  This area can be shown or hidden by selecting Options-&gt;&apos;Show quick task bar&apos; from the menu above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik her for at nulstille informationen i den nye opgave.
+
+Dette område kaldes genvejspanelet.
+
+Det hjælper dig med hurtigt at tilføje en ny opgave til din liste. Dette område kan blive vist eller skjules ved at vælge Valgmuligheder-&gt;&apos;Vis genvejspanel&apos; fra menuen foroven.</translation>
     </message>
 </context>
 </TS>
