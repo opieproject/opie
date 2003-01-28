4c52bd9dd99e3958b29f413f6e97161123ab773b
Oliver Fels <groucho@handhelds.org>
2003-01-28 08:36:56 +0000

    Dozens of translation files missing- added

---
+++ i18n/de/libtodayaddressbookplugin.ts
@@ -0,0 +1,95 @@
+<!DOCTYPE TS><TS>
+<defaultcodec>iso8859-1</defaultcodec>
+<context>
+    <name>AddressBookPluginConfig</name>
+    <message>
+        <source>Max Lines: </source>
+        <translation>Anzahl. Einträge: </translation>
+    </message>
+    <message>
+        <source>Set the maximum number of lines that should be shown for each anniversaries/birthdays</source>
+        <translation>Setzt die maximale Anzahl an Einträgen imgegeben Zeitraum</translation>
+    </message>
+    <message>
+        <source>Clip line after X chars: </source>
+        <translation>Zeile abschneiden nach N Einträgen: </translation>
+    </message>
+    <message>
+        <source>After how many chars should be the info about the task be cut off</source>
+        <translation>Nach wie vielen Zeichen soll Zeiel abgeschnitten werden</translation>
+    </message>
+    <message>
+        <source>Days look ahead: </source>
+        <translation>Vorausschai in Tagen :</translation>
+    </message>
+    <message>
+        <source>How many days we should search forward</source>
+        <translation>Wieviele Tage soll vorau geschaut werden</translation>
+    </message>
+    <message>
+        <source>To activate settings: Restart application !</source>
+        <translation>Einstellungen aktivieren: Anwendung neu starten !</translation>
+    </message>
+    <message>
+        <source>Set Headline Color: </source>
+        <translation>Überschriftenfarbe: </translation>
+    </message>
+    <message>
+        <source>Colors for the headlines !</source>
+        <translation>Farbe der Überschrift !</translation>
+    </message>
+    <message>
+        <source>Set Entry Color: </source>
+        <translation>Einstragsfarbe: </translation>
+    </message>
+    <message>
+        <source>This color will be used for shown birthdays/anniversaries !</source>
+        <translation>Die Farbe des Eintrags selbs !</translation>
+    </message>
+    <message>
+        <source>Set Urgent
+Color if below </source>
+        <translation>Setzt die Farbe für
+fällige Termine</translation>
+    </message>
+    <message>
+        <source> days: </source>
+        <translation>Tage: </translation>
+    </message>
+    <message>
+        <source>This urgent color will be used if we are close to the event !</source>
+        <translation>Diese Farbe wird benutzt wenn der Termin kurz bevorsteht !</translation>
+    </message>
+    <message>
+        <source>The urgent color will be used if the birthday/anniversary is closer than given days !</source>
+        <translation>Diese Farbe wird benutzt wenn der Termin näher ist als der eingestellte Zeitraum !</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>AddressBook plugin</source>
+        <translation>Addressbuch-Plugin</translation>
+    </message>
+    <message>
+        <source>Next birthdays in &lt;b&gt; %1 &lt;/b&gt; days:</source>
+        <translation>Nächste Geburtstage &lt;b&gt;%1&lt;/b&gt; Tagen: </translation>
+    </message>
+    <message>
+        <source>No birthdays in &lt;b&gt; %1 &lt;/b&gt; days!</source>
+        <translation>Keine Geburtstage in  &lt;b&gt;%1&lt;/b&gt; Tagen!</translation>
+    </message>
+    <message>
+        <source>Next anniversaries in &lt;b&gt; %1 &lt;/b&gt; days:</source>
+        <translation>Nächste Jahrestage in &lt;b&gt;%1&lt;/b&gt; Tagen: </translation>
+    </message>
+    <message>
+        <source>No anniversaries in &lt;b&gt; %1 &lt;/b&gt; days!</source>
+        <translation>Keine Jahrestage &lt;b&gt;%1&lt;/b&gt; Tagen !</translation>
+    </message>
+    <message>
+        <source>Database does not provide this search query ! Please upgrade libOpie !&lt;br&gt;</source>
+        <translation>Die Datenbank kann die Anfrage nicht durchführen. Vermutlich muss das libopie-Paket upgegradet werden ! &lt;br&gt;</translation>
+    </message>
+</context>
+</TS>
