5c6a21bf3ff393ecf69f93a5eeeb71570c1deb0b
Maximilian Reiß <max.reiss@gmx.de>
2003-03-26 22:30:46 +0000

    ups

---
+++ noncore/net/opietooth/lib/startdunconnection.h
@@ -0,0 +1,40 @@
+#ifndef startdunconnection_h
+#define startdunconnection_h
+
+#include <qobject.h>
+#include "connection.h"
+#include <opie/oprocess.h>
+
+namespace OpieTooth {
+
+    class StartDunConnection : StartConnection {
+
+	Q_OBJECT
+
+    public:
+	StartDunConnection();
+	StartDunConnection( QString mac );
+	~StartDunConnection();
+
+	QString name();
+	void setName( QString name );
+	StartConnection::ConnectionType type();
+	void setConnectionType( );
+	void start();
+	void stop();
+
+    private:
+	QString m_name;
+	QString m_mac;
+	ConnectionType m_connectionType;
+	OProcess* m_dunConnect;
+	
+    private slots:
+	void slotExited( OProcess* proc );
+                void slotStdOut( OProcess* proc, char* chars, int len );
+    };
+
+ 
+}
+
+#endif
---
+++ noncore/net/opietooth/manager/dunpopup.h
@@ -0,0 +1,33 @@
+#ifndef DUNPOPUP_H
+#define DUNPOPUP_H
+
+#include <qpopupmenu.h>
+#include <qaction.h>
+
+#include <startdunconnection.h>
+
+#include "btdeviceitem.h"
+
+
+namespace OpieTooth {
+
+    class DunPopup : public QPopupMenu {
+
+	Q_OBJECT
+
+    public:
+        DunPopup( OpieTooth::BTDeviceItem* );
+        ~DunPopup();
+
+    private:
+        QAction* m_push;
+	OpieTooth::StartDunConnection* m_dunconnection;
+	OpieTooth::BTDeviceItem *m_item;
+    private slots:
+        void slotConnect();
+        void slotDisconnect();
+        void slotConnectAndConfig();
+   };
+};
+
+#endif
