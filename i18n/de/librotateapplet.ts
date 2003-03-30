6b1fef601995d3b9776e32af9d56750d26e6f85a
Michael Lauer <mickey@vanille-media.de>
2003-03-30 18:30:39 +0000

    enable translation of rotateapplet. carsten: will you add the rest of the .ts files?

---
+++ i18n/de/librotateapplet.ts
@@ -0,0 +1,14 @@
+<!DOCTYPE TS><TS>
+<defaultcodec>iso8859-1</defaultcodec>
+<context>
+    <name>RotateApplet</name>
+    <message>
+        <source>Rotate shortcut</source>
+        <translation>Rotieren Abkürzung</translation>
+    </message>
+    <message>
+        <source>Rotate</source>
+        <translation>Rotieren</translation>
+    </message>
+</context>
+</TS>
