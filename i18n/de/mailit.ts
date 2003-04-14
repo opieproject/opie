3d78e54cb6711080771232af4b7294b16021550c
Oliver Fels <groucho@handhelds.org>
2003-04-14 09:08:49 +0000

    - inlinepics does not seem to be current in CVS - Added mailit translation

---
+++ i18n/de/mailit.ts
@@ -0,0 +1,317 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>AddAtt</name>
+    <message>
+        <source>Attached</source>
+        <translation>Angehängt</translation>
+    </message>
+    <message>
+        <source>File type</source>
+        <translation>Dateiart</translation>
+    </message>
+    <message>
+        <source>Attatch...</source>
+        <translation>Hinzufügen...</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+</context>
+<context>
+    <name>EditAccount</name>
+    <message>
+        <source>Edit Account</source>
+        <translation>Account bearbeiten</translation>
+    </message>
+    <message>
+        <source>Create new Account</source>
+        <translation>Account erstellen</translation>
+    </message>
+    <message>
+        <source>Account name</source>
+        <translation>Accountname</translation>
+    </message>
+    <message>
+        <source>Your name</source>
+        <translation>Ihr Name</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>POP username</source>
+        <translation>POP Benutzername</translation>
+    </message>
+    <message>
+        <source>POP password</source>
+        <translation>POP Passwort</translation>
+    </message>
+    <message>
+        <source>POP server</source>
+        <translation>POP Server</translation>
+    </message>
+    <message>
+        <source>SMTP server</source>
+        <translation>SMTP Server</translation>
+    </message>
+    <message>
+        <source>Synchronize</source>
+        <translation>Abgleichen</translation>
+    </message>
+    <message>
+        <source>Mail Size (k)</source>
+        <translation>Mailgröße (kB)</translation>
+    </message>
+</context>
+<context>
+    <name>EmailClient</name>
+    <message>
+        <source>Idle</source>
+        <translation>Untätig</translation>
+    </message>
+    <message>
+        <source>&amp;Mail</source>
+        <translation>&amp;Mail</translation>
+    </message>
+    <message>
+        <source>Accounts</source>
+        <translation>Accounts</translation>
+    </message>
+    <message>
+        <source>Get Mail in</source>
+        <translation>Nachricht holen aus </translation>
+    </message>
+    <message>
+        <source>Edit account</source>
+        <translation>Account bearbeiten</translation>
+    </message>
+    <message>
+        <source>Delete account</source>
+        <translation>Account löschen</translation>
+    </message>
+    <message>
+        <source>Get all mail</source>
+        <translation>Alle Mails holen</translation>
+    </message>
+    <message>
+        <source>Send mail</source>
+        <translation>Mail versenden</translation>
+    </message>
+    <message>
+        <source>Compose</source>
+        <translation>Erstellen</translation>
+    </message>
+    <message>
+        <source>Cancel transfer</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>From</source>
+        <translation>Von</translation>
+    </message>
+    <message>
+        <source>Subject</source>
+        <translation>Betreff</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <source>Inbox</source>
+        <translation>Eingang</translation>
+    </message>
+    <message>
+        <source>To</source>
+        <translation>An</translation>
+    </message>
+    <message>
+        <source>Outbox</source>
+        <translation>Ausgang</translation>
+    </message>
+    <message>
+        <source>No account selected</source>
+        <translation>Kein Accounbt ausgewählt</translation>
+    </message>
+    <message>
+        <source>You must create an account</source>
+        <translation>Sie müssen einen Account erstellen ...</translation>
+    </message>
+</context>
+<context>
+    <name>EmailHandler</name>
+    <message>
+        <source>Could not locate all files in 
+mail with subject: </source>
+        <translation>Es konnten nicht alle Dateien
+der Mail gefunden werden mit dem Betreff: </translation>
+    </message>
+    <message>
+        <source>
+Mail has NOT been sent</source>
+        <translation>Die Mail wurde *nicht* gesendet</translation>
+    </message>
+    <message>
+        <source>Attachment error</source>
+        <translation>Attachementfehler</translation>
+    </message>
+    <message>
+        <source>OK
+</source>
+        <translation>OK
+</translation>
+    </message>
+</context>
+<context>
+    <name>MailItWindow</name>
+    <message>
+        <source>Write mail</source>
+        <translation>Mail schreiben</translation>
+    </message>
+    <message>
+        <source>Examine mail</source>
+        <translation>Mail prüfen</translation>
+    </message>
+</context>
+<context>
+    <name>PopClient</name>
+    <message>
+        <source>Connection established</source>
+        <translation>Verbindung hergestellt</translation>
+    </message>
+    <message>
+        <source>Error Occured</source>
+        <translation>Fehler aufgetreten</translation>
+    </message>
+    <message>
+        <source>Retrieving </source>
+        <translation>Hole Mail</translation>
+    </message>
+    <message>
+        <source>Previous message </source>
+        <translation>Vorherige Nachricht</translation>
+    </message>
+    <message>
+        <source>Completing message </source>
+        <translation>Nachricht vervollständigen</translation>
+    </message>
+    <message>
+        <source>No new Messages</source>
+        <translation>Keine neuen Nachrichten</translation>
+    </message>
+    <message>
+        <source> new messages</source>
+        <translation>Neue Nachrchten</translation>
+    </message>
+    <message>
+        <source>No new messages</source>
+        <translation>Keine neuen Nachrichten</translation>
+    </message>
+</context>
+<context>
+    <name>ReadMail</name>
+    <message>
+        <source>&amp;View</source>
+        <translation>&amp;Ansehen</translation>
+    </message>
+    <message>
+        <source>&amp;Mail</source>
+        <translation>&amp;Mail</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Antworten</translation>
+    </message>
+    <message>
+        <source>Previous</source>
+        <translation>Vorherige</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Nächste</translation>
+    </message>
+    <message>
+        <source>Attatchments</source>
+        <translation>Attachments</translation>
+    </message>
+    <message>
+        <source>Text Format</source>
+        <translation>Textformat</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+</context>
+<context>
+    <name>SmtpClient</name>
+    <message>
+        <source>DNS lookup</source>
+        <translation>DNS-Auflösung</translation>
+    </message>
+    <message>
+        <source>Connection established</source>
+        <translation>Verbindung hergestellt</translation>
+    </message>
+    <message>
+        <source>Sending: </source>
+        <translation>Sende: </translation>
+    </message>
+    <message>
+        <source>Sent </source>
+        <translation>Gesendet</translation>
+    </message>
+    <message>
+        <source> messages</source>
+        <translation>Nachrichten</translation>
+    </message>
+</context>
+<context>
+    <name>ViewAtt</name>
+    <message>
+        <source>Install</source>
+        <translation>Installieren</translation>
+    </message>
+</context>
+<context>
+    <name>WriteMail</name>
+    <message>
+        <source>&amp;Mail</source>
+        <translation>&amp;Mail</translation>
+    </message>
+    <message>
+        <source>&amp;Add</source>
+        <translation>&amp;Hinzu</translation>
+    </message>
+    <message>
+        <source>Attatchment</source>
+        <translation>Attchment</translation>
+    </message>
+    <message>
+        <source>Enque mail</source>
+        <translation>Mail queuen</translation>
+    </message>
+    <message>
+        <source>New mail</source>
+        <translation>Neue Mail</translation>
+    </message>
+    <message>
+        <source>To:</source>
+        <translation>An: </translation>
+    </message>
+    <message>
+        <source>CC:</source>
+        <translation>Kopie: </translation>
+    </message>
+    <message>
+        <source>Subject:</source>
+        <translation>Betreff: </translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+</TS>
