1a185431c8dd09215c34a048e7eed06dac31042f
Carsten Niehaus <cniehaus@handhelds.org>
2003-04-20 09:26:31 +0000

    first files

---
+++ i18n/nl/libqkeyboard.ts
@@ -0,0 +1,9 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>InputMethods</name>
+    <message>
+        <source>Keyboard</source>
+        <translation>Toetsenbord</translation>
+    </message>
+</context>
+</TS>
