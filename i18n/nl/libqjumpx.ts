f966fa15b8957bd20c94b1366ec1568723e7b067
Ruben Lubbes <sikko@handhelds.org>
2003-04-22 18:31:52 +0000

    *** empty log message ***

--- i18n/nl/libqjumpx.ts
+++ i18n/nl/libqjumpx.ts
@@ -3,7 +3,7 @@
     <name>InputMethods</name>
     <message>
         <source>JumpX</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">JumpX</translation>
     </message>
 </context>
 </TS>
