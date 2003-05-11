d283e4ff9df8d5328ebc660a6fc37cb0b156b1a0
Carsten Niehaus <cniehaus@handhelds.org>
2003-05-11 12:54:46 +0000

    opie-i18n-hu is back

---
+++ i18n/hu/libflat.ts
@@ -0,0 +1,9 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Decoration</name>
+    <message>
+        <source>Flat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/hu/libkeyview.ts
@@ -0,0 +1,9 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Keyview</name>
+    <message>
+        <source>Keyview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
