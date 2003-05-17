cdfce248aa94d58828ac604716f4fb6305efeff0
Carsten Niehaus <cniehaus@handhelds.org>
2003-05-17 17:49:40 +0000

    first .no patches

--- i18n/no/appearance.ts
+++ i18n/no/appearance.ts
@@ -5,103 +5,113 @@
         <source>Styles control the way items such as buttons and scroll bars appear in all applications.
 
 Click here to select an available style.</source>
-        <translation type="unfinished"></translation>
+        <translation>Stiler kontroller måten objekter som knapper og rullefelt vises på i alle applikasjoner
+
+Klikk her for å velge en av de tilgjengelige stilene.</translation>
     </message>
     <message>
         <source>Settings...</source>
-        <translation type="unfinished"></translation>
+        <translation>Innstillinger...</translation>
     </message>
     <message>
         <source>Click here to configure the currently selected style.
 
 Note:  This option is not available for all styles.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å konfigurere den valgte stilen.
+
+Merk: Dette valget er ikke tilgjengelig for alle stiler.</translation>
     </message>
     <message>
         <source>Window decorations control the way the application title bar and its buttons appear.
 
 Click here to select an available decoration.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vindusdekorer kontroller måten applikasjoners tittellinje og knapper vises.
+
+Klikk her for å velge en tilgjengelig dekor.</translation>
     </message>
     <message>
         <source>Select the desired name, style and size of the default font applications will use.</source>
-        <translation type="unfinished"></translation>
+        <translation>Velg ønsket navn, stil og størrelse på standard tegnsett som applikasjonene vil bruke.</translation>
     </message>
     <message>
         <source>Color schemes are a collection of colors which are used for various parts of the display.
 
 Click here to select an available scheme.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fargesystemer er en samling av farger som blir brukt av forskjellige deler av visningen.
+
+Klikk her for å velge et tilgjengelig fargesystem.</translation>
     </message>
     <message>
         <source>Current scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Nåværende fargesystem</translation>
     </message>
     <message>
         <source>Edit...</source>
-        <translation type="unfinished"></translation>
+        <translation>Rediger...</translation>
     </message>
     <message>
         <source>Click here to change the colors in the current color scheme.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å endre farger i nåværende fargesystem.</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Slett</translation>
     </message>
     <message>
         <source>Click here to delete the color scheme selected in the list to the left.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å slette valgte fargesystem i listen til venstre.</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Lagre</translation>
     </message>
     <message>
         <source>Click here to name and save the current color scheme.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her å gi det nåværende fargesystemet et navn og lagre den.</translation>
     </message>
     <message>
         <source>Force styling for all applications.</source>
-        <translation type="unfinished"></translation>
+        <translation>Påfør stilen på alle applikasjoner.</translation>
     </message>
     <message>
         <source>Click here to allow all applications to use global appearance settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å tillate alle applikasjoner å bruke globale visningsinnstillinger.</translation>
     </message>
     <message>
         <source>Disable styling for these applications ( &lt;b&gt;*&lt;/b&gt; can be used as a wildcard):</source>
-        <translation type="unfinished"></translation>
+        <translation>Slå av stil for disse applikasjoner (&lt;b&gt;*&lt;/b&gt; kan bli brukt som wildcard):</translation>
     </message>
     <message>
         <source>If some applications do not display correctly with the global appearance settings, certain features can be turned off for that application.
 
 This area allows you to select an application and which settings you wish to disable.</source>
-        <translation type="unfinished"></translation>
+        <translation>Om noen applikasjoner ikke vises korrekt med globale visningsinnstillinger, kan visse funksjoner skues av for disse applikasjonene.
+
+Dette området tillater deg å velge en applikasjon og hvilke innstillinger du vil slå av.</translation>
     </message>
     <message>
         <source>Binary file(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Binær fil(er)</translation>
     </message>
     <message>
         <source>Click here to add an application to the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å legge til en applikasjon på listen ovenfor.</translation>
     </message>
     <message>
         <source>Click here to delete the currently selected application.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å slette den nåværende valgte applikasjon.</translation>
     </message>
     <message>
         <source>Click here to move the currently selected application up in the list.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å flytte den nåværende valgte applikasjon opp på listen.</translation>
     </message>
     <message>
         <source>Click here to move the currently selected application down in the list.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å flytte den nåværende valgte applikasjon ned på listen.</translation>
     </message>
     <message>
         <source>Tab style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fane stil:</translation>
     </message>
     <message>
         <source>Click here to select a desired style for tabbed dialogs (such as this application). The styles available are:
@@ -110,161 +120,166 @@ This area allows you to select an application and which settings you wish to dis
 2. Tabs w/icons - tabs with icons for each tab, text label only appears on current tab
 3. Drop down list - a vertical listing of tabs
 4. Drop down list w/icons - a vertical listing of tabs with icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å velge ønsket stil for fanebaserte dialoger (som denne applikasjonen). Tilgjengelige stiler er:
+
+1. Faner - normale faner med bare tekst
+2. Faner m/ikoner - faner med ikoner for hver fane og hvor tekst kun vises på aktiv fane
+3. Rullegardinmeny - en vertikal liste av faner
+4. Rullegardinmeny m/ikoner - en vertikal liste av faner med ikoner </translation>
     </message>
     <message>
         <source>Tabs</source>
-        <translation type="unfinished"></translation>
+        <translation>Faner</translation>
     </message>
     <message>
         <source>Tabs w/icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Faner m/ikoner</translation>
     </message>
     <message>
         <source>Drop down list</source>
-        <translation type="unfinished"></translation>
+        <translation>Rullegardinmeny</translation>
     </message>
     <message>
         <source>Drop down list w/icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Rullegardinmeny m/ikoner</translation>
     </message>
     <message>
         <source>Top</source>
-        <translation type="unfinished"></translation>
+        <translation>Topp</translation>
     </message>
     <message>
         <source>Click here so that tabs appear at the top of the window.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her slik at fanene vises på toppen av vinduet.</translation>
     </message>
     <message>
         <source>Bottom</source>
-        <translation type="unfinished"></translation>
+        <translation>Bunn</translation>
     </message>
     <message>
         <source>Click here so that tabs appear at the bottom of the window.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her slik at fanene vises på bunnen av vinduet.</translation>
     </message>
     <message>
         <source>Rotation direction:</source>
-        <translation type="unfinished"></translation>
+        <translation>Rotasjonsretning:</translation>
     </message>
     <message>
         <source>Appearance Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Visningsinnstillinger</translation>
     </message>
     <message>
         <source>This is a preview window.  Look here to see your new appearance as options are changed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dette er et forhåndsvisningsvindu. Kikk her for å se din nye visning da innstillingene er endret.</translation>
     </message>
     <message>
         <source>Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Stil</translation>
     </message>
     <message>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Tegnsett</translation>
     </message>
     <message>
         <source>Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Farger</translation>
     </message>
     <message>
         <source>Windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Vinduer</translation>
     </message>
     <message>
         <source>Advanced</source>
-        <translation type="unfinished"></translation>
+        <translation>Avansert</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Omstart</translation>
     </message>
     <message>
         <source>Do you want to restart %1 now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vil du starte om %1 nå?</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nei</translation>
     </message>
     <message>
         <source>Save Scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Lagre system</translation>
     </message>
     <message>
         <source>Save scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Lagre system</translation>
     </message>
     <message>
         <source>Scheme does already exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>Systemet eksisterer allerede.</translation>
     </message>
     <message>
         <source>Delete scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Slett system</translation>
     </message>
     <message>
         <source>Unable to delete current scheme.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke slette nåværende system.</translation>
     </message>
     <message>
         <source>&lt;new&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;ny&gt;</translation>
     </message>
 </context>
 <context>
     <name>EditScheme</name>
     <message>
         <source>Edit scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Rediger system</translation>
     </message>
     <message>
         <source>Click here to select a color for: </source>
-        <translation type="unfinished"></translation>
+        <translation>Klikk her for å velge farge for:</translation>
     </message>
 </context>
 <context>
     <name>SampleWindow</name>
     <message>
         <source>Sample</source>
-        <translation type="unfinished"></translation>
+        <translation>Prøve</translation>
     </message>
     <message>
         <source>Normal Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Normalt objekt</translation>
     </message>
     <message>
         <source>Disabled Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Avslått objekt</translation>
     </message>
     <message>
         <source>Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Meny</translation>
     </message>
     <message>
         <source>Normal Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Normal Tekst</translation>
     </message>
     <message>
         <source>Highlighted Text</source>
-        <translation type="unfinished"></translation>
+        <translation>Markert Tekst</translation>
     </message>
     <message>
         <source>Button</source>
-        <translation type="unfinished"></translation>
+        <translation>Knapp</translation>
     </message>
     <message>
         <source>Check Box</source>
-        <translation type="unfinished"></translation>
+        <translation>Avkrysningsboks</translation>
     </message>
     <message>
         <source>Sample window using the selected settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Prøvevindu som viser valgte innstillinger.</translation>
     </message>
 </context>
 </TS>
