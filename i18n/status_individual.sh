c09d20eb4c42a33c1b664107cb65582db3d98d79
Carsten Niehaus <cniehaus@handhelds.org>
2003-05-18 11:50:32 +0000

    from now on you can also use status_individual.sh todo XX. This will only show you the files which are not completly translated

--- i18n/status_individual.sh
+++ i18n/status_individual.sh
@@ -9,5 +9,14 @@ if [ "x${arg}x" = "xx" ]; then
 	exit
 fi
 
+if [ "${arg}" = "todo" ]; then
+	if [ "x$2"x = "xx" ]; then
+		echo "missing second argument"
+		exit
+	fi
+	for i in $2/*.ts; do echo $i - `grep translation "$i" | grep type=\"unfinished\" | wc -l` | grep -v ' 0' ; done
+	exit
+fi
+
 for i in $1/*.ts; do echo $i - `grep translation "$i" | grep type=\"unfinished\" | wc -l` ; done
 
