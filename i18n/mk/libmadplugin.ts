473dddb695f083b8d06716de07fa14c34f02fdf6
Carsten Niehaus <cniehaus@handhelds.org>
2003-05-30 12:37:22 +0000

    forgot to add these

---
+++ i18n/mk/libmadplugin.ts
@@ -0,0 +1,53 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>LibMadPlugin</name>
+    <message>
+        <source>No Song Open</source>
+        <translation>Нема отворена песна</translation>
+    </message>
+    <message>
+        <source>Info: </source>
+        <translation>Информации:</translation>
+    </message>
+    <message>
+        <source>Name: </source>
+        <translation>Име:</translation>
+    </message>
+    <message>
+        <source>Genre: </source>
+        <translation>Жанр:</translation>
+    </message>
+    <message>
+        <source>Bitrate: </source>
+        <translation>Квалитет:</translation>
+    </message>
+    <message>
+        <source>URL: </source>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Наслов</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Артист</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Албум</translation>
+    </message>
+    <message>
+        <source>Year</source>
+        <translation>Година</translation>
+    </message>
+    <message>
+        <source>Comment</source>
+        <translation>Коментар</translation>
+    </message>
+    <message>
+        <source>, Track: </source>
+        <translation>Песна:</translation>
+    </message>
+</context>
+</TS>
