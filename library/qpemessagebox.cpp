1cd6bf71130224a16a7e25eadc3076e5272e3f06
Holger Freyther <zecke@handhelds.org>
2003-06-02 16:37:36 +0000

    use <qt></qt> for the text this means a new string and opie-lupdate will remove the old translation.. no bug but a feature of -noobsolete
    add a new never called function to do (void)QMessageBox::tr("yes") and no
    to actually be able to translate these labels....

--- library/qpemessagebox.cpp
+++ library/qpemessagebox.cpp
@@ -1,10 +1,17 @@
+#include "stringutil.h"
+
 #include "qpemessagebox.h"
 #include <qmessagebox.h>
+
+static void never_called_tr_function_um_libqpe_ts_etwas_unter_zu_jubeln() {
+(void)QMessageBox::tr("Yes");
+(void)QMessageBox::tr("No");
+}
 
 bool QPEMessageBox::confirmDelete( QWidget *parent, const QString & caption,
 			       const QString & object )
 {
-    QString msg = QObject::tr("Are you sure you want to delete\n %1?").arg( object );
+    QString msg = QObject::tr("<qt>Are you sure you want to delete\n %1?</qt>").arg( Qtopia::escapeString( object ) ); // use <qt> an esacpe String....
     int r = QMessageBox::warning( parent, caption, msg, QMessageBox::Yes,
 				  QMessageBox::No|QMessageBox::Default|
 				  QMessageBox::Escape, 0 );
