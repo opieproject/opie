b943749ec01ac780347631c48bc1557a66a996df
Michael Lauer <mickey@vanille-media.de>
2003-06-07 17:41:51 +0000

    fix c700 vs. SIMpad here also

--- library/qpeapplication.cpp
+++ library/qpeapplication.cpp
@@ -16,7 +16,7 @@
 ** Contact info@trolltech.com if any conditions of this licensing are
 ** not clear to you.
 **
-** $Id: qpeapplication.cpp,v 1.48.2.3 2003-06-06 16:37:10 zecke Exp $
+** $Id: qpeapplication.cpp,v 1.48.2.4 2003-06-07 17:41:51 mickeyl Exp $
 **
 **********************************************************************/
 #define QTOPIA_INTERNAL_LANGLIST
@@ -150,9 +150,9 @@ public:
 
             // ugly hack, remove that later after finding a sane solution
             // Addendum: Only Sharp currently has models with high resolution but (physically) small displays,
-            // so this is only useful if QT_QWS_SHARP is defined. E.g. SIMpad has 800x600 but has
+            // so this is only useful if QT_QWS_SIMPAD is NOT defined. E.g. SIMpad has 800x600 but has
             // a (physically) large enough display to use the small icons
-            #ifdef QT_QWS_SHARP
+            #ifndef QT_QWS_SIMPAD
             if ( QPEApplication::desktop() ->width() >= 600 && ( mw->inherits("QMainWindow") || mw->isA("QMainWindow") ) )  {
                 ( (  QMainWindow* ) mw )->setUsesBigPixmaps( true );
             }
@@ -535,7 +535,7 @@ QPEApplication::QPEApplication( int & argc, char **argv, Type t )
 		AppLnk::setSmallIconSize( 10 );
 		AppLnk::setBigIconSize( 28 );
 	}
-    #ifdef QT_QWS_SHARP
+    #ifndef QT_QWS_SIMPAD
 	else if ( dw > 600 ) {
                                setFont( QFont( "vera", 16 ) );
                                 AppLnk::setSmallIconSize( 24 );
