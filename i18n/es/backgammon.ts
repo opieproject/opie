9fb3f4ed3d8b81e79c7ea537f4ee4b98f7c5a270
Jose Luis Fernandez Barros <fbarros@handhelds.org>
2003-06-15 01:18:00 +0000

    initial translation

--- i18n/es/backgammon.ts
+++ i18n/es/backgammon.ts
@@ -3,164 +3,168 @@
     <name>AI_Dialog</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Configuración</translation>
     </message>
     <message>
         <source>&lt;b&gt;Rescue&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Rescatar&lt;/b&gt;</translation>
     </message>
     <message>
         <source>?</source>
-        <translation type="unfinished"></translation>
+        <translation>?</translation>
     </message>
     <message>
         <source>&lt;b&gt;Eliminate&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Eliminar&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Expose&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Exponer&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Protect&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Proteger&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Safe&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Seguro&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Empty&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Vacío&lt;/b&gt;</translation>
     </message>
     <message>
         <source>Default Values</source>
-        <translation type="unfinished"></translation>
+        <translation>Valores predefinidos</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayuda</translation>
     </message>
     <message>
         <source>Bring the pieces out of the endzone</source>
-        <translation type="unfinished"></translation>
+        <translation>Traer las piezas fuera de la zona final</translation>
     </message>
     <message>
         <source>Eliminate an opponents piece</source>
-        <translation type="unfinished"></translation>
+        <translation>Eliminar una pieza del oponente</translation>
     </message>
     <message>
         <source>Expose you own pieces.
 After such a move only one piece will remain in the slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Exponer sus propias piezas.
+Después de ese movimiento sólo una pieza permanecerá en la casilla</translation>
     </message>
     <message>
         <source>Protect a single piece by
 putting another one in this slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Proteger una sóla pieza
+colocando otra en esta casilla</translation>
     </message>
     <message>
         <source>Move piece to a slot already
 occupied by the player</source>
-        <translation type="unfinished"></translation>
+        <translation>Mover pieza a una casilla ya
+ocupada por el jugador</translation>
     </message>
     <message>
         <source>Move piece to an empty slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Mover pieza a una casilla vacía</translation>
     </message>
     <message>
         <source>Press and hold the ? buttton
 next to a field for help</source>
-        <translation type="unfinished"></translation>
+        <translation>Pulse y mantenga el botón ?
+junto a un campo para obtener ayuda</translation>
     </message>
 </context>
 <context>
     <name>BackGammon</name>
     <message>
         <source>Backgammon</source>
-        <translation type="unfinished"></translation>
+        <translation>Backgammon</translation>
     </message>
     <message>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuevo</translation>
     </message>
     <message>
         <source>Load</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargar</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Eliminar</translation>
     </message>
     <message>
         <source>Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Juego</translation>
     </message>
     <message>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Predefinidos</translation>
     </message>
     <message>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <source>Player</source>
-        <translation type="unfinished"></translation>
+        <translation>Jugador</translation>
     </message>
     <message>
         <source>AI</source>
-        <translation type="unfinished"></translation>
+        <translation>IA</translation>
     </message>
     <message>
         <source>Rules</source>
-        <translation type="unfinished"></translation>
+        <translation>Reglas</translation>
     </message>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opciones</translation>
     </message>
     <message>
         <source>deleted theme %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>¿tema borrado %1?</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>Ok</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancelar</translation>
     </message>
     <message>
         <source>Load Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargar tema</translation>
     </message>
     <message>
         <source>&lt;b&gt;no move&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;sin movimiento&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Player 1 wins. Click on board for new game.&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Ganó el jugador 1. Pulse en el tablero para jugar de nuevo.&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Player 2 wins. Click on board for new game.&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Ganó el jugador 2. Pulse en el tablero para jugar de nuevo.&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;P2 turn&lt;/b&gt;</source>
         <comment>P means player</comment>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Turno J2&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;P1 turn&lt;/b&gt;</source>
         <comment>P means player</comment>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Turno J1&lt;/b&gt;</translation>
     </message>
 </context>
 </TS>
