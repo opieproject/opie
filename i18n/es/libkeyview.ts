2ca94701fdcc8fdce36bc819e545297a15ffcdba
Carsten Niehaus <cniehaus@handhelds.org>
2003-06-21 21:19:50 +0000

    removing files which do not belong in CVS

--- i18n/es/libkeyview.ts
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE TS><TS>
-<context>
-    <name>Keyview</name>
-    <message>
-        <source>Keyview</source>
-        <translation>Keyview</translation>
-    </message>
-</context>
-</TS>
