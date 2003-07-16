bcf3ae933fad6e96283c54fd765176611669c8bf
Patrick S. Vogt <tille@handhelds.org>
2003-07-16 13:25:02 +0000

    fixed layout and it stoped crashing ;)

--- noncore/settings/networksettings/ppp/interfaceinformationppp.cpp
+++ noncore/settings/networksettings/ppp/interfaceinformationppp.cpp
@@ -22,14 +22,14 @@
 InterfaceInformationPPP::InterfaceInformationPPP(QWidget *parent, const char *name, Interface *i, WFlags f)
     :InterfaceInformationImp(parent, name, i, f)
 {
-    qDebug("InterfaceInformationPPP::InterfaceInformationPPP");
+    qDebug("InterfaceInformationPPP::InterfaceInformationPPP %s", name);
     con = new ConnectWidget( (InterfacePPP*)i, this, "con" );
     con->setSizePolicy( QSizePolicy(QSizePolicy::MinimumExpanding,
                                     QSizePolicy::Fixed) );
-    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    InterfaceInformationLayout->addItem( spacer, 7, 0 );
-    InterfaceInformationLayout->addMultiCellWidget( con, 8, 8, 0, 1 );
-    // InterfaceInformationLayout->addWidget( con, 7, 0 );
+//      QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
+//      InterfaceInformationLayout->addItem( spacer, 7, 0 );
+//    InterfaceInformationLayout->addMultiCellWidget( con, 8, 8, 0, 1 );
+     InterfaceInformationLayout->addWidget( con, 1, 0 );
      connect(i, SIGNAL(begin_connect()),con, SLOT(preinit()));
 }
 
