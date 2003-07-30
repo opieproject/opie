8ebae872dc256540c895e819c8a1b411bc1108b1
Stefan Eilers <stefan@eilers-online.net>
2003-07-30 15:24:31 +0000

    Ok.. I think it is time to make the jump to 1.0.0

--- core/pim/addressbook/version.h
+++ core/pim/addressbook/version.h
@@ -1,9 +1,9 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define MAINVERSION  "0"
-#define SUBVERSION   "9"
-#define PATCHVERSION "5"
+#define MAINVERSION  "1"
+#define SUBVERSION   "0"
+#define PATCHVERSION "0"
 
 #define APPNAME "OPIE_ADDRESSBOOK"
 
