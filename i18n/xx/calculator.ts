e2d83768f6b34f568b1f7ea1f0b60f7fde3ebe6d
Maximilian Reiß <max.reiss@gmx.de>
2003-08-10 19:56:27 +0000

    string for calculator

--- i18n/xx/calculator.ts
+++ i18n/xx/calculator.ts
@@ -133,4 +133,203 @@
         <translation>FOO</translation>
     </message>
 </context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Standard</source>
+        <translation type="unfinished">FOO</translation>
+    </message>
+    <message>
+        <source>Weight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Area</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperatures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>acres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&#xb0;C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>carats</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>cm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>cu cm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>cu ft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>cu in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&#xb0;F</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>fl oz (US)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>g</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>gal (US)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>hectares</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>kg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>km</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>l</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>lb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lg tons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>mg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>mi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>naut. mi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>oz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>pt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq cm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq ft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq km</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq mi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>sq yd</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>st</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>St tons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>tblspoon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>teaspoons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>tonnes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>yd</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
