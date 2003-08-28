be25be2d2ed1925a1fb87b7c5bd42e7e037b27cc
Holger Freyther <zecke@handhelds.org>
2003-08-28 14:35:37 +0000

    Make it compile with opie ( use QLibrary )

--- core/launcher/irserver.cpp
+++ core/launcher/irserver.cpp
@@ -1,20 +1,40 @@
+/**********************************************************************
+** Copyright (C) 2000-2002 Trolltech AS.  All rights reserved.
+**
+** This file is part of the Qtopia Environment.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
+
 #include "irserver.h"
 
-#include <qpe/qlibrary.h>
-#include <qpe/qpeapplication.h>
+#include <qtopia/qlibrary.h>
+#include <qtopia/qpeapplication.h>
 
 #include <qtranslator.h>
-#include <stdlib.h>
 
 #include "obexinterface.h"
 
 #include <qdir.h>
 
 IrServer::IrServer( QObject *parent, const char *name )
-  : QObject( parent, name )
+  : QObject( parent, name ), obexIface(0)
 {
     lib = 0;
-    iface = 0;
+    obexIface = 0;
     QString path = QPEApplication::qpeDir() + "/plugins/obex/";
     QDir dir( path, "lib*.so" );
     QStringList list = dir.entryList();
@@ -22,7 +42,7 @@ IrServer::IrServer( QObject *parent, const char *name )
     for ( it = list.begin(); it != list.end(); ++it ) {
 	QLibrary *trylib = new QLibrary( path + *it );
 	//qDebug("trying lib %s", (path + (*it)).latin1() );
-	if ( trylib->queryInterface( IID_ObexInterface, (QUnknownInterface**)&iface ) == QS_OK ) {
+	if ( trylib->queryInterface( IID_ObexInterface, (QUnknownInterface**)&obexIface ) == QS_OK ) {
 	    lib = trylib;
 	    //qDebug("found obex lib" );
 	    QString lang = getenv( "LANG" );
@@ -46,7 +66,8 @@ IrServer::IrServer( QObject *parent, const char *name )
 
 IrServer::~IrServer()
 {
-    if ( iface )
-	iface->release();
+    if ( obexIface )
+	obexIface->release();
     delete lib;
 }
+
