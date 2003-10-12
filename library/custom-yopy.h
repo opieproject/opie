32db2e4bd383d0bedb1498519058005cefe5fead
Otfried Cheong <ocheong@handhelds.org>
2003-10-12 07:53:59 +0000

    Added Yopy to build system.

---
+++ library/custom-yopy.h
@@ -0,0 +1,8 @@
+/**********************************************************************
+**
+** Customization for Yopy
+**
+**********************************************************************/
+
+// Don't yet know how to calibrate from Opie
+// #define QPE_NEED_CALIBRATION
