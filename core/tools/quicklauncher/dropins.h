43d7dd5cccca5ab8f83b62bbc28a3accd9285258
Stefan Eilers <stefan@eilers-online.net>
2003-11-17 16:02:31 +0000

    Fixing the ".so" stuff for MacOS-X .. Quicklauncher apps are now loaded successfully..

--- core/tools/quicklauncher/dropins.h
+++ core/tools/quicklauncher/dropins.h
@@ -29,6 +29,11 @@ namespace Opie {
         QRESULT res = QS_FALSE;
         *iface = 0;
 
+	// This code is very platform specific.. We should find better
+	// solutions to handle names.. Maybe one central function would be
+	// better than checking this ".so" stuff all around in the sources.. 
+	// (eilers)
+
         // Below lines from TT then mine again
         QString name = libFile;
         if ( libFile.findRev(".so") == (int)libFile.length()-3 ) {
@@ -36,7 +41,11 @@ namespace Opie {
             if ( name.find( "lib" ) == 0 )
                 name = name.mid( 3 );
         }
+#ifdef Q_OS_MACX
+        QString path = QPEApplication::qpeDir() + "/plugins/application/lib"+name+".dylib";
+#else
         QString path = QPEApplication::qpeDir() + "/plugins/application/lib"+name+".so";
+#endif
 
         QLibrary *lib = new QLibrary( path );
         if ( lib->queryInterface( uuid, iface ) == QS_OK && iface ) {
