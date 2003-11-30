33f5a1286eb94da111a0560767111255b8a2457e
Matthias Hentges <coredump@handhelds.org>
2003-11-30 19:03:26 +0000

    DE-Translation updates for networksettings and security-settings

--- i18n/de/networksettings.ts
+++ i18n/de/networksettings.ts
@@ -1,4 +1,16 @@
 <!DOCTYPE TS><TS>
+<defaultcodec>iso8859-1</defaultcodec>
+<context>
+    <name>AccountWidget</name>
+    <message>
+        <source>IP</source>
+        <translation>IP</translation>
+    </message>
+    <message>
+        <source>Gateway</source>
+        <translation>Gateway</translation>
+    </message>
+</context>
 <context>
     <name>AddConnection</name>
     <message>
@@ -16,6 +28,180 @@
     <message>
         <source>Services</source>
         <translation>Dienste</translation>
+    </message>
+</context>
+<context>
+    <name>DialWidget</name>
+    <message>
+        <source>&amp;Remove</source>
+        <translation>&amp;Entfernen</translation>
+    </message>
+</context>
+<context>
+    <name>InterfaceAdvanced</name>
+    <message>
+        <source>Advanced Interface Information</source>
+        <translation>Erweiterte Informationen</translation>
+    </message>
+    <message>
+        <source>eth0</source>
+        <translation>eth0</translation>
+    </message>
+    <message>
+        <source>IP Address</source>
+        <translation>IP Adresse</translation>
+    </message>
+    <message>
+        <source>Interface</source>
+        <translation>Gerät</translation>
+    </message>
+    <message>
+        <source>Subnet Mask</source>
+        <translation>Netzmaske</translation>
+    </message>
+    <message>
+        <source>0.0.0.0</source>
+        <translation>0.0.0.0</translation>
+    </message>
+    <message>
+        <source>DHCP Information</source>
+        <translation>DHCP Information</translation>
+    </message>
+    <message>
+        <source>DHCP Server</source>
+        <translation>DHCP Server</translation>
+    </message>
+    <message>
+        <source>Lease Expires</source>
+        <translation>Lease verfällt</translation>
+    </message>
+    <message>
+        <source>Lease Obtained</source>
+        <translation>Lease erhalten</translation>
+    </message>
+    <message>
+        <source>Broadcast</source>
+        <translation>Broadcast</translation>
+    </message>
+    <message>
+        <source>MAC Address</source>
+        <translation>MAC Adresse</translation>
+    </message>
+    <message>
+        <source>00:00:00:00:00:00</source>
+        <translation>00:00:00:00:00:00</translation>
+    </message>
+</context>
+<context>
+    <name>InterfaceInformation</name>
+    <message>
+        <source>Interface Information</source>
+        <translation>Geräte Information</translation>
+    </message>
+    <message>
+        <source>IP Address</source>
+        <translation>IP Adresse</translation>
+    </message>
+    <message>
+        <source>0.0.0.0</source>
+        <translation>0.0.0.0</translation>
+    </message>
+    <message>
+        <source>Subnet Mask</source>
+        <translation>Netzmaske</translation>
+    </message>
+    <message>
+        <source>00:00:00:00:00:00</source>
+        <translation>00:00:00:00:00:00</translation>
+    </message>
+    <message>
+        <source>MAC Address</source>
+        <translation>MAC Adresse</translation>
+    </message>
+    <message>
+        <source>Broadcast</source>
+        <translation>Broadcast</translation>
+    </message>
+    <message>
+        <source>&amp;Refresh</source>
+        <translation>&amp;Erneuern</translation>
+    </message>
+    <message>
+        <source>S&amp;top</source>
+        <translation>S&amp;top</translation>
+    </message>
+    <message>
+        <source>R&amp;estart</source>
+        <translation>N&amp;eustart</translation>
+    </message>
+    <message>
+        <source>&amp;Start</source>
+        <translation>&amp;Start</translation>
+    </message>
+    <message>
+        <source>&amp;View Advanced Information</source>
+        <translation>Erweiterte &amp;Informationen</translation>
+    </message>
+    <message>
+        <source>be &amp;silent</source>
+        <translation>Ruhig &amp;sein</translation>
+    </message>
+</context>
+<context>
+    <name>InterfacePPP</name>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+<context>
+    <name>InterfaceSetup</name>
+    <message>
+        <source>Interface Configuration</source>
+        <translation>Geräte Konfiguration</translation>
+    </message>
+    <message>
+        <source>Automatically bring up</source>
+        <translation>Automatisch starten</translation>
+    </message>
+    <message>
+        <source>DHCP</source>
+        <translation>DHCP</translation>
+    </message>
+    <message>
+        <source>Static Ip Configuration</source>
+        <translation>Statische IP</translation>
+    </message>
+    <message>
+        <source>Subnet Mask</source>
+        <translation>Netzmaske</translation>
+    </message>
+    <message>
+        <source>255.255.255.0</source>
+        <translation>255.255.255.0</translation>
+    </message>
+    <message>
+        <source>First DNS</source>
+        <translation>Erster DNS</translation>
+    </message>
+    <message>
+        <source>IP Address</source>
+        <translation>IP Adresse</translation>
+    </message>
+    <message>
+        <source>Gateway</source>
+        <translation>Gateway</translation>
+    </message>
+    <message>
+        <source>Second DNS</source>
+        <translation>Zweiter DNS</translation>
+    </message>
+</context>
+<context>
+    <name>InterfaceSetupImp</name>
+    <message>
+        <source>Restarting interface</source>
+        <translation>Gerät neustarten</translation>
     </message>
 </context>
 <context>
@@ -108,10 +294,6 @@
 <context>
     <name>MainWindowImp</name>
     <message>
-        <source>Disconnected</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
@@ -125,7 +307,164 @@
     </message>
     <message>
         <source>Please select an interface first.</source>
-        <translation>Bitte wählen Sie zuerst eine Schnittstelle.</translation>
+        <translation>&lt;p&gt;Bitte wählen Sie zuerst eine Schnittstelle.</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Getrennt</translation>
+    </message>
+</context>
+<context>
+    <name>ModemTransfer</name>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+<context>
+    <name>WLAN</name>
+    <message>
+        <source>Wireless Configuration</source>
+        <translation>WLAN Konfiguration</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Generell</translation>
+    </message>
+    <message>
+        <source>any</source>
+        <translation>Alle</translation>
+    </message>
+    <message>
+        <source>SSID</source>
+        <translation>SSID</translation>
+    </message>
+    <message>
+        <source>Infrastructure</source>
+        <translation>Infrastruktur</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Managed</source>
+        <translation>Managed</translation>
+    </message>
+    <message>
+        <source>Ad-Hoc</source>
+        <translation>Ad-Hoc</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Modus</translation>
+    </message>
+    <message>
+        <source>Specify &amp;Access Point</source>
+        <translation>Acces Point fest &amp;einstellen</translation>
+    </message>
+    <message>
+        <source>MAC</source>
+        <translation>MAC</translation>
+    </message>
+    <message>
+        <source>Specify &amp;Channel</source>
+        <translation>&amp;Kanal fest einstellen</translation>
+    </message>
+    <message>
+        <source>Rescan Neighbourhood</source>
+        <translation>Neu scannen</translation>
+    </message>
+    <message>
+        <source>Chn</source>
+        <translation>Chn</translation>
+    </message>
+    <message>
+        <source>Encryption</source>
+        <translation>Verschlüsselung</translation>
+    </message>
+    <message>
+        <source>&amp;Enable Encryption</source>
+        <translation>Verschlüsselung &amp;einschalten</translation>
+    </message>
+    <message>
+        <source>&amp;Key Setting</source>
+        <translation>&amp;Schlüssel</translation>
+    </message>
+    <message>
+        <source>Key &amp;1</source>
+        <translation>Schlüssel &amp;1</translation>
+    </message>
+    <message>
+        <source>Key &amp;2</source>
+        <translation>Schlüssel &amp;2</translation>
+    </message>
+    <message>
+        <source>Key &amp;3</source>
+        <translation>Schlüssel &amp;3</translation>
+    </message>
+    <message>
+        <source>Key &amp;4</source>
+        <translation>Schlüssel &amp;4</translation>
+    </message>
+    <message>
+        <source>Non-encrypted Packets</source>
+        <translation>Unverschlüsselte Pakete</translation>
+    </message>
+    <message>
+        <source>&amp;Accept</source>
+        <translation>&amp;Annehmen</translation>
+    </message>
+    <message>
+        <source>&amp;Reject</source>
+        <translation>&amp;Verwerfen</translation>
+    </message>
+</context>
+<context>
+    <name>WlanInfo</name>
+    <message>
+        <source>Interface Information</source>
+        <translation>Geräte Information</translation>
+    </message>
+    <message>
+        <source>802.11b</source>
+        <translation>802.11b</translation>
+    </message>
+    <message>
+        <source>Channel</source>
+        <translation>Kanal</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Modus</translation>
+    </message>
+    <message>
+        <source>ESSID</source>
+        <translation>ESSID</translation>
+    </message>
+    <message>
+        <source>Station</source>
+        <translation>Station</translation>
+    </message>
+    <message>
+        <source>AP</source>
+        <translation>AP</translation>
+    </message>
+    <message>
+        <source>Rate</source>
+        <translation>Durchsatz</translation>
+    </message>
+    <message>
+        <source>Quality</source>
+        <translation>Qualität</translation>
+    </message>
+    <message>
+        <source>Noise</source>
+        <translation>Rauschen</translation>
+    </message>
+    <message>
+        <source>Signal</source>
+        <translation>Signal</translation>
     </message>
 </context>
 </TS>
--- i18n/de/security.ts
+++ i18n/de/security.ts
@@ -41,51 +41,53 @@ Access denied</source>
     </message>
     <message>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Achtung</translation>
     </message>
     <message>
         <source>&lt;p&gt;All user-defined net ranges will be lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle manuell eingegebenen
+Netzbereiche gehen verloren.</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>
+</translation>
     </message>
     <message>
         <source>192.168.129.0/24</source>
-        <translation type="unfinished">192.168.129.0/24</translation>
+        <translation>192.168.129.0/24</translation>
     </message>
     <message>
         <source>192.168.1.0/24</source>
-        <translation type="unfinished">192.168.1.0/24</translation>
+        <translation>192.168.1.0/24</translation>
     </message>
     <message>
         <source>192.168.0.0/16</source>
-        <translation type="unfinished">192.168.0.0/16</translation>
+        <translation>192.168.0.0/16</translation>
     </message>
     <message>
         <source>172.16.0.0/12</source>
-        <translation type="unfinished">172.16.0.0/12</translation>
+        <translation>172.16.0.0/12</translation>
     </message>
     <message>
         <source>10.0.0.0/8</source>
-        <translation type="unfinished">10.0.0.0/8</translation>
+        <translation>10.0.0.0/8</translation>
     </message>
     <message>
         <source>1.0.0.0/8</source>
-        <translation type="unfinished">1.0.0.0/8</translation>
+        <translation>1.0.0.0/8</translation>
     </message>
     <message>
         <source>WARNING</source>
-        <translation type="unfinished"></translation>
+        <translation>WARNUNG</translation>
     </message>
     <message>
         <source>&lt;p&gt;Selecting IntelliSync here will disable the FTP password.&lt;p&gt;Every machine in your netrange will be able to sync with your Zaurus!</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Wenn Sie hier IntelliSync auswählen wird das FTP-Passwort abgeschaltet.&lt;p&gt;Jeder PC im eingestellten Netzbereich kann dann mit Ihrem Zaurus synchronisieren!</translation>
     </message>
 </context>
 <context>
@@ -116,10 +118,42 @@ eingegeben werden</translation>
         <translation>Synchronisierung gestatten aus dem Netzwerk:</translation>
     </message>
     <message>
+        <source>192.168.0.0/16</source>
+        <translation type="obsolete">192.168.0.0/16</translation>
+    </message>
+    <message>
+        <source>172.16.0.0/12</source>
+        <translation type="obsolete">172.16.0.0/12</translation>
+    </message>
+    <message>
+        <source>10.0.0.0/8</source>
+        <translation type="obsolete">10.0.0.0/8</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation type="obsolete">Alle</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="obsolete">Keine</translation>
+    </message>
+    <message>
         <source>&lt;P&gt;Pass code protection provides a minimal level of protection from casual access to this device.</source>
         <translation>&lt;P&gt;Das Passwort bietet einen minimalen Schutz, 
 sollte jemand unerwünscht Zugriff auf dieses 
 Gerät erhalten.</translation>
+    </message>
+    <message>
+        <source>192.168.129.0/24 (default)</source>
+        <translation type="obsolete">192.168.129.0/24 (Standard)</translation>
+    </message>
+    <message>
+        <source>192.168.1.0/24</source>
+        <translation type="obsolete">192.168.1.0/24</translation>
+    </message>
+    <message>
+        <source>1.0.0.0/8</source>
+        <translation type="obsolete">1.0.0.0/8</translation>
     </message>
     <message>
         <source>Passcode</source>
@@ -137,29 +171,32 @@ Gerät erhalten.</translation>
         <source>This button will let you change the security passcode.  
 
 Note: This is *not* the sync password.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dieser Button ändert das Sicherheitspasswort
+
+Das ist *nicht* das sync Passwort.</translation>
     </message>
     <message>
         <source>Delete the current passcode.  
 You can enter a new one at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuelles Sicherheitspasswort löschen.
+Es kann jederzeit ein neues erstellt werden.</translation>
     </message>
     <message>
         <source>If you enable this option, a passcode must be entered after every resume event of your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn ausgewählt muss bei jedem Start des Zaurus ein Passwort eingegeben werden.</translation>
     </message>
     <message>
         <source>If you select this option, opie-login will automagically log you in with the user you have selected in the drop-down list.  
 
 For the Zaurus 5x00 this is always the user &quot;root&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn diese Option angewählt ist wird der ausgewählte Benutzer beim Start automatisch eingelogt.</translation>
     </message>
     <message>
         <source>This drop-down list allows you to select the user for auto-login
 (if enabled above).  
 
 You can only select an actually configured user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hier kann man den Benutzer für Autologin wählen.</translation>
     </message>
     <message>
         <source>Select a net-range or enter a new one.  
@@ -172,11 +209,16 @@ The entry &quot;All&quot; will allow *any* IP to connect.
 The entry &quot;None&quot; will *deny* any connection.  
 
 If unsure, select &quot;Any&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wählen Sie einen Netzbereich, oder erstellen Sie einen neuen.
+
+Diese Liste stellt den Netzbereich ein, für welchen das Synchronisieren des Zaurus erlaubt wird.
+
+Der Eintrag &quot;Alle&quot; erlaubt jedem PC den Zugriff,
+der Eintrag &quot;Keine&quot; schaltet das Synchronisieren aus.</translation>
     </message>
     <message>
         <source>Delete Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Eintrag löschen</translation>
     </message>
     <message>
         <source>Delete the selected net range from the list
@@ -185,30 +227,32 @@ If you press this button, the currently selected net range will be deleted from 
 
 If you hit this button in error, the button &quot;Restore Defaults&quot; will restore
 the list to the state it has been when you initially booted your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn dieser Button gedrückt wird, wird der gerade angezeigte Netzbereich gelöscht.</translation>
     </message>
     <message>
         <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation>Vorgabe</translation>
     </message>
     <message>
         <source>This button will restore the list of net ranges
 to the defaults.
 
 Beware that all manually entered net ranges will be lost!</source>
-        <translation type="unfinished"></translation>
+        <translation>Mit diesem Button können die Vorgabe-Netzbereiche wieder hergestellt werden.
+
+Alle manuell eingegebenen Netzbereiche gehen verloren!</translation>
     </message>
     <message>
         <source>Select your sync software</source>
-        <translation type="unfinished"></translation>
+        <translation>Synchronisationssoftware auswählen</translation>
     </message>
     <message>
         <source>QTopia</source>
-        <translation type="unfinished"></translation>
+        <translation>QTopia</translation>
     </message>
     <message>
         <source>IntelliSync</source>
-        <translation type="unfinished"></translation>
+        <translation>IntelliSync</translation>
     </message>
     <message>
         <source>This option changes the login behaviour of the internal FTP server.  
@@ -216,7 +260,21 @@ Beware that all manually entered net ranges will be lost!</source>
 If you have problems syncing, try the &quot;IntelliSync&quot; setting, which will accept any password you throw at the sync (FTP) port.  
 
 The default is Qtopia. It will let you log in with username &quot;root&quot; and password &quot;Qtopia&quot;.   The password is CaSeSeNsItIvE.</source>
-        <translation type="unfinished"></translation>
+        <translation>Diese Option sollte nur bei Problemen mit dem Synchronisieren verändert werden.
+
+Die Vorgabe ist &quot;QTopia&quot;.</translation>
+    </message>
+    <message>
+        <source>Qtopia 1.7</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Opie 1.0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Both</source>
+        <translation></translation>
     </message>
 </context>
 </TS>
