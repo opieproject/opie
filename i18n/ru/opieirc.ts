75b841888493dd91b026a30b0cb80c1e5b2eee2c
Sergei Klink <sudonix@handhelds.org>
2004-01-03 00:05:10 +0000

    First commit(opieirc.ts will probably need serious revision)

---
+++ i18n/ru/opieirc.ts
@@ -0,0 +1,516 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>IRCChannelTab</name>
+    <message>
+        <source>Talking on channel</source>
+        <translation>Разговор на канале</translation>
+    </message>
+    <message>
+        <source>Channel discussion</source>
+        <translation>Беседа канала</translation>
+    </message>
+    <message>
+        <source>Type your message here to participate in the channel discussion</source>
+        <translation>Здесь введите свое сообщение чтобы поучаствовать в беседе</translation>
+    </message>
+    <message>
+        <source>CTCP</source>
+        <translation>CTCP</translation>
+    </message>
+    <message>
+        <source>Query</source>
+        <translation>Запрос</translation>
+    </message>
+    <message>
+        <source>Ping</source>
+        <translation>Пинг</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Версия</translation>
+    </message>
+    <message>
+        <source>Whois</source>
+        <translation>Whois</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Отсоединен</translation>
+    </message>
+</context>
+<context>
+    <name>IRCConnection</name>
+    <message>
+        <source>Connected, logging in ..</source>
+        <translation>Подключен, вхожу ..</translation>
+    </message>
+    <message>
+        <source>Successfully logged in.</source>
+        <translation>Вошел успешно.</translation>
+    </message>
+    <message>
+        <source>Socket error : </source>
+        <translation>Ошибка сокета : </translation>
+    </message>
+    <message>
+        <source>Connection closed</source>
+        <translation>Соединение закрыто</translation>
+    </message>
+</context>
+<context>
+    <name>IRCMessageParser</name>
+    <message>
+        <source>Nonexistant channel join - desynchronized?</source>
+        <translation>Несуществующий канал - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Person has already joined the channel - desynchronized?</source>
+        <translation>Человек уже присоединился к каналу - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>You already joined the channel - desynchronized?</source>
+        <translation>Вы уже присоединились к каналу - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Parting person not found - desynchronized?</source>
+        <translation>Уходящий человек не найден - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Channel for part not found - desynchronized?</source>
+        <translation>Канал для отключения не найден - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Channel message with unknown sender</source>
+        <translation>Сообщение на канале с неизвестным отправителем</translation>
+    </message>
+    <message>
+        <source>Received PRIVMSG of unknown type</source>
+        <translation>Принято PRIVMSG неизвестного типа</translation>
+    </message>
+    <message>
+        <source>Nickname change of an unknown person</source>
+        <translation>Смена клички неизвестного человека</translation>
+    </message>
+    <message>
+        <source>Unknown person quit - desynchronized?</source>
+        <translation>Выход неизвестного человека - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source> changed topic to </source>
+        <translation>тема сменена на</translation>
+    </message>
+    <message>
+        <source>Unknown channel topic - desynchronized?</source>
+        <translation>Неизвестная тема на канале - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Received a CTCP PING from </source>
+        <translation>Принят CTCP PING от</translation>
+    </message>
+    <message>
+        <source>Received a CTCP VERSION from </source>
+        <translation>Принят CTCP VERSION от</translation>
+    </message>
+    <message>
+        <source>CTCP ACTION with unknown person - Desynchronized?</source>
+        <translation>CTCP ACTION с неизвестным человеком - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>CTCP ACTION with unknown channel - Desynchronized?</source>
+        <translation>CTCP ACTION с неизвестным каналом - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>CTCP ACTION with bad recipient</source>
+        <translation>CTCP ACTION с плохим адресатом</translation>
+    </message>
+    <message>
+        <source>Mode change has unknown type</source>
+        <translation>Смена режима с неизвестным типом</translation>
+    </message>
+    <message>
+        <source>Mode change with unknown person - Desynchronized?</source>
+        <translation>Смена режима для неизвестного человека - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Mode change with unknown flag</source>
+        <translation>Смена режима с неизвестным флагом</translation>
+    </message>
+    <message>
+        <source>Mode change with unknown kannel - Desynchronized?</source>
+        <translation>Смена режима с неизвестным каналом - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>User modes not supported yet</source>
+        <translation>Пользовательские режимы пока не поддерживаются</translation>
+    </message>
+    <message>
+        <source>You were kicked from </source>
+        <translation>Вы были выброшены с</translation>
+    </message>
+    <message>
+        <source> by </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> was kicked from </source>
+        <translation>был выброшен с</translation>
+    </message>
+    <message>
+        <source>Unknown person kick - desynchronized?</source>
+        <translation>Выброс неизестного человека - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Unknown channel kick - desynchronized?</source>
+        <translation>Выброс неизвестного канала - не синхронизирован?</translation>
+    </message>
+    <message>
+        <source>Server message with unknown channel</source>
+        <translation>Сообщение сервера с неизвестным каналом</translation>
+    </message>
+    <message>
+        <source>You joined channel </source>
+        <translation>Вы присоединились к каналу</translation>
+    </message>
+    <message>
+        <source>Nickname is in use, please reconnect with a different nickname</source>
+        <translation>Кличка уже используется, пожалуйста пересоединитесь с другой</translation>
+    </message>
+    <message>
+        <source>No such nickname</source>
+        <translation>Нет такой клички</translation>
+    </message>
+    <message>
+        <source>Received unhandled numeric command: %1</source>
+        <translation>Принята необработанная цифровая команда: %1</translation>
+    </message>
+    <message>
+        <source>Received unhandled ctcp command: %1</source>
+        <translation>Принята необработанная ctcp команда: %1</translation>
+    </message>
+    <message>
+        <source>Received unhandled literal command: %1</source>
+        <translation>Принята необработанная буквенная команда: %1</translation>
+    </message>
+    <message>
+        <source>%1 joined channel %2</source>
+        <translation>%1 подключился к каналу %2</translation>
+    </message>
+    <message>
+        <source>You left channel %1</source>
+        <translation>Вы ушли с канала %1</translation>
+    </message>
+    <message>
+        <source>%1 left channel %2</source>
+        <translation>%1 ушел с канала %2</translation>
+    </message>
+    <message>
+        <source>Channel message with unknown channel %1</source>
+        <translation>Сообщение канала с неизвестного канала %1</translation>
+    </message>
+    <message>
+        <source>You are now known as %1</source>
+        <translation>Вы теперб известны как %1</translation>
+    </message>
+    <message>
+        <source>%1 is now known as %2</source>
+        <translation>%1 теперь известен как %2</translation>
+    </message>
+    <message>
+        <source>%1 has quit (%2)</source>
+        <translation>%1 вышел (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>IRCQueryTab</name>
+    <message>
+        <source>Talking to </source>
+        <translation>Разговор с</translation>
+    </message>
+    <message>
+        <source>Private discussion</source>
+        <translation>Приватный разговор</translation>
+    </message>
+    <message>
+        <source>Type your text here in order to send a message to the other person</source>
+        <translation>Здесь введите свое сообщение чтобы отправить другому человеку</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Отключен</translation>
+    </message>
+</context>
+<context>
+    <name>IRCServerEditor</name>
+    <message>
+        <source>Profile name :</source>
+        <translation>Имя профиля:</translation>
+    </message>
+    <message>
+        <source>The name of this server profile in the overview</source>
+        <translation>Имя этого профиля сервера в в обзоре</translation>
+    </message>
+    <message>
+        <source>Hostname :</source>
+        <translation>Имя хоста :</translation>
+    </message>
+    <message>
+        <source>The server to connect to - can be any valid host name or IP address</source>
+        <translation>Сервер к которому нужно подключиться - может быть любым хостом или IP адресом</translation>
+    </message>
+    <message>
+        <source>Port :</source>
+        <translation>Порт :</translation>
+    </message>
+    <message>
+        <source>The server port to connect to. Usually 6667</source>
+        <translation>Порт на сервере к которому нужно подключиться. Обычно 6667</translation>
+    </message>
+    <message>
+        <source>Nickname :</source>
+        <translation>Кличка :</translation>
+    </message>
+    <message>
+        <source>Your nick name on the IRC network</source>
+        <translation>Ваша кличка на IRC сети</translation>
+    </message>
+    <message>
+        <source>Realname :</source>
+        <translation>Настоящее имя :</translation>
+    </message>
+    <message>
+        <source>Your real name</source>
+        <translation>Ваше настоящее имя</translation>
+    </message>
+    <message>
+        <source>Password :</source>
+        <translation>Пароль :</translation>
+    </message>
+    <message>
+        <source>Password to connect to the server (if required)</source>
+        <translation>Пароль для соединения с сервером (если необходимо)</translation>
+    </message>
+    <message>
+        <source>Channels :</source>
+        <translation>Каналы :</translation>
+    </message>
+    <message>
+        <source>Comma-Separated list of all channels you would like to join automatically</source>
+        <translation>Список всех каналов, через запятую, к которым вы хотите подключится автоматически</translation>
+    </message>
+    <message>
+        <source>Edit server information</source>
+        <translation>Изменить информацию о сервере</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <source>Profile name required</source>
+        <translation>Необходимо имя профиля</translation>
+    </message>
+    <message>
+        <source>Host name required</source>
+        <translation>Необходимо имя хоста</translation>
+    </message>
+    <message>
+        <source>Port required</source>
+        <translation>Необходим порт</translation>
+    </message>
+    <message>
+        <source>Nickname required</source>
+        <translation>Необходима кличка</translation>
+    </message>
+    <message>
+        <source>Realname required</source>
+        <translation>Необходимо настоящее имя</translation>
+    </message>
+    <message>
+        <source>The channel list needs to contain a
+comma separated list of channel
+ names which start with either &apos;#&apos; or &apos;+&apos;</source>
+        <translation>Список каналов должен содержать
+список имен каналов через запятую
+которые начинаются с &apos;#&apos; или &apos;+&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>IRCServerList</name>
+    <message>
+        <source>Serverlist Browser</source>
+        <translation>Браузер серверов</translation>
+    </message>
+    <message>
+        <source>Please choose a server profile</source>
+        <translation>Пожалуйста выберите профиль сервера</translation>
+    </message>
+    <message>
+        <source>Select a server profile from this list and then tap on OK in the upper-right corner</source>
+        <translation>Выберите профиль сервера из этого списка и нажмите OK в правом верхнем углу</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected server profile</source>
+        <translation>Удалить выбранный профиль сервера</translation>
+    </message>
+    <message>
+        <source>Edit the currently selected server profile</source>
+        <translation>Изменить выбранный профиль сервера</translation>
+    </message>
+    <message>
+        <source>Add a new server profile</source>
+        <translation>Добавить новый профиль сервера</translation>
+    </message>
+</context>
+<context>
+    <name>IRCServerTab</name>
+    <message>
+        <source>Connection to</source>
+        <translation>Соединение с</translation>
+    </message>
+    <message>
+        <source>Server messages</source>
+        <translation>Сообщения сервера</translation>
+    </message>
+    <message>
+        <source>Type commands here. A list of available commands can be found inside the OpieIRC help</source>
+        <translation>Здесь вводите команды. Список доступных команд находится внутри справки OpieIRC</translation>
+    </message>
+</context>
+<context>
+    <name>IRCSettings</name>
+    <message>
+        <source>Lines displayed :</source>
+        <translation>Линий отображено :</translation>
+    </message>
+    <message>
+        <source>Amount of lines to be displayed in chats before old lines get deleted - this is necessary to restrain memory consumption. Set to 0 if you don&apos;t need this</source>
+        <translation>Количество линий отображаемых в чатах прежде чем они удаляются - это необходимо чтобы ограничить расход памяти. Установите на 0 если вам это не нужно</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <source>Background color :</source>
+        <translation>Цвет заднего плана :</translation>
+    </message>
+    <message>
+        <source>Background color to be used in chats</source>
+        <translation>Цвет заднего плана используемый в чатах</translation>
+    </message>
+    <message>
+        <source>Normal text color :</source>
+        <translation>Цвет обычного текста :</translation>
+    </message>
+    <message>
+        <source>Text color to be used in chats</source>
+        <translation>Цвет текста используемого в чатах</translation>
+    </message>
+    <message>
+        <source>Error color :</source>
+        <translation>Цвет ошибок :</translation>
+    </message>
+    <message>
+        <source>Text color to be used to display errors</source>
+        <translation>Цвет текста используемого для отображения ошибок</translation>
+    </message>
+    <message>
+        <source>Text written by yourself :</source>
+        <translation>Текст написанный вами :</translation>
+    </message>
+    <message>
+        <source>Text color to be used to identify text written by yourself</source>
+        <translation>Цвет текста используемого чтобы видеть текст написанный вами</translation>
+    </message>
+    <message>
+        <source>Text written by others :</source>
+        <translation>Текст написанный другими :</translation>
+    </message>
+    <message>
+        <source>Text color to be used to identify text written by others</source>
+        <translation>Цвет текста используемого чтобы видеть текст написанный другими</translation>
+    </message>
+    <message>
+        <source>Text written by the server :</source>
+        <translation>Текст написанный сервером :</translation>
+    </message>
+    <message>
+        <source>Text color to be used to identify text written by the server</source>
+        <translation>Цвет текста используемого чтобы видеть текст написанный сервером</translation>
+    </message>
+    <message>
+        <source>Notifications :</source>
+        <translation>Уведомления :</translation>
+    </message>
+    <message>
+        <source>Text color to be used to display notifications</source>
+        <translation>Цвет текста используемого для отображения уведомлений</translation>
+    </message>
+    <message>
+        <source>Colors</source>
+        <translation>Цвета</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>IRCTab</name>
+    <message>
+        <source>Missing description</source>
+        <translation>Нету описания</translation>
+    </message>
+    <message>
+        <source>Description of the tab&apos;s content</source>
+        <translation>Описание содержания этой вкладки</translation>
+    </message>
+    <message>
+        <source>Close this tab</source>
+        <translation>Закрыть эту вкладку</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>IRC Client</source>
+        <translation>IRC Клиент</translation>
+    </message>
+    <message>
+        <source>Server connections, channels, queries and other things will be placed here</source>
+        <translation>Соединения с серверами, каналами, запросы и прочие вещи будут здесь</translation>
+    </message>
+    <message>
+        <source>IRC</source>
+        <translation>IRC</translation>
+    </message>
+    <message>
+        <source>New connection</source>
+        <translation>Новое соединение</translation>
+    </message>
+    <message>
+        <source>Create a new connection to an IRC server</source>
+        <translation>Создать новое соединение с IRC сервером</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Configure OpieIRC&apos;s behavior and appearance</source>
+        <translation>Настроить поведение и внешний вид OpieIRC</translation>
+    </message>
+</context>
+</TS>
