a84f2ca750cce4696f95449895a8fb23f9e7a759
Pavel Ruzicka <pavouk@handhelds.org>
2004-01-12 19:53:51 +0000

    done calibrate translation

--- i18n/cz/calibrate.ts
+++ i18n/cz/calibrate.ts
@@ -4,11 +4,12 @@
     <message>
         <source>Touch the crosshairs firmly and
 accurately to calibrate your screen.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukazujte přesně na kříže, aby
+byla obrazovka zkalibrována.</translation>
     </message>
     <message>
         <source>Welcome to Opie</source>
-        <translation type="unfinished"></translation>
+        <translation>Opie vás vítá</translation>
     </message>
 </context>
 </TS>
