a17a7069d075253e964a2fb6fc153347fc0bb146
Pavel Ruzicka <pavouk@handhelds.org>
2004-01-15 19:22:42 +0000

    Backgammon translation done.

--- i18n/cz/backgammon.ts
+++ i18n/cz/backgammon.ts
@@ -3,164 +3,168 @@
     <name>AI_Dialog</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavení</translation>
     </message>
     <message>
         <source>&lt;b&gt;Rescue&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Zachránit&lt;/b&gt;</translation>
     </message>
     <message>
         <source>?</source>
-        <translation type="unfinished"></translation>
+        <translation>?</translation>
     </message>
     <message>
         <source>&lt;b&gt;Eliminate&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Eliminovat&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Expose&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Ukázat&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Protect&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Chránit&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Safe&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Jistota&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Empty&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Prázdný&lt;/b&gt;</translation>
     </message>
     <message>
         <source>Default Values</source>
-        <translation type="unfinished"></translation>
+        <translation>Výchozí hodnoty</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Pomoc</translation>
     </message>
     <message>
         <source>Bring the pieces out of the endzone</source>
-        <translation type="unfinished"></translation>
+        <translation>Přeneste kameny mimo koncovou zónu</translation>
     </message>
     <message>
         <source>Eliminate an opponents piece</source>
-        <translation type="unfinished"></translation>
+        <translation>Vylučte kameny protihráče</translation>
     </message>
     <message>
         <source>Expose you own pieces.
 After such a move only one piece will remain in the slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Odkryjte vaše kameny.
+Po tahu zbude v drážce jen jeden kámen</translation>
     </message>
     <message>
         <source>Protect a single piece by
 putting another one in this slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Ochraňte jeden kámen položením jiného
+v této drážce</translation>
     </message>
     <message>
         <source>Move piece to a slot already
 occupied by the player</source>
-        <translation type="unfinished"></translation>
+        <translation>Přesuňte kámen do drážky
+již obsazené hráčem</translation>
     </message>
     <message>
         <source>Move piece to an empty slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Přesuňte kámen do prázdné drážky</translation>
     </message>
     <message>
         <source>Press and hold the ? buttton
 next to a field for help</source>
-        <translation type="unfinished"></translation>
+        <translation>Pro pomoc stiskněte a držte
+? na ploše</translation>
     </message>
 </context>
 <context>
     <name>BackGammon</name>
     <message>
         <source>Backgammon</source>
-        <translation type="unfinished"></translation>
+        <translation>Backgammon</translation>
     </message>
     <message>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Nový</translation>
     </message>
     <message>
         <source>Load</source>
-        <translation type="unfinished"></translation>
+        <translation>Otevřít</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Smazat</translation>
     </message>
     <message>
         <source>Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Hra</translation>
     </message>
     <message>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Výchozí</translation>
     </message>
     <message>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Téma</translation>
     </message>
     <message>
         <source>Player</source>
-        <translation type="unfinished"></translation>
+        <translation>Hráč</translation>
     </message>
     <message>
         <source>AI</source>
-        <translation type="unfinished"></translation>
+        <translation>AI</translation>
     </message>
     <message>
         <source>Rules</source>
-        <translation type="unfinished"></translation>
+        <translation>Pravidla</translation>
     </message>
     <message>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Možnosti</translation>
     </message>
     <message>
         <source>deleted theme %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>smazání tématu %1?</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Storno</translation>
     </message>
     <message>
         <source>Load Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Otevřít téma</translation>
     </message>
     <message>
         <source>&lt;b&gt;no move&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;bez tahu&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Player 1 wins. Click on board for new game.&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Hráč 1 vyhrál. Pro novou hru klikněte na desku.&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Player 2 wins. Click on board for new game.&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Hráč 2 vyhrál. Pro novou hru klikněte na desku.&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;P2 turn&lt;/b&gt;</source>
         <comment>P means player</comment>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Hraje 2. hráč&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;P1 turn&lt;/b&gt;</source>
         <comment>P means player</comment>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Hraje 1. hráč&lt;/b&gt;</translation>
     </message>
 </context>
 </TS>
