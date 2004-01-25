4ec1835a8a76f0f82373965987aca8c382806a63
Sergei Klink <sudonix@handhelds.org>
2004-01-25 02:05:16 +0000

    Some more files translated

--- i18n/ru/security.ts
+++ i18n/ru/security.ts
@@ -3,159 +3,168 @@
     <name>Security</name>
     <message>
         <source>Set passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Уст. пароль</translation>
     </message>
     <message>
         <source>Change passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Изменить пароль</translation>
     </message>
     <message>
         <source>Enter passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите пароль</translation>
     </message>
     <message>
         <source>Passcode incorrect</source>
-        <translation type="unfinished"></translation>
+        <translation>Неверный пароль</translation>
     </message>
     <message>
         <source>The passcode entered is incorrect.
 Access denied</source>
-        <translation type="unfinished"></translation>
+        <translation>Введеный пароль неверен.
+Доступ запрещен</translation>
     </message>
     <message>
         <source>Any</source>
-        <translation type="unfinished"></translation>
+        <translation>Любой</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Ни одного</translation>
     </message>
     <message>
         <source>Enter new passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите новый пароль</translation>
     </message>
     <message>
         <source>Re-enter new passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите пароль еще раз</translation>
     </message>
     <message>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Внимание</translation>
     </message>
     <message>
         <source>&lt;p&gt;All user-defined net ranges will be lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Все заданные пользователем участки будут потеряны.</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Да</translation>
     </message>
     <message>
         <source>192.168.129.0/24</source>
-        <translation type="unfinished"></translation>
+        <translation>192.168.129.0/24</translation>
     </message>
     <message>
         <source>192.168.1.0/24</source>
-        <translation type="unfinished"></translation>
+        <translation>192.168.1.0/24</translation>
     </message>
     <message>
         <source>192.168.0.0/16</source>
-        <translation type="unfinished"></translation>
+        <translation>192.168.0.0/16</translation>
     </message>
     <message>
         <source>172.16.0.0/12</source>
-        <translation type="unfinished"></translation>
+        <translation>172.16.0.0/12</translation>
     </message>
     <message>
         <source>10.0.0.0/8</source>
-        <translation type="unfinished"></translation>
+        <translation>10.0.0.0/8</translation>
     </message>
     <message>
         <source>1.0.0.0/8</source>
-        <translation type="unfinished"></translation>
+        <translation>1.0.0.0/8</translation>
     </message>
     <message>
         <source>WARNING</source>
-        <translation type="unfinished"></translation>
+        <translation>ВНИМАНИЕ</translation>
     </message>
     <message>
         <source>&lt;p&gt;Selecting IntelliSync here will disable the FTP password.&lt;p&gt;Every machine in your netrange will be able to sync with your Zaurus!</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Выбор IntelliSync запретит пароль FTP.&lt;p&gt;Любая машина в заданном участке сможет синхронизироваться с вашим КПК!</translation>
     </message>
 </context>
 <context>
     <name>SecurityBase</name>
     <message>
         <source>Security Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки безопасности</translation>
     </message>
     <message>
         <source>Change passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Изменить пароль</translation>
     </message>
     <message>
         <source>Clear passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Сбросить пароль</translation>
     </message>
     <message>
         <source>Require pass code at power-on</source>
-        <translation type="unfinished"></translation>
+        <translation>Требовать пароль при включении</translation>
     </message>
     <message>
         <source>Sync</source>
-        <translation type="unfinished"></translation>
+        <translation>Синхр.</translation>
     </message>
     <message>
         <source>Accept sync from network:</source>
-        <translation type="unfinished"></translation>
+        <translation>Принимать запросы из сети:</translation>
     </message>
     <message>
         <source>&lt;P&gt;Pass code protection provides a minimal level of protection from casual access to this device.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;P&gt;Пароль предоставляет минимальный уровень защиты от случайного доступа к устройству.</translation>
     </message>
     <message>
         <source>Passcode</source>
-        <translation type="unfinished"></translation>
+        <translation>Пароль</translation>
     </message>
     <message>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Войти</translation>
     </message>
     <message>
         <source>Login Automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Входить автоматически</translation>
     </message>
     <message>
         <source>This button will let you change the security passcode.  
 
 Note: This is *not* the sync password.</source>
-        <translation type="unfinished"></translation>
+        <translation>Эта кнопка позволит вам изменить пароль.  
+
+Заметьте: это НЕ пароль для синхронизации.</translation>
     </message>
     <message>
         <source>Delete the current passcode.  
 You can enter a new one at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Удалить текущий пароль.  
+Вы всегда сможете ввести новый.</translation>
     </message>
     <message>
         <source>If you enable this option, a passcode must be entered after every resume event of your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Если вы разрешите эту опцию, нужно будет вводить пароль при каждом включении КПК.</translation>
     </message>
     <message>
         <source>If you select this option, opie-login will automagically log you in with the user you have selected in the drop-down list.  
 
 For the Zaurus 5x00 this is always the user &quot;root&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Если вы выберите эту опцию, opie-login автоматически войдет в систему под выбранным из списка пользователем.  
+
+На Zaurus 5x00 это всегда пользователь &quot;root&quot;.</translation>
     </message>
     <message>
         <source>This drop-down list allows you to select the user for auto-login
 (if enabled above).  
 
 You can only select an actually configured user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Этот список позволит выбрать пользователя для авто-входа
+(если разрешен).  
+
+Вы можете выбрать только уже настроенное имя пользователя.</translation>
     </message>
     <message>
         <source>Select a net-range or enter a new one.  
@@ -168,11 +177,20 @@ The entry &quot;All&quot; will allow *any* IP to connect.
 The entry &quot;None&quot; will *deny* any connection.  
 
 If unsure, select &quot;Any&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите или введите новый участок сети.  
+
+Этот список позволяет выбрать участок сети из которого можно подключится к вашему КПК.  
+
+Например, участок сети 192.168.1.0/24 позволит любой машине с IP между 192.168.1.1 и 192.168.1.254 подключаться к (и синхронизироваться с) вашим КПК.  
+
+Запись &quot;Все&quot; позволит подключиться *любому* IP. 
+Запись &quot;Ни одного&quot; *запретит* все соединения.  
+
+Если неуверены, выберите &quot;Все&quot;.</translation>
     </message>
     <message>
         <source>Delete Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Удалить запись</translation>
     </message>
     <message>
         <source>Delete the selected net range from the list
@@ -181,30 +199,38 @@ If you press this button, the currently selected net range will be deleted from 
 
 If you hit this button in error, the button &quot;Restore Defaults&quot; will restore
 the list to the state it has been when you initially booted your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Удалить выбранный участок из списка
+
+Если вы нажмете эту кнопку, выбранный участок будет удален из списка.
+
+Если вы нажали эту кнопку случайно, кнопка &quot;По умолчанию&quot;
+восстановит список в том виде, когда вы первый раз запустили ваш КПК.</translation>
     </message>
     <message>
         <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation>По умолчанию</translation>
     </message>
     <message>
         <source>This button will restore the list of net ranges
 to the defaults.
 
 Beware that all manually entered net ranges will be lost!</source>
-        <translation type="unfinished"></translation>
+        <translation>Эта кнопка восстановит список участков сети
+по умолчанию.
+
+Учтите что вручную введенные участки будут потеряны!</translation>
     </message>
     <message>
         <source>Select your sync software</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите программу</translation>
     </message>
     <message>
         <source>QTopia</source>
-        <translation type="unfinished"></translation>
+        <translation>QTopia</translation>
     </message>
     <message>
         <source>IntelliSync</source>
-        <translation type="unfinished"></translation>
+        <translation>IntelliSync</translation>
     </message>
     <message>
         <source>This option changes the login behaviour of the internal FTP server.  
@@ -212,19 +238,23 @@ Beware that all manually entered net ranges will be lost!</source>
 If you have problems syncing, try the &quot;IntelliSync&quot; setting, which will accept any password you throw at the sync (FTP) port.  
 
 The default is Qtopia. It will let you log in with username &quot;root&quot; and password &quot;Qtopia&quot;.   The password is CaSeSeNsItIvE.</source>
-        <translation type="unfinished"></translation>
+        <translation>Эта опция изменяет поведение внутреннего FTP сервера.  
+
+Если есть проблемы с соединением, попробуйте значение &quot;IntelliSync&quot;, с которым будет приниматься любое соединение по FTP порту.
+
+Значение по умолчанию - Qtopia. Оно позволит соединяться с именем &quot;root&quot; и паролем &quot;Qtopia&quot;.   Пароль чувствителен к РеГиСтРу.</translation>
     </message>
     <message>
         <source>Qtopia 1.7</source>
-        <translation type="unfinished"></translation>
+        <translation>Qtopia 1.7</translation>
     </message>
     <message>
         <source>Opie 1.0</source>
-        <translation type="unfinished"></translation>
+        <translation>Opie 1.0</translation>
     </message>
     <message>
         <source>Both</source>
-        <translation type="unfinished"></translation>
+        <translation>Оба</translation>
     </message>
 </context>
 </TS>
