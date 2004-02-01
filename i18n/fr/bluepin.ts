f649c2a80509281dd47de2876e912d772167d8cb
Amrein-Marie Christophe <christophe@handhelds.org>
2004-02-01 15:28:17 +0000

    Open Source will win all markets.

--- i18n/fr/bluepin.ts
+++ i18n/fr/bluepin.ts
@@ -4,34 +4,34 @@
     <name>PinDlgBase</name>
     <message>
         <source>Please enter pin</source>
-        <translation type="unfinished">Veuillez entrer le code d&apos;accès</translation>
+        <translation>Veuillez entrer le code PIN</translation>
     </message>
     <message>
         <source>Please enter PIN:</source>
-        <translation type="unfinished">Veuillez entrez le code d&apos;accès :</translation>
+        <translation>Entrez le code PIN :</translation>
     </message>
     <message>
         <source>Save pin</source>
-        <translation type="unfinished">Enregistrer le code d&apos;accès</translation>
+        <translation>Enregistrer code PIN</translation>
     </message>
     <message>
         <source>&amp;Ok</source>
-        <translation type="unfinished">&amp;Confirmer</translation>
+        <translation>&amp;OK</translation>
     </message>
     <message>
         <source>&amp;Cancel</source>
-        <translation type="unfinished">&amp;Annuler</translation>
+        <translation>&amp;Annuler</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Outgoing connection to </source>
-        <translation type="unfinished"></translation>
+        <translation>Connection à </translation>
     </message>
     <message>
         <source>Incoming connection from </source>
-        <translation type="unfinished"></translation>
+        <translation>Connection entrante de </translation>
     </message>
 </context>
 </TS>
