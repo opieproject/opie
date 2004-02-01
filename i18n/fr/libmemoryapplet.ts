6c9f739490dc8b70d32ff56b57bbe8ee18c80e48
Amrein-Marie Christophe <christophe@handhelds.org>
2004-02-01 18:10:33 +0000

    ;-/

--- i18n/fr/libmemoryapplet.ts
+++ i18n/fr/libmemoryapplet.ts
@@ -3,19 +3,19 @@
     <name>LoadInfo</name>
     <message>
         <source>Application CPU usage (%)</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisation CPU par les applications (%)</translation>
     </message>
     <message>
         <source>System CPU usage (%)</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisation système du CPU (%)</translation>
     </message>
     <message>
         <source>This page shows how much this device&apos;s processor is being used.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cette page affiche le niveau d&apos;utilisation du processeur de votre appareil.</translation>
     </message>
     <message>
         <source>Type: </source>
-        <translation type="unfinished"></translation>
+        <translation>Type : </translation>
     </message>
 </context>
 <context>
@@ -28,129 +28,135 @@ Memory is categorized as follows:
 2. Buffers - temporary storage used to improve performance
 3. Cached - information that has recently been used, but has not been freed yet.
 4. Free - memory not currently used by Opie or any running applications.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cette page vous montre l&apos;utilisation de la mémoire de votre PDA.
+La mémoire (i.e RAM) est divisée en catégories comme suit :
+
+1. Utilisée - Mémoire utilisée par Opie et toutes autres applications en cours d&apos;exécution.
+2. Buffers - Mémoire temporaire utilisée pour accroître les performances
+3. Cache - Espace utilisé récemment et non encore libéré.
+4. Libre - Mémoire disponible non encore utilisée par Opie ou par les applications en cours.</translation>
     </message>
     <message>
         <source>Total Memory: %1 kB</source>
-        <translation type="unfinished"></translation>
+        <translation>Mémoire totale : %1 Ko</translation>
     </message>
     <message>
         <source>Used (%1 kB)</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisée (%1 Ko)</translation>
     </message>
     <message>
         <source>Buffers (%1 kB)</source>
-        <translation type="unfinished"></translation>
+        <translation>Buffers (%1 Ko)</translation>
     </message>
     <message>
         <source>Cached (%1 kB)</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache (%1 kB)</translation>
     </message>
     <message>
         <source>Free (%1 kB)</source>
-        <translation type="unfinished"></translation>
+        <translation>Libre (%1 kB)</translation>
     </message>
     <message>
         <source>Total Swap: %1 kB</source>
-        <translation type="unfinished"></translation>
+        <translation>Echange totale :  %1 kB</translation>
     </message>
 </context>
 <context>
     <name>MemoryStatus</name>
     <message>
         <source>Memory Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Status mémoire</translation>
     </message>
     <message>
         <source>Memory</source>
-        <translation type="unfinished"></translation>
+        <translation>Mémoire</translation>
     </message>
     <message>
         <source>Swapfile</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier d&apos;échange</translation>
     </message>
     <message>
         <source>&lt;center&gt;&lt;b&gt;Memory Monitor Plugin&lt;/b&gt;&lt;br&gt;Copyright (C) 2003 Anton Maslovsky&lt;br&gt;&amp;lt;&lt;a href=&quot;mailto:my-zaurus@narod.ru&quot;&gt;my-zaurus@narod.ru&lt;/a&gt;&amp;gt;&lt;br&gt;&lt;a href=&quot;http://my-zaurus.narod.ru&quot;&gt;http://my-zaurus.narod.ru&lt;/a&gt;&lt;br&gt;Based on source code from:&lt;br&gt; qswap (udoseidel@gmx.de) &lt;br&gt; Battery Applet (trolltech.com) &lt;br&gt; SysInfo (OPIE)&lt;br&gt;&lt;br&gt;This program is licensed under GNU GPL.&lt;/center&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;center&gt;&lt;b&gt;Plugin Info Mémoire&lt;/b&gt;&lt;br&gt;Copyright (C) 2003 Anton Maslovsky&lt;br&gt;&amp;lt;&lt;a href=&quot;mailto:my-zaurus@narod.ru&quot;&gt;my-zaurus@narod.ru&lt;/a&gt;&amp;gt;&lt;br&gt;&lt;a href=&quot;http://my-zaurus.narod.ru&quot;&gt;http://my-zaurus.narod.ru&lt;/a&gt;&lt;br&gt;Code source basé sur :&lt;br&gt; qswap (udoseidel@gmx.de) &lt;br&gt; Applet Batterie (trolltech.com) &lt;br&gt; SysInfo (OPIE)&lt;br&gt;&lt;br&gt;Ce programme est sous licence GNU GPL.&lt;/center&gt;</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>A propos</translation>
     </message>
 </context>
 <context>
     <name>Swapfile</name>
     <message>
         <source>Swapfile location</source>
-        <translation type="unfinished"></translation>
+        <translation>Chemin du fichier d&apos;échange</translation>
     </message>
     <message>
         <source>RAM</source>
-        <translation type="unfinished"></translation>
+        <translation>RAM</translation>
     </message>
     <message>
         <source>CF Card</source>
-        <translation type="unfinished"></translation>
+        <translation>Carte CF</translation>
     </message>
     <message>
         <source>SD Card</source>
-        <translation type="unfinished"></translation>
+        <translation>Carte SD</translation>
     </message>
     <message>
         <source> On </source>
-        <translation type="unfinished"></translation>
+        <translation>Activé</translation>
     </message>
     <message>
         <source> Off </source>
-        <translation type="unfinished"></translation>
+        <translation>Désactivé</translation>
     </message>
     <message>
         <source>Manage Swapfile</source>
-        <translation type="unfinished"></translation>
+        <translation>Gérer fichier d&apos;échange</translation>
     </message>
     <message>
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation>Générer</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer</translation>
     </message>
     <message>
         <source>2 Mb,4 Mb,6 Mb,8 Mb</source>
-        <translation type="unfinished"></translation>
+        <translation>2 Mb, 4 Mb, 6 Mb, 8 Mb</translation>
     </message>
     <message>
         <source>Failed to detach swapfile.</source>
-        <translation type="unfinished"></translation>
+        <translation>Echec lors de la détachement du fichier d&apos;échange.</translation>
     </message>
     <message>
         <source>Swapfile deactivated.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier d&apos;échange désactivé.</translation>
     </message>
     <message>
         <source>Failed to create swapfile.</source>
-        <translation type="unfinished"></translation>
+        <translation>Echec de création du fichier d&apos;échange.</translation>
     </message>
     <message>
         <source>Failed to initialize swapfile.</source>
-        <translation type="unfinished"></translation>
+        <translation>Echec d&apos;initialisation du fichier d&apos;échange.</translation>
     </message>
     <message>
         <source>Swapfile created.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier d&apos;échange crée.</translation>
     </message>
     <message>
         <source>Failed to remove swapfile.</source>
-        <translation type="unfinished"></translation>
+        <translation>Echec de suppression du fichier d&apos;échange.</translation>
     </message>
     <message>
         <source>Swapfile removed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier d&apos;échange supprimé.</translation>
     </message>
     <message>
         <source>Swapfile activated.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichier d&apos;échange activé.</translation>
     </message>
 </context>
 </TS>
