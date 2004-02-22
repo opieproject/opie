94daafcf2c1804f0e70442362156f01cc3a53551
Dan Williams <drw392772@gmail.com>
2004-02-22 00:39:48 +0000

    Backup: libopie->libopie2

--- noncore/settings/backup/main.cpp
+++ noncore/settings/backup/main.cpp
@@ -1,7 +1,7 @@
 #include "backuprestore.h"
 #include "qnetworkprotocol.h"
 #include <qpe/qpeapplication.h>
-#include <opie/oapplicationfactory.h>
+#include <opie2/oapplicationfactory.h>
 
 
 OPIE_EXPORT_APP( OApplicationFactory<BackupAndRestore> )
