4a6036fcd92871917161aedc81a7261aef5d8fea
Rajko Albrecht <alwin@handhelds.org>
2004-02-24 02:15:14 +0000

    user gets a message why this dialog appears

--- core/launcher/mediadlg.cpp
+++ core/launcher/mediadlg.cpp
@@ -3,6 +3,7 @@
 #include <qtopia/storage.h>
 #include <qpixmap.h>
 #include <qlayout.h>
+#include <qlabel.h>
 
 Mediadlg::Mediadlg(FileSystem*fs,QWidget *parent, const char *name,  bool modal, WFlags fl)
     : QDialog(parent,name,modal,fl)
@@ -19,9 +20,16 @@ Mediadlg::~Mediadlg()
 void Mediadlg::init()
 {
     if (!m_fs) return;
-    m_lay = new QVBoxLayout( this );
+    m_lay = new QVBoxLayout( this);
+    m_lay->setSpacing(0);
+    m_lay->setMargin(0);
+    QLabel*tLabel = new QLabel(this);
+    tLabel->setText("<center>"+tr("A new storage media detected:")+"<br>"+m_fs->name()+"<br>"+tr("What should I do with it?")+"</center>");
+    m_lay->addWidget(tLabel);
     m_widget = new MediumMountSetting::MediumMountWidget(m_fs->path(),QPixmap(),this);
     m_lay->addWidget(m_widget);
+    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    m_lay->addItem(spacer);
 }
 
 void Mediadlg::accept()
