1a417e51b142ae96a3287e576dbab979315506e2
Sergei Klink <sudonix@handhelds.org>
2004-02-28 22:41:36 +0000

    KBill translated

--- i18n/ru/kbill.ts
+++ i18n/ru/kbill.ts
@@ -3,62 +3,62 @@
     <name>KBill</name>
     <message>
         <source>kBill</source>
-        <translation type="unfinished"></translation>
+        <translation>кБилл</translation>
     </message>
     <message>
         <source>New game</source>
-        <translation type="unfinished"></translation>
+        <translation>Новая игра</translation>
     </message>
     <message>
         <source>Pause game</source>
-        <translation type="unfinished"></translation>
+        <translation>Пауза</translation>
     </message>
     <message>
         <source>Story of kBill</source>
-        <translation type="unfinished"></translation>
+        <translation>История кБилла</translation>
     </message>
     <message>
         <source>Rules</source>
-        <translation type="unfinished"></translation>
+        <translation>Правила</translation>
     </message>
     <message>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Справка</translation>
     </message>
     <message>
         <source>Pause Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Пауза</translation>
     </message>
     <message>
         <source>&lt;b&gt;The Story&lt;/b&gt;&lt;p&gt;Yet again, the fate of the world rests in your hands!  An evil computer hacker, known only by his handle &apos;Bill&apos;, has created the ultimate computer virus.  A virus so powerful that it has the power to transmute an ordinary computer into a toaster oven.  (oooh!) &apos;Bill&apos; has cloned himself into a billion-jillion micro-Bills.  Their sole purpose is to deliver the nefarious virus, which has been cleverly diguised as a popular operating system. As System Administrator and Exterminator, your job is to keep Bill from succeeding at his task.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;История&lt;/b&gt;&lt;p&gt;И снова судьба мира в ваших руках!  Злобный кракер, известный только по его кличке &apos;Билл&apos;, создал идеальный компьютерный вирус.  Вирус, который может запросто превратить обычный компьютер в тостер.  (ооо!) &apos;Билл&apos; склонировал себя в миллион-зиллион микро-Биллов.  Их единственная цель - занести этот мерзкий вирус, который был искусно замаскирован под популярную операционную системы. Ваша задача как Системного Администратора и Истребителя - предотвратить Билла от успешного завершения его черного дела.</translation>
     </message>
     <message>
         <source>The story of KBill</source>
-        <translation type="unfinished"></translation>
+        <translation>История КБилла</translation>
     </message>
     <message>
         <source>The rules of KBill</source>
-        <translation type="unfinished"></translation>
+        <translation>Правила игры КБилла</translation>
     </message>
     <message>
         <source>&lt;b&gt;The Rules&lt;/b&gt;&lt;p&gt;kBill has been painstakingly designed and researched in order to make it as easy to use for the whole family as it is for little Sally. Years - nay - days of beta testing and consulting with the cheapest of human interface designers have resulted in a game that is easy to use, yet nothing at all like a Macintosh.&lt;p&gt;&lt;UL&gt;&lt;LI&gt;Whack the Bills (click)&lt;/LI&gt;&lt;LI&gt;Restart the computer (click)&lt;/LI&gt;&lt;LI&gt;Pick up stolen OSes &amp; return (drag) them to their respective computers&lt;/LI&gt;&lt;LI&gt;Drag the bucket to extinguish sparks&lt;/LI&gt;&lt;LI&gt;Scoring is based on total uptime, with bonuses for killing Bills.&lt;/LI&gt;&lt;/UL&gt;&lt;P&gt;As for the rest, you can probably work it out for yourself.  We did, so it can&apos;t be too hard</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Правила Игры&lt;/b&gt;&lt;p&gt;кБилл был усердно разработан и продуман чтобы играть в него было также просто для всей семьи как для маленькой Салли. Года - не - дни бета-тестинга и консультации с самыми дешевыми разработчиками интерфейса вылились в игру которая легко используема, в тоже время в которой ничего нет, прямо как Макинтош.&lt;p&gt;&lt;UL&gt;&lt;LI&gt;Давите Биллов (щелчками)&lt;/LI&gt;&lt;LI&gt;Перезагружайте компьютеры (щелчками)&lt;/LI&gt;&lt;LI&gt;Подбирайте утащенные ОСи и возвращайте (перетаскивая) их на место&lt;/LI&gt;&lt;LI&gt;Перетаскивайте ведро чтобы тушить искры&lt;/LI&gt;&lt;LI&gt;Сбор очков основан на общем времени работы систем, с бонусами за убиение Биллов.&lt;/LI&gt;&lt;/UL&gt;&lt;P&gt;Все остальное разбирайтесь сами.  Мы смогли, так что это не так уж сложно</translation>
     </message>
 </context>
 <context>
     <name>UI</name>
     <message>
         <source>After Level</source>
-        <translation type="unfinished"></translation>
+        <translation>После уровня</translation>
     </message>
     <message>
         <source>Your score</source>
-        <translation type="unfinished"></translation>
+        <translation>Ваши очки</translation>
     </message>
 </context>
 </TS>
