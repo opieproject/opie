15cab0d1e61380c7384d7e1da0c4e7787aaaa968
Michael Lauer <mickey@vanille-media.de>
2004-03-05 21:40:26 +0000

    add skeleton files for the new packet view window (will replace the hex window)

---
+++ noncore/net/wellenreiter/gui/packetview.h
@@ -0,0 +1,40 @@
+/**********************************************************************
+** Copyright (C) 2002-2004 Michael 'Mickey' Lauer.  All rights reserved.
+**
+** This file is part of Wellenreiter II.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+**********************************************************************/
+
+#ifndef PACKETVIEW_H
+#define PACKETVIEW_H
+
+#include <qvbox.h>
+
+class QString;
+class QMultiLineEdit;
+
+class PacketView: public QVBox
+{
+
+  public:
+    PacketView( QWidget * parent = 0, const char * name = "PacketView", WFlags f = 0 );
+
+    void log( const QString& text );
+    const QString getLog() const;
+    void clear();
+
+  protected:
+    QMultiLineEdit* ledit;
+
+};
+
+#endif
+
