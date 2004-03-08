0fbd42a73b2e27137baf6feeb38ae77dc98b8c00
Michael Lauer <mickey@vanille-media.de>
2004-03-08 15:33:03 +0000

    first shot at a neat little about applet. text in about box blatantly stolen from the KDE about box. authors taken from AUTHORS file in cvs root directory. Add yourself if you're missing. Note: The dialog looks better when viewed in 320x240 or larger (as opposed to 240x320) because of OpieZilla ;)

---
+++ core/applets/aboutapplet/about.cpp
@@ -0,0 +1,89 @@
+#include "about.h"
+#include "dialog.h"
+
+#include <qpe/resource.h>
+#include <qpe/qcopenvelope_qws.h>
+
+#include <qapplication.h>
+#include <qlabel.h>
+
+AboutApplet::AboutApplet ( )
+        : QObject ( 0, "AboutApplet" )
+{}
+
+AboutApplet::~AboutApplet ( )
+{}
+
+int AboutApplet::position ( ) const
+{
+    return 1;
+}
+
+QString AboutApplet::name ( ) const
+{
+    return tr( "About shortcut" );
+}
+
+QString AboutApplet::text ( ) const
+{
+    return tr( "About" );
+}
+
+QString AboutApplet::tr( const char* s ) const
+{
+    return qApp->translate( "AboutApplet", s, 0 );
+}
+
+QString AboutApplet::tr( const char* s, const char* p ) const
+{
+    return qApp->translate( "AboutApplet", s, p );
+}
+
+QIconSet AboutApplet::icon ( ) const
+{
+    QPixmap pix;
+    QImage img = Resource::loadImage ( "about" );
+
+    if ( !img. isNull ( ) )
+        pix. convertFromImage ( img. smoothScale ( 14, 14 ) );
+    return pix;
+}
+
+QPopupMenu *AboutApplet::popup ( QWidget * ) const
+{
+    return 0;
+}
+
+void AboutApplet::activated()
+{
+    AboutDialog* d = new AboutDialog( qApp->mainWidget(), "aboutDialog", true );
+    if ( qApp->desktop()->width() == 240 )
+    {
+        d->logo->hide();
+        d->setFixedWidth( qApp->desktop()->width() - 5 );
+        d->setFixedHeight( qApp->desktop()->height() - 50 );
+    }
+    d->exec();
+}
+
+QRESULT AboutApplet::queryInterface ( const QUuid &uuid, QUnknownInterface **iface )
+{
+    *iface = 0;
+    if ( uuid == IID_QUnknown )
+        * iface = this;
+    else if ( uuid == IID_MenuApplet )
+        * iface = this;
+    else
+        return QS_FALSE;
+
+    if ( *iface )
+        ( *iface ) -> addRef ( );
+    return QS_OK;
+}
+
+Q_EXPORT_INTERFACE( )
+{
+    Q_CREATE_INSTANCE( AboutApplet )
+}
+
+
---
+++ core/applets/aboutapplet/aboutapplet.pro
@@ -0,0 +1,14 @@
+TEMPLATE      = lib
+CONFIG       += qt plugin warn_on release
+HEADERS       = about.h
+SOURCES       = about.cpp
+INTERFACES    = dialog.ui
+TARGET        = aboutapplet
+DESTDIR       = $(OPIEDIR)/plugins/applets
+INCLUDEPATH  += $(OPIEDIR)/include
+DEPENDPATH   += $(OPIEDIR)/include
+LIBS         += -lqpe
+VERSION       = 1.0.0
+
+include ( $(OPIEDIR)/include.pro )
+target.path = $$prefix/plugins/applets
