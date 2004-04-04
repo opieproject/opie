0c175a9d13672e6c73f8e4366deb9938add362ee
Gints Polis <gints.polis@cc.lv>
2004-04-04 11:09:03 +0000

    *** empty log message ***

--- i18n/lv/aqpkg.ts
+++ i18n/lv/aqpkg.ts
@@ -117,266 +117,267 @@
     <name>InstallOptionsDlgImpl</name>
     <message>
         <source>Options</source>
-        <translation type="unfinished">Opcijas</translation>
+        <translation>Opcijas</translation>
     </message>
     <message>
         <source>Force Depends</source>
-        <translation type="unfinished"></translation>
+        <translation>Piespiedu Atkarības</translation>
     </message>
     <message>
         <source>Force Reinstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Piespiedu Pārinstalācija</translation>
     </message>
     <message>
         <source>Force Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Piespiedu Izņemšana</translation>
     </message>
     <message>
         <source>Force Overwrite</source>
-        <translation type="unfinished"></translation>
+        <translation>Piespiedu Pārrakstīšana</translation>
     </message>
     <message>
         <source>Information Level</source>
-        <translation type="unfinished"></translation>
+        <translation>Informācijas Līmenis</translation>
     </message>
     <message>
         <source>Errors only</source>
-        <translation type="unfinished"></translation>
+        <translation>Tikai kļūdas</translation>
     </message>
     <message>
         <source>Normal messages</source>
-        <translation type="unfinished"></translation>
+        <translation>Normālas ziņas</translation>
     </message>
     <message>
         <source>Informative messages</source>
-        <translation type="unfinished"></translation>
+        <translation>Informatīvās ziņas</translation>
     </message>
     <message>
         <source>Troubleshooting output</source>
-        <translation type="unfinished"></translation>
+        <translation>Problēmu izvads</translation>
     </message>
 </context>
 <context>
     <name>Ipkg</name>
     <message>
         <source>Dealing with package %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Darbojos ar paku %1</translation>
     </message>
     <message>
         <source>Removing symbolic links...
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemu simboliskās saites...
+</translation>
     </message>
     <message>
         <source>Creating symbolic links for %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Veidoju simboliskās saites priekš %1.</translation>
     </message>
     <message>
         <source>Creating symbolic links for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Veidoju simboliskās saites priekš %1</translation>
     </message>
     <message>
         <source>Finished</source>
-        <translation type="unfinished"></translation>
+        <translation>Pabeigts</translation>
     </message>
     <message>
         <source>Removing status entry...</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemu statusa ierakstu...</translation>
     </message>
     <message>
         <source>status file - </source>
-        <translation type="unfinished"></translation>
+        <translation>statusa fails -</translation>
     </message>
     <message>
         <source>package - </source>
-        <translation type="unfinished"></translation>
+        <translation>paka -</translation>
     </message>
     <message>
         <source>Couldn&apos;t open status file - </source>
-        <translation type="unfinished"></translation>
+        <translation>Nevaru atvērt statusa failu -</translation>
     </message>
     <message>
         <source>Couldn&apos;t create tempory status file - </source>
-        <translation type="unfinished"></translation>
+        <translation>Nevaru izveidot pagaidu statusa failu -</translation>
     </message>
     <message>
         <source>Couldn&apos;t start ipkg process</source>
-        <translation type="unfinished"></translation>
+        <translation>Nevaru iestartēt ipkg procesu</translation>
     </message>
     <message>
         <source>Could not open :</source>
-        <translation type="unfinished"></translation>
+        <translation>Nevaru atvērt :</translation>
     </message>
     <message>
         <source>Creating directory </source>
-        <translation type="unfinished"></translation>
+        <translation>Veidoju direktoriju</translation>
     </message>
     <message>
         <source>Linked %1 to %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Saistīts %1 ar %2</translation>
     </message>
     <message>
         <source>Failed to link %1 to %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Neizdevās saite %1 uz %2</translation>
     </message>
     <message>
         <source>Removed %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemts %1</translation>
     </message>
     <message>
         <source>Failed to remove %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Neizdevās izņemt %1</translation>
     </message>
     <message>
         <source>Removed </source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemts</translation>
     </message>
     <message>
         <source>Failed to remove </source>
-        <translation type="unfinished"></translation>
+        <translation>Neizdevās izņemt</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>AQPkg - Package Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>AQPkg - Paku Menedžeris</translation>
     </message>
     <message>
         <source>Type the text to search for here.</source>
-        <translation type="unfinished"></translation>
+        <translation>Šeit rakstiet tekstu meklēšanai.</translation>
     </message>
     <message>
         <source>Click here to hide the Quick Jump toolbar.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai paslēptu Ātrās Pārvietošanās instrumentjoslu.</translation>
     </message>
     <message>
         <source>Update lists</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjaunot sarakstus</translation>
     </message>
     <message>
         <source>Click here to update package lists from servers.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit, lai no serveriem atjauninātu paku sarakstus.</translation>
     </message>
     <message>
         <source>Upgrade</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjaunināt</translation>
     </message>
     <message>
         <source>Click here to upgrade all installed packages if a newer version is available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai atjauninātu instalētās pakas ja ir pieejama jaunāka versija.</translation>
     </message>
     <message>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Lejuplādēt</translation>
     </message>
     <message>
         <source>Click here to download the currently selected package(s).</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai lejuplādētu iezīmēto paku(as).</translation>
     </message>
     <message>
         <source>Apply changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzlikt izmaiņas</translation>
     </message>
     <message>
         <source>Click here to install, remove or upgrade currently selected package(s).</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai instalētu, izņemtu vai atjauninātu iezīmēto paku(as).</translation>
     </message>
     <message>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfigurēt</translation>
     </message>
     <message>
         <source>Click here to configure this application.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai konfigurētušo aplikāciju.</translation>
     </message>
     <message>
         <source>Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>Darbības</translation>
     </message>
     <message>
         <source>Show packages not installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Rādīt pakas kuras nav instalētas</translation>
     </message>
     <message>
         <source>Click here to show packages available which have not been installed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai rādītu pakas kuras ir pieejamas bet vēl nav instalētas.</translation>
     </message>
     <message>
         <source>Show installed packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Rādīt instalētās pakas</translation>
     </message>
     <message>
         <source>Click here to show packages currently installed on this device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai rādītu pakas kuras ir instalētas.</translation>
     </message>
     <message>
         <source>Show updated packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Rādīt atjauninātās pakas</translation>
     </message>
     <message>
         <source>Click here to show packages currently installed on this device which have a newer version available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai rādītu pakas kuras ir instalētas un kurām ir pieejama jaunāka versija.</translation>
     </message>
     <message>
         <source>Filter by category</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrēt pēc kategorijas</translation>
     </message>
     <message>
         <source>Click here to list packages belonging to one category.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai uzrādītu pakas kuras pieder vienai kategorijai.</translation>
     </message>
     <message>
         <source>Set filter category</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzstādīt kategorijas filtru</translation>
     </message>
     <message>
         <source>Click here to change package category to used filter.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit, lai mainītu paku filtra kategoriju.</translation>
     </message>
     <message>
         <source>Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Meklēt</translation>
     </message>
     <message>
         <source>Click here to search for text in package names.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai meklētu pakas pēc nosaukuma.</translation>
     </message>
     <message>
         <source>Find next</source>
-        <translation type="unfinished"></translation>
+        <translation>Meklēt nākamo</translation>
     </message>
     <message>
         <source>Click here to find the next package name containing the text you are searching for.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai meklētu nākamo paku kuras nosaukums satur norādīto tekstu.</translation>
     </message>
     <message>
         <source>Quick Jump keypad</source>
-        <translation type="unfinished"></translation>
+        <translation>Ātrās Pārvietošanāš pogas</translation>
     </message>
     <message>
         <source>Click here to display/hide keypad to allow quick movement through the package list.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai parādītu/paslēptu taustiņus kuri ļauj ātri pārvietoties pa paku sarakstu.</translation>
     </message>
     <message>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Skats</translation>
     </message>
     <message>
         <source>Click here to hide the find toolbar.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai paslēptu meklēšanas instrumentu paneli.</translation>
     </message>
     <message>
         <source>Servers:</source>
-        <translation type="unfinished"></translation>
+        <translation>Serveri:</translation>
     </message>
     <message>
         <source>Click here to select a package feed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai atzīmētu paku avotu.</translation>
     </message>
     <message>
         <source>Packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakas</translation>
     </message>
     <message>
         <source>This is a listing of all packages for the server feed selected above.
@@ -386,29 +387,37 @@ A blue dot next to the package name indicates that the package is currently inst
 A blue dot with a star indicates that a newer version of the package is available from the server feed.
 
 Click inside the box at the left to select a package.</source>
-        <translation type="unfinished"></translation>
+        <translation>Šis ir uzrādītā servera visu paku saraksts.
+
+Zilais punkts pie pakas vārda norāda ka paka jau ir uzinstalēta
+
+Zilais punkts ar zvaigznīti norāda ka šai pakai dotajā serverī ir pieejams jauninājums.
+
+Klikšķiniet pa kreisi no pakas, lai to atzīmētu.</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt</translation>
     </message>
     <message>
         <source>Click here to uninstall the currently selected package(s).</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai izņemtu iezīmēto paku(as).</translation>
     </message>
     <message>
         <source>Building server list:
 <byte value="x9"/>%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Veidoju serveru sarakstu:
+ %1</translation>
     </message>
     <message>
         <source>Building package list for:
 <byte value="x9"/>%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Veidoju paku sarakstu priekš:
+  %1</translation>
     </message>
     <message>
         <source>Refreshing server package lists</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjaunoju serveru paku sarakstu</translation>
     </message>
     <message>
         <source>WARNING: Upgrading while
@@ -417,86 +426,94 @@ is NOT recommended!
 
 Are you sure?
 </source>
-        <translation type="unfinished"></translation>
+        <translation>UZMANĪBU: Atjaunināšana
+Opie/Qtopia darbības laikā
+NAV rekomendēta!
+
+Vai esiet pārliecināts?
+</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Brīdinājums</translation>
     </message>
     <message>
         <source>Upgrading installed packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjauninu instalētās pakas</translation>
     </message>
     <message>
         <source>Are you sure you wish to delete
 %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vai esiet pārliecināti ka vēlates dzēst
+%1 ?</translation>
     </message>
     <message>
         <source>Are you sure?</source>
-        <translation type="unfinished"></translation>
+        <translation>Esie pārliecināts?</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nē</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Jā</translation>
     </message>
     <message>
         <source>Download to where</source>
-        <translation type="unfinished"></translation>
+        <translation>Lejuplādēt uz</translation>
     </message>
     <message>
         <source>Enter path to download to</source>
-        <translation type="unfinished"></translation>
+        <translation>Norādiet ceļu kur lejuplādēt</translation>
     </message>
     <message>
         <source>Install Remote Package</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalēt Attālināto Paku</translation>
     </message>
     <message>
         <source>Enter package location</source>
-        <translation type="unfinished"></translation>
+        <translation>Ievadiet pakas atrašanās vietu</translation>
     </message>
     <message>
         <source>Nothing to do</source>
-        <translation type="unfinished"></translation>
+        <translation>Nav ko darīt</translation>
     </message>
     <message>
         <source>No packages selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Nav atzīmētu paku</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>Labi</translation>
     </message>
     <message>
         <source>Do you wish to remove or reinstall
 %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vai vēlaties izņemt vai pārinstalēt
+%1?</translation>
     </message>
     <message>
         <source>Remove or ReInstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt vai Pārinstalēt</translation>
     </message>
     <message>
         <source>ReInstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārinstalēt</translation>
     </message>
     <message>
         <source>Do you wish to remove or upgrade
 %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Vai vēlaties izņemt vai atjaunināt 
+%1?</translation>
     </message>
     <message>
         <source>Remove or Upgrade</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt vai Atjaunināt</translation>
     </message>
     <message>
         <source>Updating Launcher...</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjauninu Palaidēju...</translation>
     </message>
 </context>
 <context>
