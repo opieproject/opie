ace85adf57c97eb943933e7325ba8ee3e0e05525
Gints Polis <gints.polis@cc.lv>
2004-04-04 19:20:23 +0000

    *** empty log message ***

--- i18n/lv/citytime.ts
+++ i18n/lv/citytime.ts
@@ -3,69 +3,69 @@
     <name>CityTime</name>
     <message>
         <source>Time Changing</source>
-        <translation type="unfinished"></translation>
+        <translation>Laika Maiņa</translation>
     </message>
     <message>
         <source>There was a problem setting timezone %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Bija problēma uzstādot laika zonu %1</translation>
     </message>
     <message>
         <source>Restore Time Zone</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjaunot Laika Zonu</translation>
     </message>
     <message>
         <source>There was a problem setting your timezone.Your time may be wrong now...</source>
-        <translation type="unfinished"></translation>
+        <translation>Bija problēma uzstādot laika zonu. Jūsu laiks tagad var būt nepareizs...</translation>
     </message>
 </context>
 <context>
     <name>CityTimeBase</name>
     <message>
         <source>City Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilsētas Laiks</translation>
     </message>
     <message>
         <source>Click on one of the set cities to replace it</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet uz vienu no pilsētām, lai to aizvietotu</translation>
     </message>
     <message>
         <source></source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>ZoneMap</name>
     <message>
         <source>CITY</source>
-        <translation type="unfinished"></translation>
+        <translation>PILSĒTA</translation>
     </message>
     <message>
         <source>Unable to Find Timezone Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Nevaru Atrast Laika zonas Info</translation>
     </message>
     <message>
         <source>&lt;p&gt;Unable to find any timezone information in %1</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Nevaru atrast laika zonas informāciju iekš %1</translation>
     </message>
     <message>
         <source>Continent</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontinents</translation>
     </message>
     <message>
         <source>Select a continent/country here, then select a city</source>
-        <translation type="unfinished"></translation>
+        <translation>Šeit izvēlaties kontinentu/valsti, un tad izvēlaties pilsētu</translation>
     </message>
     <message>
         <source>City</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilsēta</translation>
     </message>
     <message>
         <source>Couldn&apos;t Find Map</source>
-        <translation type="unfinished"></translation>
+        <translation>Nevaru Atrast Karti</translation>
     </message>
     <message>
         <source>&lt;p&gt;Couldn&apos;t load map: %1, exiting</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Nevaru ielādēt karti: %1, pārtraucu darbu</translation>
     </message>
 </context>
 </TS>
