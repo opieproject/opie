07b2fa44ba2c405cf113a325e201af05061a5a8b
Gints Polis <gints.polis@cc.lv>
2004-04-10 11:52:28 +0000

    Today in Latvian

--- i18n/lv/today.ts
+++ i18n/lv/today.ts
@@ -3,132 +3,132 @@
     <name>Today</name>
     <message>
         <source>Owned by </source>
-        <translation type="unfinished"></translation>
+        <translation>Īpašnieks</translation>
     </message>
     <message>
         <source>Please fill out the business card</source>
-        <translation type="unfinished"></translation>
+        <translation>Lūdzu aizpildiet savu vizītkarti</translation>
     </message>
     <message>
         <source>Click here to launch the associated app</source>
-        <translation type="unfinished"></translation>
+        <translation>Klišķiniet šeit lai palaistu saistīto aplikāciju</translation>
     </message>
     <message>
         <source>No plugins found</source>
-        <translation type="unfinished"></translation>
+        <translation>Nav atrasti spraudņi</translation>
     </message>
     <message>
         <source>No plugins activated</source>
-        <translation type="unfinished"></translation>
+        <translation>Nav aktviēti spraudņi</translation>
     </message>
 </context>
 <context>
     <name>TodayBase</name>
     <message>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Šodien</translation>
     </message>
     <message>
         <source>Today by Maximilian Rei&#xdf;</source>
-        <translation type="unfinished"></translation>
+        <translation>Šodien - Maximilian Reiß</translation>
     </message>
     <message>
         <source>Click here to get to the config dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet šeit lai iegūtu konfigurēšanas dialogu</translation>
     </message>
 </context>
 <context>
     <name>TodayConfig</name>
     <message>
         <source>Today Config</source>
-        <translation type="unfinished"></translation>
+        <translation>Šodien Konfigurācija</translation>
     </message>
     <message>
         <source>Load which plugins in what order:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kādā kārtībā ielādēt kādus spraudņus:</translation>
     </message>
     <message>
         <source>Check a checkbox to activate/deactivate a plugin or use the arrow buttons on the right to change the appearance order</source>
-        <translation type="unfinished"></translation>
+        <translation>Klikšķiniet uz izvēles rūtiņas lai aktivizētu/deaktivizētu spraudni vai arī lietojiet bultiņas lai mainītu attēlošanas kārtību</translation>
     </message>
     <message>
         <source>Move Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārvietot uz Augšu</translation>
     </message>
     <message>
         <source>Move Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārvietot uz Leju</translation>
     </message>
     <message>
         <source>active/order</source>
-        <translation type="unfinished"></translation>
+        <translation>aktīvs/kārtība</translation>
     </message>
     <message>
         <source>Misc</source>
-        <translation type="unfinished"></translation>
+        <translation>Dažādi</translation>
     </message>
 </context>
 <context>
     <name>TodayConfigMiscBase</name>
     <message>
         <source>Form1</source>
-        <translation type="unfinished"></translation>
+        <translation>Forma1</translation>
     </message>
     <message>
         <source>Check this if today should be autostarted on resume.</source>
-        <translation type="unfinished"></translation>
+        <translation>Atķeksējiet šo ja vēlaties lai šodien tiktu parādīts pēc ieslēgšanas.</translation>
     </message>
     <message>
         <source>autostart on resume?</source>
-        <translation type="unfinished"></translation>
+        <translation>autorstartēt pēc atjaunošanas?</translation>
     </message>
     <message>
         <source>tiny banner</source>
-        <translation type="unfinished"></translation>
+        <translation>sīkais baneris</translation>
     </message>
     <message>
         <source>Have small banner </source>
-        <translation type="unfinished"></translation>
+        <translation>Būs mazs baneris</translation>
     </message>
     <message>
         <source> min</source>
-        <translation type="unfinished"></translation>
+        <translation>min</translation>
     </message>
     <message>
         <source>How many minutes has the PDA been suspended before the autostart feature kicks in on resume</source>
-        <translation type="unfinished"></translation>
+        <translation>Cik minūtes PDA jābūt iemigušam pirms autostartēšanas iespēja iedarbojas pie atjaunošanas</translation>
     </message>
     <message>
         <source>minutes inactive</source>
-        <translation type="unfinished"></translation>
+        <translation>neaktivitātes minūtes</translation>
     </message>
     <message>
         <source> pixel</source>
-        <translation type="unfinished"></translation>
+        <translation>pikseļi</translation>
     </message>
     <message>
         <source>Set the icon size in pixel</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzstādīt ikonas izmēru pikseļos</translation>
     </message>
     <message>
         <source>icon size</source>
-        <translation type="unfinished"></translation>
+        <translation>ikonas izmērs</translation>
     </message>
     <message>
         <source> sec</source>
-        <translation type="unfinished"></translation>
+        <translation>sek</translation>
     </message>
     <message>
         <source>never</source>
-        <translation type="unfinished"></translation>
+        <translation>nekad</translation>
     </message>
     <message>
         <source>How often should Today refresh itself</source>
-        <translation type="unfinished"></translation>
+        <translation>Cik bieži Šodien vajag sevi atjaunot</translation>
     </message>
     <message>
         <source>refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>atjaunot</translation>
     </message>
 </context>
 </TS>
