220ec6664b940bce33c8f5fb8dd3eb12837ee5b6
Daniel Grabowski <daniel@handhelds.org>
2004-04-22 10:30:55 +0000

    All done with polish characters.

--- i18n/pl/security.ts
+++ i18n/pl/security.ts
@@ -3,100 +3,100 @@
     <name>Security</name>
     <message>
         <source>Set passcode</source>
-        <translation>Ustaw haslo</translation>
+        <translation>Ustaw hasło</translation>
     </message>
     <message>
         <source>Change passcode</source>
-        <translation>Zmien haslo</translation>
+        <translation>Zmień hasło</translation>
     </message>
     <message>
         <source>Enter passcode</source>
-        <translation>Podaj haslo</translation>
+        <translation>Podaj hasło</translation>
     </message>
     <message>
         <source>Passcode incorrect</source>
-        <translation>Haslo bledne</translation>
+        <translation>Błędne hasło</translation>
     </message>
     <message>
         <source>The passcode entered is incorrect.
 Access denied</source>
-        <translation>Haslo nieprawidlowe.
-Dostep zabroniony</translation>
+        <translation>Podane hasło jest nieprawidłowe.
+Dostęp zabroniony</translation>
     </message>
     <message>
         <source>Any</source>
-        <translation>Kazdy</translation>
+        <translation>Wszystkie</translation>
     </message>
     <message>
         <source>None</source>
-        <translation>Brak</translation>
+        <translation>Żadne</translation>
     </message>
     <message>
         <source>Enter new passcode</source>
-        <translation>Wpisz nowe haslo</translation>
+        <translation>Wpisz nowe hasło</translation>
     </message>
     <message>
         <source>Re-enter new passcode</source>
-        <translation>Powtorz nowe haslo</translation>
+        <translation>Powtórz nowe hasło</translation>
     </message>
     <message>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Uwaga</translation>
     </message>
     <message>
         <source>&lt;p&gt;All user-defined net ranges will be lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Wszystkie zakresy użytkownika będę stracone.</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Anuluj</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <source>192.168.129.0/24</source>
-        <translation type="unfinished">192.168.129.0/24</translation>
+        <translation>192.168.129.0/24</translation>
     </message>
     <message>
         <source>192.168.1.0/24</source>
-        <translation type="unfinished">192.168.1.0/24</translation>
+        <translation>192.168.1.0/24</translation>
     </message>
     <message>
         <source>192.168.0.0/16</source>
-        <translation type="unfinished">192.168.0.0/16</translation>
+        <translation>192.168.0.0/16</translation>
     </message>
     <message>
         <source>172.16.0.0/12</source>
-        <translation type="unfinished">172.16.0.0/12</translation>
+        <translation>172.16.0.0/12</translation>
     </message>
     <message>
         <source>10.0.0.0/8</source>
-        <translation type="unfinished">10.0.0.0/8</translation>
+        <translation>10.0.0.0/8</translation>
     </message>
     <message>
         <source>1.0.0.0/8</source>
-        <translation type="unfinished">1.0.0.0/8</translation>
+        <translation>1.0.0.0/8</translation>
     </message>
 </context>
 <context>
     <name>SecurityBase</name>
     <message>
         <source>Security Settings</source>
-        <translation>Ustawienia bezpieczenstwa</translation>
+        <translation>Ustawienia bezpieczeństwa</translation>
     </message>
     <message>
         <source>Change passcode</source>
-        <translation>Zmien haslo</translation>
+        <translation>Zmień hasło</translation>
     </message>
     <message>
         <source>Clear passcode</source>
-        <translation>Wyczysc haslo</translation>
+        <translation>Wyczyść hasło</translation>
     </message>
     <message>
         <source>Require pass code at power-on</source>
-        <translation>Wymaj hasla po uruchomieniu</translation>
+        <translation>Wymagaj hasła po uruchomieniu</translation>
     </message>
     <message>
         <source>Sync</source>
@@ -104,7 +104,7 @@ Dostep zabroniony</translation>
     </message>
     <message>
         <source>Accept sync from network:</source>
-        <translation>Akceptuj synchronizacje z sieci:</translation>
+        <translation>Akceptuj synchronizację z sieci:</translation>
     </message>
     <message>
         <source>192.168.0.0/16</source>
@@ -128,7 +128,7 @@ Dostep zabroniony</translation>
     </message>
     <message>
         <source>&lt;P&gt;Pass code protection provides a minimal level of protection from casual access to this device.</source>
-        <translation>&lt;P&gt;Haslo zabezpieczajace pozwala na minimalny poziom zabezpieczen do tego urzadzenia.</translation>
+        <translation>&lt;P&gt;Ochrona hasłem jest podstawowym zabezpieczeniem przed przypadkowym dostępem do tego urządzenia.</translation>
     </message>
     <message>
         <source>192.168.129.0/24 (default)</source>
@@ -144,7 +144,7 @@ Dostep zabroniony</translation>
     </message>
     <message>
         <source>Passcode</source>
-        <translation>Haslo</translation>
+        <translation>Hasło</translation>
     </message>
     <message>
         <source>Login</source>
@@ -158,29 +158,37 @@ Dostep zabroniony</translation>
         <source>This button will let you change the security passcode.  
 
 Note: This is *not* the sync password.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ten przycisk służy do zmiany hasła.
+
+Uwaga: To *nie* jest hasło synchronizacji.</translation>
     </message>
     <message>
         <source>Delete the current passcode.  
 You can enter a new one at any time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń aktualne hasło.
+W dowolnej chwili możesz podać nowe.</translation>
     </message>
     <message>
         <source>If you enable this option, a passcode must be entered after every resume event of your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Jeśli włączysz tą opcję, będzie trzeba podawać hasło po każdym rozbudzeniu Zaurusa.</translation>
     </message>
     <message>
         <source>If you select this option, opie-login will automagically log you in with the user you have selected in the drop-down list.  
 
 For the Zaurus 5x00 this is always the user &quot;root&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Jeśli wybierzesz tą opcję, będziesz automatycznie logowany jako użytkownik, którego wybrałeś z listy rozwijanej.
+
+Dla Zaurusa 5x00 jest to zawsze użytkownik &quot;root&quot;.</translation>
     </message>
     <message>
         <source>This drop-down list allows you to select the user for auto-login
 (if enabled above).  
 
 You can only select an actually configured user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ta lista rozwijana pozwala na wybór automatycznie logowanego użytkownika
+(jeśli opcja jest włączona powyżej).
+
+Możesz wybrać tylko skonfigurowanego użytkownika.</translation>
     </message>
     <message>
         <source>Select a net-range or enter a new one.  
@@ -193,11 +201,20 @@ The entry &quot;All&quot; will allow *any* IP to connect.
 The entry &quot;None&quot; will *deny* any connection.  
 
 If unsure, select &quot;Any&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz zakres sieciowy lub podaj nowy.
+
+Ta lista rozwijana służy do wyboru zakresu adresów, z których można łączyć się z Zaurusem.
+
+Na przykład zakres 192.168.1.0/24 pozwala na połączenie (i synchronizację) każdej maszynie z IP pomiędzy 192.168.1.1 i 192.168.1.254.
+
+&quot;Wszystkie&quot; pozwoli na łączenie z *każdego* IP.
+&quot;Żadne&quot; odmówi *każdemu* połączeniu.
+
+Jeśli nie jesteś pewien co wybrać, wybierz &quot;Wszystkie&quot;.</translation>
     </message>
     <message>
         <source>Delete Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń wpis</translation>
     </message>
     <message>
         <source>Delete the selected net range from the list
@@ -206,42 +223,54 @@ If you press this button, the currently selected net range will be deleted from 
 
 If you hit this button in error, the button &quot;Restore Defaults&quot; will restore
 the list to the state it has been when you initially booted your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń wybrany zakres sieciowy z listy
+
+Jeśli wciśniesz ten przycisk, aktualnie wybrany zakres sieciowy zostanie usunięty z listy zakresów sieciowych.
+
+Jeśli wciśniesz ten przycisk przez pomyłkę, to przycisk &quot;Przywróć domyślne&quot;
+przywróci listę do stanu w jakim była przy pierwszym starcie Zaurusa.</translation>
     </message>
     <message>
         <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation>Przywróć domyślne</translation>
     </message>
     <message>
         <source>This button will restore the list of net ranges
 to the defaults.
 
 Beware that all manually entered net ranges will be lost!</source>
-        <translation type="unfinished"></translation>
+        <translation>Ten przycisk przywróci listę zakresów sieciowych
+do stanu domyślnego.
+
+Uwaga, wszystkie ręcznie wprowadzone zakresy zostaną usunięte!</translation>
     </message>
     <message>
         <source>Select your sync software</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz oprogramowanie do synchronizacji</translation>
     </message>
     <message>
         <source>IntelliSync</source>
-        <translation type="unfinished"></translation>
+        <translation>IntelliSync</translation>
     </message>
     <message>
         <source>Qtopia 1.7</source>
-        <translation type="unfinished"></translation>
+        <translation>Qtopia 1.7</translation>
     </message>
     <message>
         <source>Opie 1.0</source>
-        <translation type="unfinished"></translation>
+        <translation>Opie 1.0</translation>
     </message>
     <message>
         <source>Alter the Sync Protocol to tweak for the different devices.
 <byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    IntelliSync will disable the password check but you&apos;ll be prompted to accept the connection.
 <byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    Opie1.0 will be compatible with QtopiaDesktop, MultiSync and KitchenSync
 <byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    Qtopia1.7 will alter the way data is send to the client and will not work with older clients.
-<byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    </source>
-        <translation type="unfinished"></translation>
+<byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/></source>
+        <translation>Zmień protokół synchronizacji aby go dostosować do różnych urządzeń.
+    IntelliSync wyłączy sprawdzanie hasła, ale zostaniesz poproszony o akceptację połączenia.
+    Opie1.0 będzie kompatybilne z QtopiaDesktop, MultiSync i KitchenSync.
+    Qtopia1.7 zmieni sposób wysyłania danych do klienta i nie będzie działać ze starszymi klientami.
+    </translation>
     </message>
 </context>
 </TS>
