b258ae040a37c124b5ebe75c8c92c0331494035b
Holger Freyther <zecke@handhelds.org>
2004-05-21 01:00:48 +0000

    No dynamic_cast if we've no rtti. Ask tronical about the correctness because if we would static_cast rightaway the compilers quits with errors..

--- libopie2/opiepim/core/opimaccessfactory.h
+++ libopie2/opiepim/core/opimaccessfactory.h
@@ -44,7 +44,7 @@ namespace Opie {
 /**
  * This class is our factory for creating PIM access objects. You should use these objects to load, store and search
  * information in our PIM databases. Currently we support objects for accessing the datebook, the contact- and the
- * todolist. 
+ * todolist.
  * @see OPimGlobal for more information of used values.
  *
  * This class - as the whole PIM Api - is making use of templates
@@ -61,9 +61,9 @@ namespace Opie {
 template<class T>
 class OPimAccessFactory
 {
- public: 
+ public:
 
-	// Maybe we should introduce a global class for storing such global enums 
+	// Maybe we should introduce a global class for storing such global enums
 	// (something like opimglobal.h) ? (eilers)
 
 	OPimAccessFactory() {};
@@ -78,31 +78,31 @@ class OPimAccessFactory
 	 * @see OPimGlobal
 	 */
 	static T* create( OPimGlobal::PimType type, OPimGlobal::DatabaseStyle dbStyle, const QString& appName ){
-
+            OPimBase *base;
 		switch ( type ){
 		case OPimGlobal::TODOLIST:
-			return dynamic_cast<T*>( new OPimTodoAccess( OBackendFactory<OPimTodoAccessBackend>::create( type, dbStyle, appName ) ) );
+		        base = new OPimTodoAccess( OBackendFactory<OPimTodoAccessBackend>::create( type, dbStyle, appName ) );
 		case OPimGlobal::CONTACTLIST:
-			return dynamic_cast<T*>( new OPimContactAccess( QString::null, QString::null, OBackendFactory<OPimContactAccessBackend>::create( type, dbStyle, appName ) ) );
+		        base = new OPimContactAccess( QString::null, QString::null, OBackendFactory<OPimContactAccessBackend>::create( type, dbStyle, appName ) );
 		case OPimGlobal::DATEBOOK:
-			return dynamic_cast<T*>( new ODateBookAccess( OBackendFactory<ODateBookAccessBackend>::create( type, dbStyle, appName ) ) );
+		        base = new ODateBookAccess( OBackendFactory<ODateBookAccessBackend>::create( type, dbStyle, appName ) );
 		default:
 			return 0l;
-			
 		}
+                return static_cast<T*>( base );
 	}
 
 
 	/**
 	 * Returns the selected PIM access-object, using the default database style
-	 * Which style is selected is defined in the configfile "pimaccess.conf" in 
+	 * Which style is selected is defined in the configfile "pimaccess.conf" in
 	 * the directory "Settings"
 	 * @param type Type of the selected database (addressbook, todolist or datebook)
 	 * @param appName "Name" of your application. This should be any constant string which is used
 	 *                by some backends for creating special files (i.e.journal files). Please keep the
 	 *                string unique for your application !
 	 * @see OPimGlobal
-	 * 
+	 *
 	 */
 	static T* defaultAccess( typename OPimGlobal::PimType type, const QString& appName ){
 
