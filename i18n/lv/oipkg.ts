556ac5cbda5e45495152758d568b1635f932bdd7
Gints Polis <gints.polis@cc.lv>
2004-06-07 16:10:19 +0000

    *** empty log message ***

--- i18n/lv/oipkg.ts
+++ i18n/lv/oipkg.ts
@@ -3,263 +3,263 @@
     <name>InstallDialog</name>
     <message>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalēt</translation>
     </message>
     <message>
         <source>Ipkg options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ipkg opcijas</translation>
     </message>
     <message>
         <source>-force-depends</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>-force-reinstall</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>-force-removal-of-essential-packages</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>-force-overwrite</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>To remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Lai izņemtu</translation>
     </message>
     <message>
         <source>To install</source>
-        <translation type="unfinished"></translation>
+        <translation>Lai Instalētu</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>Package Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakotņu Menedžeris</translation>
     </message>
     <message>
         <source>feeds</source>
-        <translation type="unfinished"></translation>
+        <translation>barotnes</translation>
     </message>
     <message>
         <source>ipkgfind&amp;killefiz</source>
-        <translation type="unfinished"></translation>
+        <translation>ipkgfind&amp;killefiz</translation>
     </message>
     <message>
         <source>documents</source>
-        <translation type="unfinished"></translation>
+        <translation>dokumenti</translation>
     </message>
     <message>
         <source>Package</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakotne</translation>
     </message>
     <message>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Skatīt</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzstādījumi</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Palīdzība</translation>
     </message>
     <message>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzstādīt</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Atjaunot</translation>
     </message>
     <message>
         <source>Setups</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzstādījumi</translation>
     </message>
     <message>
         <source>Servers</source>
-        <translation type="unfinished"></translation>
+        <translation>Serveri</translation>
     </message>
     <message>
         <source>Destinations</source>
-        <translation type="unfinished"></translation>
+        <translation>Mērķi</translation>
     </message>
     <message>
         <source>Section:</source>
-        <translation type="unfinished"></translation>
+        <translation>Sadaļa:</translation>
     </message>
     <message>
         <source>Close Section</source>
-        <translation type="unfinished"></translation>
+        <translation>Aizvērt Sadaļu</translation>
     </message>
     <message>
         <source>Sections</source>
-        <translation type="unfinished"></translation>
+        <translation>Sadaļas</translation>
     </message>
     <message>
         <source>Filter: </source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrs:</translation>
     </message>
     <message>
         <source>Clear Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Attīrīt Filtru</translation>
     </message>
     <message>
         <source>Close Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Aizvērt Meklēšanu</translation>
     </message>
     <message>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrs</translation>
     </message>
     <message>
         <source>Search: </source>
-        <translation type="unfinished"></translation>
+        <translation>Meklēt:</translation>
     </message>
     <message>
         <source>Clear Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Attīrīt Meklēšanu</translation>
     </message>
     <message>
         <source>Do Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Meklēt</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Meklēšana</translation>
     </message>
     <message>
         <source>Destination: </source>
-        <translation type="unfinished"></translation>
+        <translation>Mērķis:</translation>
     </message>
     <message>
         <source>Link</source>
-        <translation type="unfinished"></translation>
+        <translation>Saite</translation>
     </message>
     <message>
         <source>Close Destinations</source>
-        <translation type="unfinished"></translation>
+        <translation>Aizvērt Mērķus</translation>
     </message>
     <message>
         <source>Package Actions</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakotņu Darbības</translation>
     </message>
     <message>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalēt</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt</translation>
     </message>
     <message>
         <source>Package Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakotnes Statuss</translation>
     </message>
     <message>
         <source>New version, installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Jauna versija, instalēta</translation>
     </message>
     <message>
         <source>New version, not installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Jauna versija, nav instalēta</translation>
     </message>
     <message>
         <source>Old version, installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Veca versija, instalēta</translation>
     </message>
     <message>
         <source>Old version, not installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Veca versija, nav instalēta</translation>
     </message>
     <message>
         <source>Old version, new version installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Veca versija, jauna versija instalēta</translation>
     </message>
     <message>
         <source>New version, old version installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Jauna versija, veca versija instalēta</translation>
     </message>
 </context>
 <context>
     <name>PackageListView</name>
     <message>
         <source>Package</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakotne</translation>
     </message>
 </context>
 <context>
     <name>PackageManagerSettings</name>
     <message>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Jauns</translation>
     </message>
 </context>
 <context>
     <name>PackageManagerSettingsBase</name>
     <message>
         <source>Package Servers</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakotņu Serveri</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzstādījumi</translation>
     </message>
     <message>
         <source>Install destination:</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalācijas mērķis:</translation>
     </message>
     <message>
         <source>Link to root destination</source>
-        <translation type="unfinished"></translation>
+        <translation>Saistīt ar root mērķi</translation>
     </message>
     <message>
         <source>Used servers: (Highlighted feeds are active)</source>
-        <translation type="unfinished"></translation>
+        <translation>Pielietotie serveri: (Izceltie avoti ir aktīvie)</translation>
     </message>
     <message>
         <source>Servers</source>
-        <translation type="unfinished"></translation>
+        <translation>Serveri</translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Vārdi:</translation>
     </message>
     <message>
         <source>URL:</source>
-        <translation type="unfinished"></translation>
+        <translation>URL:</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt</translation>
     </message>
     <message>
         <source>New</source>
-        <translation type="unfinished"></translation>
+        <translation>Jauns</translation>
     </message>
     <message>
         <source>Destinations</source>
-        <translation type="unfinished"></translation>
+        <translation>Mērķi</translation>
     </message>
     <message>
         <source>link to /</source>
-        <translation type="unfinished"></translation>
+        <translation>saistīt ar /</translation>
     </message>
     <message>
         <source>Remove links</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt saites</translation>
     </message>
     <message>
         <source>volatile</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>removeable</source>
-        <translation type="unfinished"></translation>
+        <translation>izņemams</translation>
     </message>
 </context>
 <context>
@@ -267,89 +267,89 @@
     <message>
         <source>
 All done.</source>
-        <translation type="unfinished"></translation>
+        <translation>Viss paveikts.</translation>
     </message>
     <message>
         <source>Removing</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemu</translation>
     </message>
     <message>
         <source>please wait</source>
-        <translation type="unfinished"></translation>
+        <translation>lūdzu uzgaidiet</translation>
     </message>
     <message>
         <source>Error while removing </source>
-        <translation type="unfinished"></translation>
+        <translation>Kļūda izņemot</translation>
     </message>
     <message>
         <source>Installing</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalēju</translation>
     </message>
     <message>
         <source>Error while installing</source>
-        <translation type="unfinished"></translation>
+        <translation>Kļūda instalējot</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Description: </source>
-        <translation type="unfinished"></translation>
+        <translation>Apraksts:</translation>
     </message>
     <message>
         <source>Size: </source>
-        <translation type="unfinished"></translation>
+        <translation>Izmērs</translation>
     </message>
     <message>
         <source>other</source>
-        <translation type="unfinished"></translation>
+        <translation>cits</translation>
     </message>
     <message>
         <source>Install Name: </source>
-        <translation type="unfinished"></translation>
+        <translation>Instalācijas Vārds:</translation>
     </message>
     <message>
         <source> on </source>
-        <translation type="unfinished"></translation>
+        <translation>uz</translation>
     </message>
     <message>
         <source>Name: </source>
-        <translation type="unfinished"></translation>
+        <translation>Vārds:</translation>
     </message>
     <message>
         <source>Link: </source>
-        <translation type="unfinished"></translation>
+        <translation>Saite:</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Jā</translation>
     </message>
     <message>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nē</translation>
     </message>
     <message>
         <source>Destination: </source>
-        <translation type="unfinished"></translation>
+        <translation>Mērķis:</translation>
     </message>
     <message>
         <source>Status: </source>
-        <translation type="unfinished"></translation>
+        <translation>Statuss:</translation>
     </message>
     <message>
         <source>Install to</source>
-        <translation type="unfinished"></translation>
+        <translation>Instalēt uz</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Izņemt</translation>
     </message>
 </context>
 <context>
     <name>RunWindow</name>
     <message>
         <source>running...</source>
-        <translation type="unfinished"></translation>
+        <translation>palaiž...</translation>
     </message>
 </context>
 </TS>
