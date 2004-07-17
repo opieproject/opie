3e502aeaeb343464174d1c001ce5558848f6fa2a
Holger Freyther <zecke@handhelds.org>
2004-07-17 20:33:12 +0000

    Use the MultiauthPassword::authenticate method instead of the handcrafting

--- core/settings/security/demo/main.cpp
+++ core/settings/security/demo/main.cpp
@@ -11,24 +11,13 @@
  * \em Note: calls are not parsed by doxygen since they are done via a QInterfacePtr:
  * \see http://dudu.dyn.2-h.org/nist/doxydoc/allOpie//classQInterfacePtr.html
  */
-int main( int argc, char ** argv ) 
+int main( int argc, char ** argv )
 {
     Opie::Core::OApplication app(argc, argv, "Multi-authentication demo");
 
-    /* Constructs the main window, which displays messages and blocks
-     * access to the desktop
+    /*
+     * Run authentication and retursn if successfull
+     * This uses the global settings
      */
-    Opie::Security::MultiauthMainWindow win;
-
-    app.showMainWidget(&win);
-
-    // resize the QDialog object so it fills all the screen
-    QRect desk = qApp->desktop()->geometry();
-    win.setGeometry( 0, 0, desk.width(), desk.height() );
-
-    // the authentication has already succeeded (without win interactions)
-    if ( win.isAlreadyDone() )
-        return 0;
-
-    return app.exec();
+    Opie::Security::MultiauthPassword::authenticate();
 }
