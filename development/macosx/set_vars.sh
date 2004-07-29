965654fbb8909431e820030903f4b810f6424730
Stefan Eilers <stefan@eilers-online.net>
2004-07-29 10:52:49 +0000

    Minor update. Added config and environment settings to compile on curren Opie release..

--- development/macosx/set_vars.sh
+++ development/macosx/set_vars.sh
@@ -2,7 +2,9 @@
 unset LD_PREBIND
 unset LD_PREBIND_ALLOW_OVERLAP
 unset LD_SEG_ADDR_TABLE
-
+unset QMAKESPEC
+# Needed to activate Prebinding capababilites (since 10.2 and newer)
+MACOSX_DEPLOYMENT_TARGET=10.2
 #Set Vars for QT 2.3.2
 THISDIR=`pwd`
 QTDIR=$THISDIR/qt-2.3.2
@@ -18,11 +20,15 @@ DYLD_LIBRARY_PATH="$QTDIR/lib":$DYLD_LIBRARY_PATH
 OPIEDIR=$THISDIR/opie
 DYLD_LIBRARY_PATH="$OPIEDIR/lib":$DYLD_LIBRARY_PATH
 
+#Set special compiler
+# PATH=$OPIEDIR/../tools/bin:$PATH
+# DYLD_LIBRARY_PATH=$OPIEDIR/../tools/bin:$DYLD_LIBRARY_PATH
+
 #Show special environment
 
 PS1='\u:(QT):\w\$ '
 
-export QTDIR QPEDIR OPIEDIR PATH DYLD_LIBRARY_PATH PS1
+export QTDIR QPEDIR OPIEDIR PATH DYLD_LIBRARY_PATH PS1 MACOSX_DEPLAYMENT_TARGET
 
 # Now start virtual framebuffer device..
 qvfb -depth 16 &
