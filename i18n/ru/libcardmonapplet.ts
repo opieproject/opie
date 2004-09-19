d0a2433327af763db616a4954c1186d0c00a1d8c
Sergei Klink <sudonix@handhelds.org>
2004-09-19 20:30:41 +0000

    Minor updates

--- i18n/ru/libcardmonapplet.ts
+++ i18n/ru/libcardmonapplet.ts
@@ -3,11 +3,11 @@
     <name>CardMonitor</name>
     <message>
         <source>CF/PCMCIA card eject failed!</source>
-        <translation>Выброс CF/PCMCIA карты не удался!</translation>
+        <translation>Не могу выдвинуть CF/PCMCIA карту!</translation>
     </message>
     <message>
         <source>SD/MMC card eject failed!</source>
-        <translation>Выброс SD/MMC карты не удался!</translation>
+        <translation>Не могу выдвинуть SD/MMC карту!</translation>
     </message>
     <message>
         <source>New card: </source>
@@ -15,27 +15,27 @@
     </message>
     <message>
         <source>Ejected: </source>
-        <translation>Выброшена:</translation>
+        <translation>Выдвинута:</translation>
     </message>
     <message>
         <source>Eject SD/MMC card</source>
-        <translation>Выбросить SD/MMC карту</translation>
+        <translation>Выдвинуть SD/MMC карту</translation>
     </message>
     <message>
         <source>Eject card 0: %1</source>
-        <translation>Выбросить карту 0: %1</translation>
+        <translation>Выдвинуть карту 0: %1</translation>
     </message>
     <message>
         <source>Eject card 1: %1</source>
-        <translation>Выбросить карту 1: %1</translation>
+        <translation>Выдвинуть карту 1: %1</translation>
     </message>
     <message>
         <source>New card: SD/MMC</source>
-        <translation type="unfinished"></translation>
+        <translation>Новая карта: SD/MMC</translation>
     </message>
     <message>
         <source>Ejected: SD/MMC</source>
-        <translation type="unfinished"></translation>
+        <translation>Выдвинута: SD/MMC</translation>
     </message>
 </context>
 </TS>
