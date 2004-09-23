23c3e1eb0523af27e3b6c45fafbfad6bbe3238c9
Gints Polis <gints.polis@cc.lv>
2004-09-23 19:24:17 +0000

    Latvian translations for security

--- i18n/lv/security.ts
+++ i18n/lv/security.ts
@@ -3,179 +3,178 @@
     <name>LoginBase</name>
     <message>
         <source>LoginBase</source>
-        <translation type="unfinished"></translation>
+        <translation>PieslēgumaBāze</translation>
     </message>
     <message>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Pieslēgums</translation>
     </message>
     <message>
         <source>Login Automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Pieslēgties Automātiski</translation>
     </message>
     <message>
         <source>If you select this option, opie-login will automagically log you in with the user you have selected in the drop-down list.  
 
 For the Zaurus 5x00 this is always the user &quot;root&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja jūs izvēlēsaties šo opciju, opie-logon automāŧiski pieslēgs jūs ar izvēlēto lietotāju.  
+
+Zaurus 5x00 tas vienmēr ir &quot;root&quot;.</translation>
     </message>
     <message>
         <source>This drop-down list allows you to select the user for auto-login
 (if enabled above).  
 
 You can only select an actually configured user.</source>
-        <translation type="unfinished"></translation>
+        <translation>Šis combo saraksts atļauj jums izvēlēties auto pieslēgumu
+(ja tas atļauts augstāk)  
+
+Jūs varat izvēlēties konfigurētu lietotāju.</translation>
     </message>
 </context>
 <context>
     <name>MultiauthConfig</name>
     <message>
         <source>Security configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Drošības konfigurācija</translation>
     </message>
     <message>
         <source>Load which plugins in what order:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kurus spraudņus un kādā kārtībā:</translation>
     </message>
     <message>
         <source>Check a checkbox to activate/deactivate a plugin or use the arrow buttons on the right to change the order they will appear in</source>
-        <translation type="unfinished"></translation>
+        <translation>Atzīmējiet ar ķeksi lai aktivizētu/deaktivizētu spraudni vai lietojiet bultiņas, lai izmainītu to parādīšanas kārtību</translation>
     </message>
     <message>
         <source>Move Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Uz augšu</translation>
     </message>
     <message>
         <source>Move Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Uz Leju</translation>
     </message>
     <message>
         <source>plugins</source>
-        <translation type="unfinished"></translation>
+        <translation>spraudņi</translation>
     </message>
     <message>
         <source>Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Autentifikācija</translation>
     </message>
     <message>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Pieslēgums</translation>
     </message>
     <message>
         <source>Sync</source>
-        <translation type="unfinished"></translation>
+        <translation>Sinh</translation>
     </message>
     <message>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Uzmanību</translation>
     </message>
     <message>
         <source>&lt;p&gt;All user-defined net ranges will be lost.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Visi lieotāju-definēti diapazoni pazudīs.</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Atlikt</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Labi</translation>
     </message>
     <message>
         <source>192.168.129.0/24</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>192.168.1.0/24</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>192.168.0.0/16</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>172.16.0.0/12</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>10.0.0.0/8</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>1.0.0.0/8</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Any</source>
-        <translation type="unfinished"></translation>
+        <translation>Visi</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Neviens</translation>
     </message>
 </context>
 <context>
     <name>MultiauthGeneralConfig</name>
     <message>
         <source>When to lock Opie</source>
-        <translation type="unfinished"></translation>
+        <translation>Kad noslēgt Opie</translation>
     </message>
     <message>
         <source>on Opie start</source>
-        <translation type="unfinished"></translation>
+        <translation>pie Opie startēšanas</translation>
     </message>
     <message>
         <source>on Opie resume</source>
-        <translation type="unfinished"></translation>
+        <translation>pie Opie atjaunošanas</translation>
     </message>
     <message>
         <source>Multiple plugins authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Vairāku spraudņu autentifikācija</translation>
     </message>
     <message>
         <source>Required successes</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepieciešamās izdošanās</translation>
     </message>
     <message>
         <source>Debug options</source>
-        <translation type="unfinished"></translation>
+        <translation>Atblusošanas opcijas</translation>
     </message>
     <message>
         <source>Don&apos;t protect this config screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Neaizsargāt šo konfigurācijas ekrānu</translation>
     </message>
     <message>
         <source>Show explanatory screens</source>
-        <translation type="unfinished"></translation>
+        <translation>Rādīt skaidrojošās formas</translation>
     </message>
     <message>
         <source>Allow to bypass authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Atļaut izlaist autentifikāciju</translation>
     </message>
     <message>
         <source>Note: the third option implies the second one</source>
-        <translation type="unfinished"></translation>
+        <translation>Piezīme: trešā opcija ietekmē otro</translation>
     </message>
-</context>
-<context>
-    <name>Security</name>
-</context>
-<context>
-    <name>SecurityBase</name>
 </context>
 <context>
     <name>SyncBase</name>
     <message>
         <source>SyncBase</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Sync</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Accept sync from network:</source>
-        <translation type="unfinished"></translation>
+        <translation>Akceptēt sync no tīkla:</translation>
     </message>
     <message>
         <source>Select a net-range or enter a new one.  
@@ -188,11 +187,20 @@ The entry &quot;All&quot; will allow *any* IP to connect.
 The entry &quot;None&quot; will *deny* any connection.  
 
 If unsure, select &quot;Any&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Izvēlaties tīkla diapazonu vai ievadiet jaunu.  
+
+Šis kombo lists ļauj jums izvēlēties diapazonu kuram tiks atļauta pieslēgšanās  
+
+Piemēram, diapazons 192.168.1.0/24 atļaus sinhronizēties datoriem ar IP starp 192.168.1.1. un 192.168.1.254  
+
+Ieraksts &quot;Visi&quot; ļaus pieslēgties *jebkuram*.
+Ieraksts &quot;Neviens&quot; *aizliegs* pieslēgumu visiem.  
+
+Ja nēsiet pārliecināti, izvēlaties &quot;Visi&quot;. </translation>
     </message>
     <message>
         <source>Delete Entry</source>
-        <translation type="unfinished"></translation>
+        <translation>Dzēst Ierakstu</translation>
     </message>
     <message>
         <source>Delete the selected net range from the list
@@ -201,42 +209,54 @@ If you press this button, the currently selected net range will be deleted from 
 
 If you hit this button in error, the button &quot;Restore Defaults&quot; will restore
 the list to the state it has been when you initially booted your Zaurus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dzēst izvēlēto diapazonu no saraksta
+
+Ja jūs nospiedīsiet šo pogu, iezīmētais diapazons tiks dzēsts no saraksta.
+
+Ja jus nospiedāt šo pogu kļūdas pēc, jūs variet atjaunot noklusētos
+uzstādījumus ar pogu &quot;Atjaunot Noklusētos&quot;.</translation>
     </message>
     <message>
         <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation>Ataunot Noklusētos</translation>
     </message>
     <message>
         <source>This button will restore the list of net ranges
 to the defaults.
 
 Beware that all manually entered net ranges will be lost!</source>
-        <translation type="unfinished"></translation>
+        <translation>Šī poga atjaunos diapazonus
+uz noklusētajiem
+
+Uzmanību! Visi ar roku ievadītie diapazoni tiks dzēsti!</translation>
     </message>
     <message>
         <source>Select your sync software</source>
-        <translation type="unfinished"></translation>
+        <translation>Izvēlaties sinhronizēšanas programatūru</translation>
     </message>
     <message>
         <source>Qtopia 1.7</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Opie 1.0</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>IntelliSync</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Alter the Sync Protocol to tweak for the different devices.
 <byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    IntelliSync will disable the password check but you&apos;ll be prompted to accept the connection.
 <byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    Opie1.0 will be compatible with QtopiaDesktop, MultiSync and KitchenSync
 <byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    Qtopia1.7 will alter the way data is send to the client and will not work with older clients.
-<byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/>    </source>
-        <translation type="unfinished"></translation>
+<byte value="x9"/><byte value="x9"/><byte value="x9"/><byte value="x9"/></source>
+        <translation>Norādiet Sync Protokolu lai apmierinātu atšķirīgas iekārtas
+                      IntelliSync izslēgs paroles pārbaudi bet jums būs jāakceptē pieslēgums.
+                     Opie1.0 ir savietojams ar QtopiaDesktop, MultiSync un KitchenSync
+                     Qtopia1.7 mainīs kā dati tiek sūtīti klientam un tas nestrādās vecākiem klientiem.
+</translation>
     </message>
 </context>
 </TS>
