f53f4cf1ab6319941f77d8f323f6d43f477693ff
Clement Seveillac <clem@handhelds.org>
2004-09-30 21:44:18 +0000

    simpler explanation

--- core/settings/security/demo/main.cpp
+++ core/settings/security/demo/main.cpp
@@ -2,21 +2,11 @@
 
 #include <opie2/oapplication.h>
 
-/// main() function of the opie-multiauth-standalone demonstration application
-/**
- * Calls each plugin one after the other, and asks them to perform one authentication.
- * It loads the plugins one after another through the MultiauthPluginInterface,
- * then calls MultiauthPluginObject::authenticate().
- * \em Note: calls are not parsed by doxygen since they are done via a QInterfacePtr:
- * \see http://dudu.dyn.2-h.org/nist/doxydoc/allOpie//classQInterfacePtr.html
- */
+/// Run an authentication sequence using the global opie-security settings
 int main( int argc, char ** argv )
 {
     Opie::Core::OApplication app(argc, argv, "Multi-authentication demo");
 
-    /*
-     * Run authentication and retursn if successfull
-     * This uses the global settings
-     */
+    // Run the authentication process until it succeeds
     Opie::Security::MultiauthPassword::authenticate();
 }
