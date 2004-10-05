8d910df87ac9c2db5eb9da83687d63ad9213f82c
Maximilian Reiß <max.reiss@gmx.de>
2004-10-05 21:47:16 +0000

    scripts to fix the stupid embedix rom inittab behavior

---
+++ share/opie-console/sl6000_embedix_restart_0_1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Use egrep to fid out if we need to comment /etc/sync/serialctl
+# from the inittab
+/usr/bin/env egrep -q '^#pd:[0-9]:respawn:/etc/sync/serialctl' /etc/inittab 2>/dev/null
+
+# If egrep did not find an enabled /etc/sync/serialctl in /etc/inittab,
+# exit now (nothing to do)
+if [ $? -ne 0 ]; then
+  exit 0
+fi
+
+# This "cp -af" is to get the perms correct on /etc/inittab.tmp
+/usr/bin/env cp -af /etc/inittab /etc/inittab.tmp
+
+# This sed comments out the "etc/sync/serialctl" line
+/usr/bin/env sed 's%^#\(pd:[0-9]:respawn:/etc/sync/serialctl\)%\1%' /etc/inittab > /etc/inittab.tmp
+
+# We have not yet modified /etc/inittab, so to stay on the safe side
+# only modify it if the sed succeeded at what we asked it to do.
+if [ $? -eq 0 ]; then
+  # This "cp -af" puts the new /etc/inittab into place
+  /usr/bin/env cp -af /etc/inittab.tmp /etc/inittab
+  # Unlink the tmp file
+  rm /etc/inittab.tmp
+  # HUP init
+  /usr/bin/env kill -HUP 1
+fi
+
