fd4b8861d0e8dea8d2bc704b5d00d91f1f7284e4
Holger Freyther <zecke@handhelds.org>
2004-10-10 16:23:46 +0000

    -Stop bashism -Do not use /usr/bin/env but the full qualified path to the executable

--- share/opie-console/sl6000_embedix_restart_0_1.sh
+++ share/opie-console/sl6000_embedix_restart_0_1.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Use egrep to fid out if we need to comment /etc/sync/serialctl
 # from the inittab
-/usr/bin/env egrep -q '^#pd:[0-9]:respawn:/etc/sync/serialctl' /etc/inittab 2>/dev/null
+/bin/egrep -q '^#pd:[0-9]:respawn:/etc/sync/serialctl' /etc/inittab 2>/dev/null
 
 # If egrep did not find an enabled /etc/sync/serialctl in /etc/inittab,
 # exit now (nothing to do)
@@ -11,19 +11,19 @@ if [ $? -ne 0 ]; then
 fi
 
 # This "cp -af" is to get the perms correct on /etc/inittab.tmp
-/usr/bin/env cp -af /etc/inittab /etc/inittab.tmp
+/bin/cp -af /etc/inittab /etc/inittab.tmp
 
 # This sed comments out the "etc/sync/serialctl" line
-/usr/bin/env sed 's%^#\(pd:[0-9]:respawn:/etc/sync/serialctl\)%\1%' /etc/inittab > /etc/inittab.tmp
+/bin/sed 's%^#\(pd:[0-9]:respawn:/etc/sync/serialctl\)%\1%' /etc/inittab > /etc/inittab.tmp
 
 # We have not yet modified /etc/inittab, so to stay on the safe side
 # only modify it if the sed succeeded at what we asked it to do.
 if [ $? -eq 0 ]; then
   # This "cp -af" puts the new /etc/inittab into place
-  /usr/bin/env cp -af /etc/inittab.tmp /etc/inittab
+  /bin/cp -af /etc/inittab.tmp /etc/inittab
   # Unlink the tmp file
-  rm /etc/inittab.tmp
+  /bin/rm /etc/inittab.tmp
   # HUP init
-  /usr/bin/env kill -HUP 1
+  /bin/kill -HUP 1
 fi
 
