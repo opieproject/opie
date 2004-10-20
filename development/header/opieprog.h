c48cbe59239a3476aa9664bb8b8655bcff5ce2a2
Holger Freyther <zecke@handhelds.org>
2004-10-20 16:58:54 +0000

    -Fix white space -Mention GPL not LGPL in opieprog.h...

--- development/header/opieprog.h
+++ development/header/opieprog.h
@@ -14,12 +14,12 @@
     : ..    .:,     . . .    without even the implied warranty of
     =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
   _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
-..}^=.=       =       ;      Library General Public License for more
+..}^=.=       =       ;      General Public License for more
 ++=   -.     .`     .:       details.
  :     =  ...= . :.=-        
  -.   .:....=;==+<;          You should have received a copy of the GNU
-  -_. . .   )=.  =           Library General Public License along with
-    --        :-=`           this library; see the file COPYING.LIB. 
+  -_. . .   )=.  =           General Public License along with
+    --        :-=`           this library; see the file COPYING.BIN. 
                              If not, write to the Free Software Foundation,
                              Inc., 59 Temple Place - Suite 330,
                              Boston, MA 02111-1307, USA.
