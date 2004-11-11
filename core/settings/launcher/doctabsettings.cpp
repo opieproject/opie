1b0dd0f82e4ad8fa91685193fe71a633234b462e
Rajko Albrecht <alwin@handhelds.org>
2004-11-11 21:08:16 +0000

    send qcop signal when enable/disable doctab

--- core/settings/launcher/doctabsettings.cpp
+++ core/settings/launcher/doctabsettings.cpp
@@ -1,26 +1,26 @@
 /*
-                             This file is part of the OPIE Project
+                     This file is part of the OPIE Project
                =.            Copyright (c)  2002 Trolltech AS <info@trolltech.com>
-             .=l.            Copyright (c)  2003 Michael Lauer <mickeyl@handhelds.org>
-           .>+-=
- _;:,     .>    :=|.         This file is free software; you can
-.> <`_,   >  .   <=          redistribute it and/or modify it under
-:`=1 )Y*s>-.--   :           the terms of the GNU General Public
-.="- .-=="i,     .._         License as published by the Free Software
- - .   .-<_>     .<>         Foundation; either version 2 of the License,
-     ._= =}       :          or (at your option) any later version.
-    .%`+i>       _;_.
-    .i_,=:_.      -<s.       This file is distributed in the hope that
-     +  .  -:.       =       it will be useful, but WITHOUT ANY WARRANTY;
-    : ..    .:,     . . .    without even the implied warranty of
-    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
-  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU General
-..}^=.=       =       ;      Public License for more details.
-++=   -.     .`     .:
- :     =  ...= . :.=-        You should have received a copy of the GNU
- -.   .:....=;==+<;          General Public License along with this file;
-  -_. . .   )=.  =           see the file COPYING. If not, write to the
-    --        :-=`           Free Software Foundation, Inc.,
+      .=l.            Copyright (c)  2003 Michael Lauer <mickeyl@handhelds.org>
+     .>+-=
+_;:,   .>  :=|.         This file is free software; you can
+.> <`_,  > .  <=          redistribute it and/or modify it under
+:`=1 )Y*s>-.--  :           the terms of the GNU General Public
+.="- .-=="i,   .._         License as published by the Free Software
+- .  .-<_>   .<>         Foundation; either version 2 of the License,
+  ._= =}    :          or (at your option) any later version.
+  .%`+i>    _;_.
+  .i_,=:_.   -<s.       This file is distributed in the hope that
+  + . -:.    =       it will be useful, but WITHOUT ANY WARRANTY;
+  : ..  .:,   . . .    without even the implied warranty of
+  =_    +   =;=|`    MERCHANTABILITY or FITNESS FOR A
+ _.=:.    :  :=>`:     PARTICULAR PURPOSE. See the GNU General
+..}^=.=    =    ;      Public License for more details.
+++=  -.   .`   .:
+:   = ...= . :.=-        You should have received a copy of the GNU
+-.  .:....=;==+<;          General Public License along with this file;
+ -_. . .  )=. =           see the file COPYING. If not, write to the
+  --    :-=`           Free Software Foundation, Inc.,
                              59 Temple Place - Suite 330,
                              Boston, MA 02111-1307, USA.
 
@@ -37,6 +37,10 @@
 #include <qlayout.h>
 #include <qlabel.h>
 #include <qwhatsthis.h>
+
+#ifdef Q_WS_QWS
+#include <qtopia/qcopenvelope_qws.h>
+#endif
 
 
 DocTabSettings::DocTabSettings( QWidget *parent, const char *name ):QWidget( parent, name )
@@ -68,5 +72,12 @@ void DocTabSettings::accept()
     cfg.setGroup( "DocTab" );
     cfg.writeEntry( "Enable", _enable->isChecked() );
     cfg.write();
+#if defined(Q_WS_QWS)
+    {
+        QCopEnvelope cop("QPE/Launcher","doctabEnabled(int)");
+        cop << _enable->isChecked();
+        odebug << "Sending envelope" << oendl;
+    }
+#endif
 }
 
