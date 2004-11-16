1942f1c64fb3785c3bf32a12eafcb0c05fe199da
Dan Williams <drw392772@gmail.com>
2004-11-16 17:38:05 +0000

    Minor UI tweak - in filter dialog, when option is enabled, set focus to widget that corresponds to that option (less tapping)

--- noncore/settings/packagemanager/filterdlg.cpp
+++ noncore/settings/packagemanager/filterdlg.cpp
@@ -1,27 +1,27 @@
 /*
-                            This file is part of the OPIE Project
+                    This file is part of the OPIE Project
 
                =.            Copyright (c)  2003 Dan Williams <drw@handhelds.org>
-             .=l.
-           .>+-=
- _;:,     .>    :=|.         This file is free software; you can
-.> <`_,   >  .   <=          redistribute it and/or modify it under
-:`=1 )Y*s>-.--   :           the terms of the GNU General Public
-.="- .-=="i,     .._         License as published by the Free Software
- - .   .-<_>     .<>         Foundation; either version 2 of the License,
-     ._= =}       :          or (at your option) any later version.
-    .%`+i>       _;_.
-    .i_,=:_.      -<s.       This file is distributed in the hope that
-     +  .  -:.       =       it will be useful, but WITHOUT ANY WARRANTY;
-    : ..    .:,     . . .    without even the implied warranty of
-    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
-  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU General
-..}^=.=       =       ;      Public License for more details.
-++=   -.     .`     .:
- :     =  ...= . :.=-        You should have received a copy of the GNU
- -.   .:....=;==+<;          General Public License along with this file;
-  -_. . .   )=.  =           see the file COPYING. If not, write to the
-    --        :-=`           Free Software Foundation, Inc.,
+      .=l.
+     .>+-=
+_;:,   .>  :=|.         This file is free software; you can
+.> <`_,  > .  <=          redistribute it and/or modify it under
+:`=1 )Y*s>-.--  :           the terms of the GNU General Public
+.="- .-=="i,   .._         License as published by the Free Software
+- .  .-<_>   .<>         Foundation; either version 2 of the License,
+  ._= =}    :          or (at your option) any later version.
+  .%`+i>    _;_.
+  .i_,=:_.   -<s.       This file is distributed in the hope that
+  + . -:.    =       it will be useful, but WITHOUT ANY WARRANTY;
+  : ..  .:,   . . .    without even the implied warranty of
+  =_    +   =;=|`    MERCHANTABILITY or FITNESS FOR A
+ _.=:.    :  :=>`:     PARTICULAR PURPOSE. See the GNU General
+..}^=.=    =    ;      Public License for more details.
+++=  -.   .`   .:
+:   = ...= . :.=-        You should have received a copy of the GNU
+-.  .:....=;==+<;          General Public License along with this file;
+ -_. . .  )=. =           see the file COPYING. If not, write to the
+  --    :-=`           Free Software Foundation, Inc.,
                              59 Temple Place - Suite 330,
                              Boston, MA 02111-1307, USA.
 
@@ -142,21 +142,25 @@ void FilterDlg::initItem( QComboBox *comboBox, QCheckBox *checkBox, const QStrin
 void FilterDlg::slotNameSelected( bool selected )
 {
     m_name->setEnabled( selected );
+    m_name->setFocus();
 }
 
 void FilterDlg::slotServerSelected( bool selected )
 {
     m_server->setEnabled( selected );
+    m_server->setFocus();
 }
 
 void FilterDlg::slotDestSelected( bool selected )
 {
     m_destination->setEnabled( selected );
+    m_destination->setFocus();
 }
 
 void FilterDlg::slotStatusSelected( bool selected )
 {
     m_status->setEnabled( selected );
+    m_status->setFocus();
 
     if ( !selected && !m_destCB->isEnabled() )
     {
@@ -186,4 +190,5 @@ void FilterDlg::slotStatusChanged( const QString &category )
 void FilterDlg::slotCategorySelected( bool selected )
 {
     m_category->setEnabled( selected );
+    m_category->setFocus();
 }
