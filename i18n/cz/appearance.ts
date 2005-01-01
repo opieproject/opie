e41b05a842e0075a3cae533fc8c8fb9ef32b1c29
Pavel Ruzicka <pavouk@handhelds.org>
2005-01-01 23:40:52 +0000

    Appearance Czech translation

--- i18n/cz/appearance.ts
+++ i18n/cz/appearance.ts
@@ -1,254 +1,270 @@
-<!DOCTYPE TS><TS>
-<context>
+<!DOCTYPE TS>
+<TS>
+  <context>
     <name>Appearance</name>
     <message>
-        <source>Styles control the way items such as buttons and scroll bars appear in all applications.
+      <source>Styles control the way items such as buttons and scroll bars appear in all applications.
 
 Click here to select an available style.</source>
-        <translation type="unfinished"></translation>
+      <translation>Styly zajišťují, jak budou vypadat tlačítka a posuvníky ve všech aplikacích. 
+
+Klikněte sem pro výběr jednoho ze stylů.</translation>
     </message>
     <message>
-        <source>Settings...</source>
-        <translation type="unfinished"></translation>
+      <source>Settings...</source>
+      <translation>Nastavení...</translation>
     </message>
     <message>
-        <source>Click here to configure the currently selected style.
+      <source>Click here to configure the currently selected style.
 
 Note:  This option is not available for all styles.</source>
-        <translation type="unfinished"></translation>
+      <translation>Klikněte sem pro konfiguraci aktuálně označeného stylu. 
+
+Poznámka: Tato volba není k dispozici pro všechny styly.</translation>
     </message>
     <message>
-        <source>Window decorations control the way the application title bar and its buttons appear.
+      <source>Window decorations control the way the application title bar and its buttons appear.
 
 Click here to select an available decoration.</source>
-        <translation type="unfinished"></translation>
+      <translation>Dekorace oken řídí zobrazování lišty a jejích tlačítek. 
+
+Klikněte sem pro výběr možných dekorací.</translation>
     </message>
     <message>
-        <source>Select the desired name, style and size of the default font applications will use.</source>
-        <translation type="unfinished"></translation>
+      <source>Select the desired name, style and size of the default font applications will use.</source>
+      <translation>Vyberte požadovaný název, styl a velikost výchozího písma pro aplikace.</translation>
     </message>
     <message>
-        <source>Color schemes are a collection of colors which are used for various parts of the display.
+      <source>Color schemes are a collection of colors which are used for various parts of the display.
 
 Click here to select an available scheme.</source>
-        <translation type="unfinished"></translation>
+      <translation>Barevná schémata jsou kolekce barev, které jsou požívané pro různé části zobrazení. 
+
+Klikněte sem pro výběr požadovaného schéma.</translation>
     </message>
     <message>
-        <source>Current scheme</source>
-        <translation type="unfinished"></translation>
+      <source>Current scheme</source>
+      <translation>Aktuální schéma</translation>
     </message>
     <message>
-        <source>Edit...</source>
-        <translation type="unfinished"></translation>
+      <source>Edit...</source>
+      <translation>Upravit...</translation>
     </message>
     <message>
-        <source>Click here to change the colors in the current color scheme.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to change the colors in the current color scheme.</source>
+      <translation>Klikněte sem pro změnu barev v aktálním barevném schéma.</translation>
     </message>
     <message>
-        <source>Delete</source>
-        <translation type="unfinished"></translation>
+      <source>Delete</source>
+      <translation>Smazat</translation>
     </message>
     <message>
-        <source>Click here to delete the color scheme selected in the list to the left.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to delete the color scheme selected in the list to the left.</source>
+      <translation>Klikněte sem pro smazání schéma vybraného ze seznamu vlevo.</translation>
     </message>
     <message>
-        <source>Save</source>
-        <translation type="unfinished"></translation>
+      <source>Save</source>
+      <translation>Uložit</translation>
     </message>
     <message>
-        <source>Click here to name and save the current color scheme.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to name and save the current color scheme.</source>
+      <translation>Klikněte sem pro název a uložení aktuálního schéma.</translation>
     </message>
     <message>
-        <source>Force styling for all applications.</source>
-        <translation type="unfinished"></translation>
+      <source>Force styling for all applications.</source>
+      <translation>Vynutit stylování pro všechny aplikace.</translation>
     </message>
     <message>
-        <source>Click here to allow all applications to use global appearance settings.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to allow all applications to use global appearance settings.</source>
+      <translation>Klikněte sem pro umožnení všem aplikacím používat globální nastavení vzhledu.</translation>
     </message>
     <message>
-        <source>Disable styling for these applications ( &lt;b&gt;*&lt;/b&gt; can be used as a wildcard):</source>
-        <translation type="unfinished"></translation>
+      <source>Disable styling for these applications ( &lt;b>*&lt;/b> can be used as a wildcard):</source>
+      <translation>Vypnout styl pro tylo aplikace ( &lt;b>*&lt;/b> může být použito jako wildcard):</translation>
     </message>
     <message>
-        <source>If some applications do not display correctly with the global appearance settings, certain features can be turned off for that application.
+      <source>If some applications do not display correctly with the global appearance settings, certain features can be turned off for that application.
 
 This area allows you to select an application and which settings you wish to disable.</source>
-        <translation type="unfinished"></translation>
+      <translation>Jestliže se některé aplikace nezobrazují korektně s globálním nastavením vzhledu, můžou se určité vlastnosti vypnout pro tuto aplikaci. 
+
+Tato oblast vám umožní vybrat aplikaci a nastavení, která si přejete vypnout.</translation>
     </message>
     <message>
-        <source>Binary file(s)</source>
-        <translation type="unfinished"></translation>
+      <source>Binary file(s)</source>
+      <translation>Binární soubor(y)</translation>
     </message>
     <message>
-        <source>Click here to add an application to the list above.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to add an application to the list above.</source>
+      <translation>Klikněte pro přidání aplikace do seznamu nahoře.</translation>
     </message>
     <message>
-        <source>Click here to delete the currently selected application.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to delete the currently selected application.</source>
+      <translation>Klikněte pro vymazání aktuálně vybrané aplikace.</translation>
     </message>
     <message>
-        <source>Click here to move the currently selected application up in the list.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to move the currently selected application up in the list.</source>
+      <translation>Klikněte pro přesunutí aktuálně vybrané aplikace nahoru v seznamu.</translation>
     </message>
     <message>
-        <source>Click here to move the currently selected application down in the list.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here to move the currently selected application down in the list.</source>
+      <translation>Klikněte pro přesunutí aktuálně vybrané aplikace dolů v seznamu.</translation>
     </message>
     <message>
-        <source>Tab style:</source>
-        <translation type="unfinished"></translation>
+      <source>Tab style:</source>
+      <translation>Styl záložek:</translation>
     </message>
     <message>
-        <source>Click here to select a desired style for tabbed dialogs (such as this application). The styles available are:
+      <source>Click here to select a desired style for tabbed dialogs (such as this application). The styles available are:
 
 1. Tabs - normal tabs with text labels only
 2. Tabs w/icons - tabs with icons for each tab, text label only appears on current tab
 3. Drop down list - a vertical listing of tabs
 4. Drop down list w/icons - a vertical listing of tabs with icons</source>
-        <translation type="unfinished"></translation>
+      <translation>Klikněte sem pro výběr požadovaného stylu záložek oken (jako tato aplikace). K dispozici jsou tyto styly: 
+
+1. Záložky - normální záložky pouze s textovými popiskami 
+2. Záložky s ikonami - Záložky s ikonami na každé záložce a s textem pouze na otevřené 
+3. Rolovací menu - vertikální seznam záložek 
+4. Rolovací menu s ikonami - vertikální seznam záložek s ikonami</translation>
     </message>
     <message>
-        <source>Tabs</source>
-        <translation type="unfinished"></translation>
+      <source>Tabs</source>
+      <translation>Záložky</translation>
     </message>
     <message>
-        <source>Tabs w/icons</source>
-        <translation type="unfinished"></translation>
+      <source>Tabs w/icons</source>
+      <translation>Záložky s ikonami</translation>
     </message>
     <message>
-        <source>Drop down list</source>
-        <translation type="unfinished"></translation>
+      <source>Drop down list</source>
+      <translation>Rolovací menu</translation>
     </message>
     <message>
-        <source>Drop down list w/icons</source>
-        <translation type="unfinished"></translation>
+      <source>Drop down list w/icons</source>
+      <translation>Rolovací menu s ikonami</translation>
     </message>
     <message>
-        <source>Top</source>
-        <translation type="unfinished"></translation>
+      <source>Top</source>
+      <translation>Nahoře</translation>
     </message>
     <message>
-        <source>Click here so that tabs appear at the top of the window.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here so that tabs appear at the top of the window.</source>
+      <translation>Po zaškrtnutí se budou záložky objevovat v horní části okna.</translation>
     </message>
     <message>
-        <source>Bottom</source>
-        <translation type="unfinished"></translation>
+      <source>Bottom</source>
+      <translation>Dole</translation>
     </message>
     <message>
-        <source>Click here so that tabs appear at the bottom of the window.</source>
-        <translation type="unfinished"></translation>
+      <source>Click here so that tabs appear at the bottom of the window.</source>
+      <translation>Po zaškrtnutí se budou záložky objevovat v dolní části okna.</translation>
     </message>
     <message>
-        <source>Rotation direction:</source>
-        <translation type="unfinished"></translation>
+      <source>Rotation direction:</source>
+      <translation>Směr rotace:</translation>
     </message>
     <message>
-        <source>Appearance Settings</source>
-        <translation type="unfinished"></translation>
+      <source>Appearance Settings</source>
+      <translation>Nastavení vzhledu</translation>
     </message>
     <message>
-        <source>This is a preview window.  Look here to see your new appearance as options are changed.</source>
-        <translation type="unfinished"></translation>
+      <source>This is a preview window.  Look here to see your new appearance as options are changed.</source>
+      <translation>Toto je okno náhledu.  Zde uvidíte změny ve vzhledu, jak budete měnit vlastnosti.</translation>
     </message>
     <message>
-        <source>Style</source>
-        <translation type="unfinished"></translation>
+      <source>Style</source>
+      <translation>Styl</translation>
     </message>
     <message>
-        <source>Font</source>
-        <translation type="unfinished"></translation>
+      <source>Font</source>
+      <translation>Písmo</translation>
     </message>
     <message>
-        <source>Colors</source>
-        <translation type="unfinished"></translation>
+      <source>Colors</source>
+      <translation>Barvy</translation>
     </message>
     <message>
-        <source>Windows</source>
-        <translation type="unfinished"></translation>
+      <source>Windows</source>
+      <translation>Okna</translation>
     </message>
     <message>
-        <source>Advanced</source>
-        <translation type="unfinished"></translation>
+      <source>Advanced</source>
+      <translation>Rozšířené</translation>
     </message>
     <message>
-        <source>Save Scheme</source>
-        <translation type="unfinished"></translation>
+      <source>Save Scheme</source>
+      <translation>Uložit Schéma</translation>
     </message>
     <message>
-        <source>Save scheme</source>
-        <translation type="unfinished"></translation>
+      <source>Save scheme</source>
+      <translation>Uložit schéma</translation>
     </message>
     <message>
-        <source>Scheme does already exist.</source>
-        <translation type="unfinished"></translation>
+      <source>Scheme does already exist.</source>
+      <translation>Schéma již existuje.</translation>
     </message>
     <message>
-        <source>Delete scheme</source>
-        <translation type="unfinished"></translation>
+      <source>Delete scheme</source>
+      <translation>Smazat schéma</translation>
     </message>
     <message>
-        <source>Unable to delete current scheme.</source>
-        <translation type="unfinished"></translation>
+      <source>Unable to delete current scheme.</source>
+      <translation>Nelze smazat aktuální schéma.</translation>
     </message>
     <message>
-        <source>&lt;new&gt;</source>
-        <translation type="unfinished"></translation>
+      <source>&lt;new></source>
+      <translation>&lt;nový></translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>EditScheme</name>
     <message>
-        <source>Edit scheme</source>
-        <translation type="unfinished"></translation>
+      <source>Edit scheme</source>
+      <translation>Upravit schéma</translation>
     </message>
     <message>
-        <source>Click here to select a color for: </source>
-        <translation type="unfinished"></translation>
+      <source>Click here to select a color for: </source>
+      <translation>Kliknutím vyberte barvu pro:</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SampleWindow</name>
     <message>
-        <source>Sample</source>
-        <translation type="unfinished"></translation>
+      <source>Sample</source>
+      <translation>Vzorek</translation>
     </message>
     <message>
-        <source>Normal Item</source>
-        <translation type="unfinished"></translation>
+      <source>Normal Item</source>
+      <translation>Normální položka</translation>
     </message>
     <message>
-        <source>Disabled Item</source>
-        <translation type="unfinished"></translation>
+      <source>Disabled Item</source>
+      <translation>Vypnutá položka</translation>
     </message>
     <message>
-        <source>Menu</source>
-        <translation type="unfinished"></translation>
+      <source>Menu</source>
+      <translation>Menu</translation>
     </message>
     <message>
-        <source>Normal Text</source>
-        <translation type="unfinished"></translation>
+      <source>Normal Text</source>
+      <translation>Normální text</translation>
     </message>
     <message>
-        <source>Highlighted Text</source>
-        <translation type="unfinished"></translation>
+      <source>Highlighted Text</source>
+      <translation>Zvýrazněný text</translation>
     </message>
     <message>
-        <source>Button</source>
-        <translation type="unfinished"></translation>
+      <source>Button</source>
+      <translation>Tlačítko</translation>
     </message>
     <message>
-        <source>Check Box</source>
-        <translation type="unfinished"></translation>
+      <source>Check Box</source>
+      <translation>Zaškrtávátko</translation>
     </message>
     <message>
-        <source>Sample window using the selected settings.</source>
-        <translation type="unfinished"></translation>
+      <source>Sample window using the selected settings.</source>
+      <translation>Vzorové okno používá vybrané nastavení.</translation>
     </message>
-</context>
+  </context>
 </TS>
