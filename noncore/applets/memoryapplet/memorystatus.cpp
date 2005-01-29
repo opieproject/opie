9e19c975cd4b69889471a37bdc9b45246659e65b
Michael Lauer <mickey@vanille-media.de>
2005-01-29 12:24:37 +0000

    fix

--- noncore/applets/memoryapplet/memorystatus.cpp
+++ noncore/applets/memoryapplet/memorystatus.cpp
@@ -18,7 +18,7 @@
 **
 **********************************************************************/
 #include "memorystatus.h"
-#include "../../settings/sysinfo/memory.h"
+#include "memory.h"
 #include "swapfile.h"
 
 #include <qpainter.h>
