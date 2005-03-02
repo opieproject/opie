9957b6ff45481e437e11652c4d4fbe1dbb4cb0a9
Holger Freyther <zecke@handhelds.org>
2005-03-02 20:50:45 +0000

    Import Trolltechs qss

---
+++ core/apps/qss/main.cpp
@@ -0,0 +1,55 @@
+/**********************************************************************
+** Copyright (C) 2000-2005 Trolltech AS.  All rights reserved.
+**
+** This file is part of the Qtopia Environment.
+** 
+** This program is free software; you can redistribute it and/or modify it
+** under the terms of the GNU General Public License as published by the
+** Free Software Foundation; either version 2 of the License, or (at your
+** option) any later version.
+** 
+** A copy of the GNU GPL license version 2 is included in this package as 
+** LICENSE.GPL.
+**
+** This program is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+** See the GNU General Public License for more details.
+**
+** In addition, as a special exception Trolltech gives permission to link
+** the code of this program with Qtopia applications copyrighted, developed
+** and distributed by Trolltech under the terms of the Qtopia Personal Use
+** License Agreement. You must comply with the GNU General Public License
+** in all respects for all of the code used other than the applications
+** licensed under the Qtopia Personal Use License Agreement. If you modify
+** this file, you may extend this exception to your version of the file,
+** but you are not obligated to do so. If you do not wish to do so, delete
+** this exception statement from your version.
+** 
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
+#include <qsoundqss_qws.h>
+#include <qtopia/qpeapplication.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+#ifndef SINGLE_EXEC
+int main(int argc, char **argv) 
+{
+    QApplication a(argc, argv);
+    (void)new QWSSoundServer(0);
+
+    //if (1) {
+    // later, check if root first
+	setpriority(PRIO_PROCESS, 0, -15);
+    //}
+    // hook up some quit mech.
+    return a.exec();
+}
+#endif
---
+++ core/apps/qss/qss.pro
@@ -0,0 +1,10 @@
+CONFIG		+= qtopiaapp
+CONFIG		-= buildQuicklaunch
+SOURCES		= main.cpp
+
+TARGET		= qss
+TRANSLATABLES   = $$SOURCES
+
+PACKAGE_DESCRIPTION=QWS Sound Server
+
+TRANSLATIONS=
