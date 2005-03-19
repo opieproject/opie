df4b8c8798854dbdc9e4339e32b189e34f9d1ea3
Rajko Albrecht <alwin@handhelds.org>
2005-03-19 23:08:06 +0000

    added .cvsignore files corrected the name of the christmas holidays

--- core/pim/datebook/holiday/christian/hlist.cpp
+++ core/pim/datebook/holiday/christian/hlist.cpp
@@ -7,7 +7,11 @@ const QStringList HList::entries(const QDate&aDate)const
     QStringList ret;
     if (aDate.month()==12) {
         if (aDate.day()==24) {
-            ret.append(QObject::tr("Christmas","holidays"));
+            ret.append(QObject::tr("Christmas Eve","holidays"));
+        } else if (aDate.day()==25) {
+            ret.append(QObject::tr("Christmas Day","holidays"));
+        } else if (aDate.day()==26) {
+            ret.append(QObject::tr("Boxing Day","holidays"));
         }
     } else if (aDate.month()==1) {
         if (aDate.day()==1) {
