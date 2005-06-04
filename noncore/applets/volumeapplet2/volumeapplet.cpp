41f076db03034b4d8a660aae667f538d102e8ff8
Dan Williams <drw392772@gmail.com>
2005-06-04 00:20:05 +0000

    Resource -> OResource and fix compile warning

--- noncore/applets/volumeapplet2/volumeapplet.cpp
+++ noncore/applets/volumeapplet2/volumeapplet.cpp
@@ -1,27 +1,27 @@
 /*
-                             This file is part of the Opie Project
+                     This file is part of the Opie Project
 
               =.             (C) 2005 Michael 'Mickey' Lauer <mickey@Vanille.de>
             .=l.
-           .>+-=
- _;:,     .>    :=|.         This program is free software; you can
-.> <`_,   >  .   <=          redistribute it and/or  modify it under
-:`=1 )Y*s>-.--   :           the terms of the GNU Library General Public
-.="- .-=="i,     .._         License as published by the Free Software
- - .   .-<_>     .<>         Foundation; either version 2 of the License,
-     ._= =}       :          or (at your option) any later version.
-    .%`+i>       _;_.
-    .i_,=:_.      -<s.       This program is distributed in the hope that
-     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
-    : ..    .:,     . . .    without even the implied warranty of
-    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
-  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
-..}^=.=       =       ;      Library General Public License for more
-++=   -.     .`     .:       details.
- :     =  ...= . :.=-
- -.   .:....=;==+<;          You should have received a copy of the GNU
-  -_. . .   )=.  =           Library General Public License along with
-    --        :-=`           this library; see the file COPYING.LIB.
+     .>+-=
+_;:,   .>  :=|.         This program is free software; you can
+.> <`_,  > .  <=          redistribute it and/or  modify it under
+:`=1 )Y*s>-.--  :           the terms of the GNU Library General Public
+.="- .-=="i,   .._         License as published by the Free Software
+- .  .-<_>   .<>         Foundation; either version 2 of the License,
+  ._= =}    :          or (at your option) any later version.
+  .%`+i>    _;_.
+  .i_,=:_.   -<s.       This program is distributed in the hope that
+  + . -:.    =       it will be useful,  but WITHOUT ANY WARRANTY;
+  : ..  .:,   . . .    without even the implied warranty of
+  =_    +   =;=|`    MERCHANTABILITY or FITNESS FOR A
+ _.=:.    :  :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=    =    ;      Library General Public License for more
+++=  -.   .`   .:       details.
+:   = ...= . :.=-
+-.  .:....=;==+<;          You should have received a copy of the GNU
+ -_. . .  )=. =           Library General Public License along with
+  --    :-=`           this library; see the file COPYING.LIB.
                              If not, write to the Free Software Foundation,
                              Inc., 59 Temple Place - Suite 330,
                              Boston, MA 02111-1307, USA.
@@ -35,8 +35,10 @@
 #include <opie2/otaskbarapplet.h>
 #include <opie2/osoundsystem.h>
 #include <opie2/oledbox.h>
+#include <opie2/oresource.h>
+
 #include <qpe/applnk.h>
-#include <qpe/resource.h>
+
 using namespace Opie::Core;
 using namespace Opie::MM;
 using namespace Opie::Ui;
@@ -91,7 +93,7 @@ void VolumeAppletControl::build()
     QStringList channels = mixer->allChannels();
 
     int x = 0;
-    int y = 0;
+    // int y = 0;
 
     for ( QStringList::Iterator it = channels.begin(); it != channels.end(); ++it )
     {
@@ -135,7 +137,7 @@ VolumeApplet::VolumeApplet( QWidget *parent, const char *name )
 {
     setFixedHeight( AppLnk::smallIconSize() );
     setFixedWidth( AppLnk::smallIconSize() );
-    _pixmap.convertFromImage( Resource::loadImage( "volumeapplet/volume" ).smoothScale( height(), width() ) );
+    _pixmap = Opie::Core::OResource::loadPixmap( "volume", Opie::Core::OResource::SmallIcon );
    _control = new VolumeAppletControl( this, "control" );
 }
 
