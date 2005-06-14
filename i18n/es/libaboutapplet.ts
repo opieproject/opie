6455575d44e44489c48f5a6e14e22efe338269ca
Alberto García Hierro <skyhusker@handhelds.org>
2005-06-14 17:17:13 +0000

    All strings translated

--- i18n/es/libaboutapplet.ts
+++ i18n/es/libaboutapplet.ts
@@ -3,7 +3,7 @@
     <name>AboutApplet</name>
     <message>
         <source>About shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Atajo &apos;Acerca de&apos;</translation>
     </message>
     <message>
         <source>About</source>
@@ -26,7 +26,15 @@ No single group, company or organization controls the Opie source code. Everyone
 &lt;p&gt;
 Visit &lt;i&gt;http://opie.handhelds.org&lt;/i&gt; for more information on the Opie Project.
 &lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;
+&lt;b&gt;Open Palmtop Integrated Environment&lt;/b&gt; está escrito y mantenido por el Equipo de Opie, una red mundial de ingenieros de software comprometidos con el desarrollo de software libre.
+&lt;/p&gt;
+&lt;p&gt;
+Ningún grupo, compañia u organización controla el código fuente de Opie. Todo el mundo puede contribuir.
+&lt;/p&gt;
+&lt;p&gt;
+Visite &lt;i&gt;http://opie.handhelds.org&lt;/i&gt; para más información acerca del Proyecto Opie.
+&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Authors</source>
@@ -40,22 +48,25 @@ Visit &lt;i&gt;http://opie.handhelds.org&lt;/i&gt; for more information on the O
         <source>&lt;p&gt;Software can always be improved, and the Opie Team is ready to do so. However, you - the user - must tell us when something does not work as expected or could be done better.&lt;/p&gt;
 &lt;p&gt;The Open Palmtop Integrated Environment has a bug tracking system. Visit &lt;i&gt;http://opie.handhelds.org/&lt;/i&gt; to report bugs.&lt;/p&gt;
 &lt;p&gt;If you have a suggestion for improvement then you are welcome to use the bug tracking system to register your wish. Make sure you use the severity called &quot;Feature Wish&quot;.&lt;/p&gt;</source>
-        <translation type="unfinished">&lt;p&gt;El software siempre puede mejorarse, y el Equipo de Opie está listo para hacerlo. No obstante, usted - el usuario - debe decirnos qué cree que no funciona como debería o puede mejorarse.&lt;/p&gt;
-&lt;p&gt;</translation>
+        <translation>&lt;p&gt;El software siempre puede mejorarse, y el Equipo de Opie está listo para hacerlo. No obstante, usted - el usuario - debe decirnos qué cree que no funciona como debería o puede mejorarse.&lt;/p&gt;
+&lt;p&gt;Open Palmtop Integrated Environment tiene un sistema de seguimiento de fallos. Visite &lt;i&gt;http://opie.handhelds.org/&lt;/i&gt; para enviarlos.&lt;/p&gt;
+&lt;p&gt;Si tiene cualquier sugerencia para mejorar le animamos a que use el sistema de seguimiento de fallos para registrarla. Asegúrese de usar la severidad &quot;Feature Wish&quot;.&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Join</source>
-        <translation type="unfinished"></translation>
+        <translation>Únase</translation>
     </message>
     <message>
         <source>&lt;p&gt;You don&apos;t have to be a software developer to be a member of the Opie Team. You can join the national teams that translate program interfaces. You can provide graphics, themes, sounds and improved documentation. You decide!&lt;/p&gt;
 &lt;p&gt;Visit &lt;i&gt;http://opie.handhelds.org/&lt;/i&gt; for information on some projects in which you can participate.&lt;/p&gt;
 &lt;p&gt;If you need more information or documentation, then a visit to &lt;i&gt;http://opie.handhelds.org/&lt;/i&gt; will provide you with what you need.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;No necesita ser un desarrollador de software para ser miembro del Equipo Opie. Puede unirse a los equipos nacionales que traducen las interfaces de los programas. También puede proporcionar gráficos, temas, sonidos y documentación mejorada. ¡Usted decide!&lt;/p&gt;
+&lt;p&gt;Visite &lt;i&gt;http://opie.handhelds.org/&lt;/i&gt; si desea informarse sobre los proyectos en los que puede participar&lt;/p&gt;
+&lt;p&gt;Si necesita más información o documentación puede obtenerla en &lt;i&gt;http://opie.handhelds.org/&lt;/i&gt;.&lt;/p&gt;</translation>
     </message>
     <message>
         <source>&lt;center&gt;&lt;b&gt;The Open Palmtop Integrated Environment V1.2&lt;/b&gt;&lt;/center&gt;</source>
-        <translation type="unfinished">&lt;center&gt;&lt;b&gt;E {1.2?}</translation>
+        <translation>&lt;center&gt;&lt;b&gt;The Open Palmtop Integrated Environment V1.2&lt;/b&gt;&lt;/center&gt;</translation>
     </message>
     <message>
         <source>&lt;p&gt;
@@ -70,7 +81,18 @@ Visit &lt;i&gt;http://opie.handhelds.org&lt;/i&gt; for more information on the O
 &lt;b&gt;Andreas &apos;ar&apos; Richter&lt;/b&gt; (ar@handhelds.org)&lt;br/&gt;
 &lt;b&gt;Dan &apos;drw&apos; Williams&lt;/b&gt; (drw@handhelds.org)&lt;/tt&gt;&lt;br/&gt;
 &lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;
+&lt;b&gt;Rajko &apos;alwin&apos; Albrecht&lt;/b&gt; (alwin@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Stefan &apos;eilers&apos; Eilers&lt;/b&gt; (eilers@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Holger &apos;Zecke&apos; Freyther&lt;/b&gt; (zecke@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Alberto &apos;Skyhusker&apos; Garcia Hierro&lt;/b&gt; (skyhusker@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Chris &apos;Kergoth&apos; Larson&lt;/b&gt; (kergoth@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Michael &apos;Mickey&apos; Lauer&lt;/b&gt; (mickeyl@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Lorn &apos;ljp&apos; Potter&lt;/b&gt; (ljp@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Max &apos;Harlekin&apos; Reiss&lt;/b&gt; (harlekin@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Andreas &apos;ar&apos; Richter&lt;/b&gt; (ar@handhelds.org)&lt;br/&gt;
+&lt;b&gt;Dan &apos;drw&apos; Williams&lt;/b&gt; (drw@handhelds.org)&lt;/tt&gt;&lt;br/&gt;
+&lt;/p&gt;</translation>
     </message>
 </context>
 </TS>
