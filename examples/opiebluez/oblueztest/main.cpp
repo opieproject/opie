8f62e8b75441dc2b6019c27275bf668cf564a084
Michael Lauer <mickey@vanille-media.de>
2005-07-10 13:12:10 +0000

    opieshower: add BT device enumeration

--- examples/opiebluez/oblueztest/main.cpp
+++ examples/opiebluez/oblueztest/main.cpp
@@ -1,5 +1,21 @@
+#include <opie2/odebug.h>
+#include <opie2/obluetooth.h>
+#include <opie2/oinfrared.h>
+
+using namespace Opie::Core;
+using namespace Opie::Shower;
+
 int main( int argc, char** argv )
 {
+    OBluetooth* sys = OBluetooth::instance();
+
+    OBluetooth::InterfaceIterator it = sys->iterator();
+
+    while( it.current() )
+    {
+        odebug << "APP: Bluetooth host controller interface '" << it.current()->name() << "' has MAC '" << it.current()->macAddress() << "'" << oendl;
+        ++it;
+    }
     return 0;
 }
 
