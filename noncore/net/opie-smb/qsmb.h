a3b96fc3b6840cfb83ffd2ff60d3e10f71418fcc
Lorn Potter <lpotter@trolltech.com>
2005-08-16 10:22:06 +0000

    proper opie header and maintainter

--- noncore/net/opie-smb/qsmb.h
+++ noncore/net/opie-smb/qsmb.h
@@ -1,3 +1,31 @@
+/*
+               =.            This file is part of the OPIE Project
+             .=l.            Copyright (c)  2002-2005  Kurt Korbatits <support@midget.net.au>
+           .>+-=             Copyright (c)  2005  L. Potter <lpotter@trolltech.com>
+ _;:,     .>    :=|.         This program is free software; you can 
+.> <`_,   >  .   <=          redistribute it and/or  modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU General Public
+.="- .-=="i,     .._         License as published by the Free Software
+ - .   .-<_>     .<>         Foundation; either version 2 of the License,
+     ._= =}       :          or (at your option) any later version.
+    .%`+i>       _;_.        
+    .i_,=:_.      -<s.       This program is distributed in the hope that  
+     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    : ..    .:,     . . .    without even the implied warranty of
+    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=       =       ;      General Public License for more
+++=   -.     .`     .:       details.
+ :     =  ...= . :.=-        
+ -.   .:....=;==+<;          You should have received a copy of the GNU
+  -_. . .   )=.  =           General Public License along with
+    --        :-=`           this library; see the file COPYING.BIN. 
+                             If not, write to the Free Software Foundation,
+                             Inc., 59 Temple Place - Suite 330,
+                             Boston, MA 02111-1307, USA.
+
+*/
+
 #ifndef QSMB_H
 #define QSMB_H
 
