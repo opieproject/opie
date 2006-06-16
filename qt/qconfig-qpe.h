41c72249fc135699c710042000d0943389bacf60
Lorn Potter <lpotter@trolltech.com>
2006-06-16 23:50:43 +0000

    update this for qt4

--- qt/qconfig-qpe.h
+++ qt/qconfig-qpe.h
@@ -1,7 +1,7 @@
-/**********************************************************************
-** Copyright (C) 2000 Trolltech AS.  All rights reserved.
+/****************************************************************************
+** Copyright (C) 2000-2006 Trolltech ASA.  All rights reserved.
 **
-** This file is part of Qtopia Environment.
+** This file is part of Qtopia Toolkit.
 **
 ** This file may be distributed and/or modified under the terms of the
 ** GNU General Public License version 2 as published by the Free Software
@@ -16,84 +16,162 @@
 ** Contact info@trolltech.com if any conditions of this licensing are
 ** not clear to you.
 **
-**********************************************************************/
-#ifndef QT_H
-#endif // QT_H
+****************************************************************************/
 
-// Empty leaves all features enabled.  See doc/html/features.html for choices.
-
-// Note that disabling some features will produce a libqt that is not
-// compatible with other libqt builds. Such modifications are only
-// supported on Qt/Embedded where reducing the library size is important
-// and where the application-suite is often a fixed set.
-
-#ifndef QT_DLL
-#define QT_DLL // Internal
+// Data structures
+#ifndef QT_NO_STL
+#  define QT_NO_STL
 #endif
 
-#if defined(QT_QWS_IPAQ) || defined(QT_QWS_CASSIOPEIA) || defined(QT_QWS_SL5XXX)
-# define QT_NO_QWS_CURSOR
-# define QT_NO_QWS_MOUSE_AUTO
+// Dialogs
+#ifndef QT_NO_COLORDIALOG
+#  define QT_NO_COLORDIALOG
+#endif
+#ifndef QT_NO_ERRORMESSAGE
+#  define QT_NO_ERRORMESSAGE
+#endif
+#ifndef QT_NO_INPUTDIALOG
+#  define QT_NO_INPUTDIALOG
+#endif
+#ifndef QT_NO_FILEDIALOG
+#  define QT_NO_FILEDIALOG
+#endif
+#ifndef QT_NO_PROGRESSDIALOG
+#  define QT_NO_PROGRESSDIALOG
+#endif
+#ifndef QT_NO_TABDIALOG
+#  define QT_NO_TABDIALOG
+#endif
+
+// File I/O
+#ifndef QT_NO_DOM
+#  define QT_NO_DOM
+#endif
+
+// Fonts
+#ifndef QT_NO_FREETYPE
+#  define QT_NO_FREETYPE
+#endif
+
+// Images
+#ifndef QT_NO_IMAGEFORMAT_PPM
+#  define QT_NO_IMAGEFORMAT_PPM
+#endif
+#ifndef QT_NO_IMAGE_HEURISTIC_MASK
+#  define QT_NO_IMAGE_HEURISTIC_MASK
+#endif
+#ifndef QT_NO_IMAGE_TEXT
+#  define QT_NO_IMAGE_TEXT
+#endif
+
+// Internationalization
+#ifndef QT_NO_BIG_CODECS
+#  define QT_NO_BIG_CODECS
 #endif
 #ifndef QT_NO_CODECS
-#define QT_NO_CODECS
+#  define QT_NO_CODECS
 #endif
-#define QT_NO_UNICODETABLES
-//#define QT_NO_IMAGEIO_BMP
-#define QT_NO_IMAGEIO_PPM
-//#define QT_NO_ASYNC_IO
-//#define QT_NO_ASYNC_IMAGE_IO
-#define QT_NO_FREETYPE
-#define QT_NO_BDF
-//#define QT_NO_FONTDATABASE
-#define QT_NO_DRAGANDDROP
-//#define QT_NO_CLIPBOARD
-#define QT_NO_PROPERTIES
-#define QT_NO_NETWORKPROTOCOL
 
-#define QT_NO_IMAGE_TEXT
-
-//#define QT_NO_TOOLTIP
-#define QT_NO_COLORNAMES
-#define QT_NO_TRANSFORMATIONS
-#define QT_NO_TRANSLATION_BUILDER
-#define QT_NO_COMPLEXTEXT
-#define QT_NO_PRINTER
-#define QT_NO_PICTURE
-//#define QT_NO_ICONVIEW
-#define QT_NO_DIAL
-#define QT_NO_SIZEGRIP
-#define QT_NO_WORKSPACE
-//#define QT_NO_TABLE
-//#define QT_NO_ACTION
-//#define QT_NO_SETTINGS
-#define QT_NO_STYLE_POCKETPC
-#ifndef QT_NO_STYLE_AQUA
-# define QT_NO_STYLE_AQUA
+// ItemViews
+#ifndef QT_NO_DIRMODEL
+#  define QT_NO_DIRMODEL
 #endif
-#define QT_NO_STYLE_MOTIF
-#define QT_NO_STYLE_PLATINUM
-#define QT_NO_FILEDIALOG
-#define QT_NO_FONTDIALOG
-#define QT_NO_PRINTDIALOG
-#define QT_NO_COLORDIALOG
-#define QT_NO_INPUTDIALOG
-//#define QT_NO_MESSAGEBOX
-#define QT_NO_PROGRESSDIALOG
-//#define QT_NO_TABDIALOG
-#define QT_NO_WIZARD
-#define QT_NO_EFFECTS
-//#define QT_NO_COMPONENT
-#define QT_NO_DOM
-#define QT_NO_SEMIMODAL
-//#define QT_NO_PROGRESSBAR
-#define QT_NO_SPLITTER
 
-//#define QT_NO_QWS_SAVEFONTS
-//#define QT_NO_QWS_PROPERTIES
+// Kernel
+#ifndef QT_NO_CURSOR
+#  define QT_NO_CURSOR
+#endif
+#ifndef QT_NO_DRAGANDDROP
+#  define QT_NO_DRAGANDDROP
+#endif
+#ifndef QT_NO_EFFECTS
+#  define QT_NO_EFFECTS
+#endif
+#ifndef QT_NO_SESSIONMANAGER
+#  define QT_NO_SESSIONMANAGER
+#endif
 
-#define QT_NO_QWS_BEOS_WM_STYLE
-#define QT_NO_QWS_KDE2_WM_STYLE
-#define QT_NO_QWS_KDE_WM_STYLE
-#define QT_NO_QWS_WINDOWS_WM_STYLE
+// Networking
+#ifndef QT_NO_NETWORKPROXY
+#  define QT_NO_NETWORKPROXY
+#endif
+#ifndef QT_NO_SOCKS5
+#  define QT_NO_SOCKS5
+#endif
 
+// Painting
+#ifndef QT_NO_COLORNAMES
+#  define QT_NO_COLORNAMES
+#endif
+#ifndef QT_NO_PICTURE
+#  define QT_NO_PICTURE
+#endif
+
+// Qtopia Core
+#ifndef QT_NO_QWS_ALPHA_CURSOR
+#  define QT_NO_QWS_ALPHA_CURSOR
+#endif
+#ifndef QT_NO_QWS_CURSOR
+#  define QT_NO_QWS_CURSOR
+#endif
+#ifndef QT_NO_QWS_DECORATION_WINDOWS
+#  define QT_NO_QWS_DECORATION_WINDOWS
+#endif
+#ifndef QT_NO_QWS_MOUSE
+#  define QT_NO_QWS_MOUSE
+#endif
+#ifndef QT_NO_QWS_MOUSE_AUTO
+#  define QT_NO_QWS_MOUSE_AUTO
+#endif
+#ifndef QT_NO_QWS_MOUSE_MANUAL
+#  define QT_NO_QWS_MOUSE_MANUAL
+#endif
+
+// Styles
+#ifndef QT_NO_STYLE_MOTIF
+#  define QT_NO_STYLE_MOTIF
+#endif
+#ifndef QT_NO_STYLE_CDE
+#  define QT_NO_STYLE_CDE
+#endif
+#ifndef QT_NO_STYLE_PLASTIQUE
+#  define QT_NO_STYLE_PLASTIQUE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSXP
+#  define QT_NO_STYLE_WINDOWSXP
+#endif
+
+// Widgets
+#ifndef QT_NO_DOCKWIDGET
+#  define QT_NO_DOCKWIDGET
+#endif
+#ifndef QT_NO_WORKSPACE
+#  define QT_NO_WORKSPACE
+#endif
+#ifndef QT_NO_CONTEXTMENU
+#  define QT_NO_CONTEXTMENU
+#endif
+#ifndef QT_NO_SPLITTER
+#  define QT_NO_SPLITTER
+#endif
+#ifndef QT_NO_SIZEGRIP
+#  define QT_NO_SIZEGRIP
+#endif
+#ifndef QT_NO_DIAL
+#  define QT_NO_DIAL
+#endif
+#ifndef QT_NO_SYNTAXHIGHLIGHTER
+#  define QT_NO_SYNTAXHIGHLIGHTER
+#endif
+#ifndef QT_NO_STATUSBAR
+#  define QT_NO_STATUSBAR
+#endif
+#ifndef QT_NO_STATUSTIP
+#  define QT_NO_STATUSTIP
+#endif
+#ifndef QT_NO_TOOLBOX
+#  define QT_NO_TOOLBOX
+#endif
+#ifndef QT_NO_TOOLTIP
+#  define QT_NO_TOOLTIP
+#endif
