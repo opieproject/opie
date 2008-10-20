63826fb015e26e278289367bbbdd66b756465c18
Paul Eggleton <bluelightning@bluelightning.org>
2008-10-20 16:59:10 +0000

    Add day view files unmodified from datebook1

---
+++ core/pim/datebook2/views/day/datebookdayallday.h
@@ -0,0 +1,117 @@
+/*
+                             This file is part of the Opie Project
+
+                             Copyright (C) Rajko Albrecht <>
+                             Copyright (C) Opie Team <opie-devel@handhelds.org>
+              =.
+            .=l.
+           .>+-=
+ _;:,     .>    :=|.         This program is free software; you can
+.> <`_,   >  .   <=          redistribute it and/or  modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU Library General Public
+.="- .-=="i,     .._         License as published by the Free Software
+ - .   .-<_>     .<>         Foundation; either version 2 of the License,
+     ._= =}       :          or (at your option) any later version.
+    .%`+i>       _;_.
+    .i_,=:_.      -<s.       This program is distributed in the hope that
+     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    : ..    .:,     . . .    without even the implied warranty of
+    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=       =       ;      Library General Public License for more
+++=   -.     .`     .:       details.
+:     =  ...= . :.=-
+ -.   .:....=;==+<;          You should have received a copy of the GNU
+  -_. . .   )=.  =           Library General Public License along with
+    --        :-=`           this library; see the file COPYING.LIB.
+                             If not, write to the Free Software Foundation,
+                             Inc., 59 Temple Place - Suite 330,
+                             Boston, MA 02111-1307, USA.
+*/
+
+#ifndef DATEBOOKDAYALLDAYBASE_H
+#define DATEBOOKDAYALLDAYBASE_H
+
+#include <qvariant.h>
+#include <qframe.h>
+#include <qscrollview.h>
+#include <qlabel.h>
+#include <qlist.h>
+#include <qpe/event.h>
+
+class QVBoxLayout;
+class QHBoxLayout;
+class QGridLayout;
+class DatebookAlldayDisp;
+class DatebookEventDesc;
+class DateBookDB;
+
+class DatebookdayAllday : public QScrollView
+{
+    Q_OBJECT
+
+public:
+    DatebookdayAllday(DateBookDB* db,
+            QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
+    ~DatebookdayAllday();
+    DatebookAlldayDisp* addEvent(const EffectiveEvent&e);
+    DatebookAlldayDisp* addHoliday(const QString&e);
+    const unsigned int items()const{return item_count;}
+
+public slots:
+    void removeAllEvents();
+
+protected:
+    QFrame * m_MainFrame;
+    QVBoxLayout* datebookdayalldayLayout;
+    DatebookEventDesc * lblDesc;
+    unsigned int item_count;
+    QList<DatebookAlldayDisp> subWidgets;
+    DateBookDB *dateBook;
+};
+
+class DatebookAlldayDisp : public QLabel
+{
+    Q_OBJECT
+
+public:
+    DatebookAlldayDisp(DateBookDB* db,const EffectiveEvent& e,
+                       QWidget* parent=0,const char* name = 0, WFlags fl=0);
+    DatebookAlldayDisp(const QString&aholiday,
+                       QWidget* parent=0,const char* name = 0, WFlags fl=0);
+    virtual ~DatebookAlldayDisp();
+
+signals:
+    void deleteMe( const Event &e );
+    void duplicateMe( const Event &e );
+    void editMe( const Event &e );
+    void beamMe( const Event &e );
+    void displayMe(const Event &e);
+
+public slots:
+
+protected:
+    EffectiveEvent m_Ev;
+    DateBookDB* dateBook;
+    void mousePressEvent( QMouseEvent *e );
+    void beam_single_event();
+    bool m_holiday:1;
+};
+
+class DatebookEventDesc: public QLabel
+{
+    Q_OBJECT
+
+public:
+    DatebookEventDesc(QWidget* Parent=0,const char* name = 0);
+    virtual ~DatebookEventDesc();
+
+public slots:
+    void disp_event(const Event&e);
+
+protected:
+    void mousePressEvent(QMouseEvent*e);
+    QTimer* m_Timer;
+};
+
+#endif // DATEBOOKDAYALLDAYBASE_H
