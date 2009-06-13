4099a4b66bbbfb10c2cbb423b9d35cb2c5921cc1
Paul Eggleton <bluelightning@bluelightning.org>
2009-06-13 12:01:08 +0000

    Fix indenting/other formatting; add standard Opie code header

--- noncore/tools/pimconverter/converter.cpp
+++ noncore/tools/pimconverter/converter.cpp
@@ -1,3 +1,32 @@
+/*
+                             This file is part of the Opie Project
+                             Copyright (C) 2009 The Opie Team <opie-devel@handhelds.org>
+              =.             
+            .=l.
+           .>+-=
+ _;:,     .>    :=|.         This program is free software; you can
+.> <`_,   >  .   <=          redistribute it and/or  modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU Library General Public
+.="- .-=="i,     .._         License as published by the Free Software
+ - .   .-<_>     .<>         Foundation; either version 2 of the License,
+     ._= =}       :          or (at your option) any later version.
+    .%`+i>       _;_.
+    .i_,=:_.      -<s.       This program is distributed in the hope that
+     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    : ..    .:,     . . .    without even the implied warranty of
+    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=       =       ;      Library General Public License for more
+++=   -.     .`     .:       details.
+ :     =  ...= . :.=-
+ -.   .:....=;==+<;          You should have received a copy of the GNU
+  -_. . .   )=.  =           Library General Public License along with
+    --        :-=`           this library; see the file COPYING.LIB.
+                             If not, write to the Free Software Foundation,
+                             Inc., 59 Temple Place - Suite 330,
+                             Boston, MA 02111-1307, USA.
+*/
+
 #include "converter.h"
 
 #include <stdlib.h> // For "system()" command
@@ -52,108 +81,120 @@ void Converter::selectedSourceFormat( int num )
     m_selectedSourceFormat = num;
 }
 
-void Converter::start_conversion(){
-
+void Converter::start_conversion()
+{
     // Creating backends to the requested databases..
     OPimBase* sourceDB;
     OPimBase* destDB;
 
     odebug << "SourceFormat: " <<  m_selectedSourceFormat << oendl;
     odebug << "DestFormat: " << m_selectedDestFormat << oendl;
-    if ( m_selectedSourceFormat == m_selectedDestFormat ){
+    if ( m_selectedSourceFormat == m_selectedDestFormat ) {
 
-	    QMessageBox::warning( this, tr("PimConverter"),
-				  tr( "<qt>It is not a good idea to use"
-				      "the same source and destformat !</qt>"));
-	    return;
-    }
-
-    switch( m_selectedSourceFormat ){
-    case XML:
-        odebug << "XMLSourceDB = " << m_selectedDatabase << "" << oendl;
-        switch( m_selectedDatabase ){
-        case ADDRESSBOOK:{
-            sourceDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::XML, "converter" );
-	}
-            break;
-        case TODOLIST:{
-            sourceDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::XML, "converter" );
-            }break;
-        case DATEBOOK:{
-            sourceDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::XML, "converter" );
-                }
-            break;
-        default:
-            owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
-            return;
-        }
-        break;
-    case SQL:
-        odebug << "SQLSourceDB = " << m_selectedDatabase << "" << oendl;
-        switch( m_selectedDatabase ){
-        case ADDRESSBOOK:{
-            sourceDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::SQL, "converter" );
-                }
-            break;
-        case TODOLIST:{
-            sourceDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::SQL, "converter" );
-            }break;
-        case DATEBOOK:{
-            sourceDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::SQL, "converter" );
-                }
-            break;
-        default:
-            owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
-            return;
-        }
-        break;
-    default:
-        owarn << "Unknown source format selected (" << m_selectedSourceFormat << ") !!" << oendl;
+        QMessageBox::warning( this, tr("PimConverter"),
+                  tr( "<qt>It is not a good idea to use"
+                      "the same source and destformat !</qt>"));
         return;
     }
 
-    switch ( m_selectedDestFormat ){
-    case XML:
-        odebug << "XMLDestDB = " << m_selectedDatabase << "" << oendl;
-        switch( m_selectedDatabase ){
-        case ADDRESSBOOK:{
-            destDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::XML, "converter" );
-                }
+    switch( m_selectedSourceFormat ) {
+        case XML:
+            odebug << "XMLSourceDB = " << m_selectedDatabase << "" << oendl;
+            switch( m_selectedDatabase ) {
+                case ADDRESSBOOK:
+                    {
+                        sourceDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::XML, "converter" );
+                    }
+                    break;
+                case TODOLIST:
+                    {
+                        sourceDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::XML, "converter" );
+                    }
+                    break;
+                case DATEBOOK:
+                    {
+                        sourceDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::XML, "converter" );
+                    }
+                    break;
+                default:
+                    owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+                    return;
+            }
             break;
-        case TODOLIST:{
-            destDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::XML, "converter" );
-            }break;
-        case DATEBOOK:{
-            destDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::XML, "converter" );
-                }
+        case SQL:
+            odebug << "SQLSourceDB = " << m_selectedDatabase << "" << oendl;
+            switch( m_selectedDatabase ) {
+                case ADDRESSBOOK:
+                    {
+                        sourceDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::SQL, "converter" );
+                    }
+                    break;
+                case TODOLIST: 
+                    {
+                        sourceDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::SQL, "converter" );
+                    }
+                    break;
+                case DATEBOOK:
+                    {
+                        sourceDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::SQL, "converter" );
+                    }
+                    break;
+                default:
+                    owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+                    return;
+            }
             break;
         default:
-            owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+            owarn << "Unknown source format selected (" << m_selectedSourceFormat << ") !!" << oendl;
             return;
-        }
-        break;
-    case SQL:
-        odebug << "SQLDestDB = " << m_selectedDatabase << "" << oendl;
-        switch( m_selectedDatabase ){
-        case ADDRESSBOOK:{
-            destDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::SQL, "converter" );
-                }
+    }
+
+    switch ( m_selectedDestFormat ) {
+        case XML:
+            odebug << "XMLDestDB = " << m_selectedDatabase << "" << oendl;
+            switch( m_selectedDatabase ) {
+                case ADDRESSBOOK:
+                    {
+                        destDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::XML, "converter" );
+                    }
+                    break;
+                case TODOLIST:
+                    {
+                        destDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::XML, "converter" );
+                    }
+                    break;
+                case DATEBOOK:
+                    {
+                        destDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::XML, "converter" );
+                    }
+                    break;
+                default:
+                    owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+                    return;
+            }
             break;
-        case TODOLIST:{
-            destDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::SQL, "converter" );
-            }break;
-        case DATEBOOK:{
-            destDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::SQL, "converter" );
-                }
+        case SQL:
+            odebug << "SQLDestDB = " << m_selectedDatabase << "" << oendl;
+            switch( m_selectedDatabase ) {
+                case ADDRESSBOOK:{
+                    destDB = OPimAccessFactory<OPimContactAccess>::create( OPimGlobal::CONTACTLIST, OPimGlobal::SQL, "converter" );
+                        }
+                    break;
+                case TODOLIST:{
+                    destDB = OPimAccessFactory<OPimTodoAccess>::create( OPimGlobal::TODOLIST, OPimGlobal::SQL, "converter" );
+                    }break;
+                case DATEBOOK:{
+                    destDB = OPimAccessFactory<ODateBookAccess>::create( OPimGlobal::DATEBOOK, OPimGlobal::SQL, "converter" );
+                        }
+                    break;
+                default:
+                    owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+                    return;
+            }
             break;
         default:
-            owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+            owarn << "Unknown destination format selected (" << m_selectedDestFormat << ")!!" << oendl;
             return;
-        }
-        break;
-    default:
-        owarn << "Unknown destination format selected (" << m_selectedDestFormat << ")!!" << oendl;
-        return;
     }
 
     if ( !sourceDB || !destDB )
@@ -168,7 +209,7 @@ void Converter::start_conversion(){
     t.start();
 
     // Clean the dest-database if requested (isChecked)
-    if ( m_eraseDB -> isChecked() ){
+    if ( m_eraseDB -> isChecked() ) {
         odebug << "Clearing destination database!" << oendl;
         destDB -> clear();
     }
@@ -178,7 +219,7 @@ void Converter::start_conversion(){
     odebug << "Try to move data for addressbook.. (" << uidList.count() << " items) " << oendl;
     m_progressBar->setTotalSteps( uidList.count() );
     int count = 0;
-    for ( uint i = 0; i < uidList.count(); ++i ){
+    for ( uint i = 0; i < uidList.count(); ++i ) {
         odebug << "Adding uid: " << uidList[i] << "" << oendl;
         OPimRecord* rec = sourceDB -> record( uidList[i] );
         destDB -> add( rec );
@@ -192,50 +233,46 @@ void Converter::start_conversion(){
 
     // Delete the frontends. Backends will be deleted automatically, too !
     // We have to cast them back to delete them properly !
-    switch( m_selectedDatabase ){
-    case ADDRESSBOOK:
-        delete static_cast<OPimContactAccess*> (sourceDB);
-        delete static_cast<OPimContactAccess*> (destDB);
-        break;
-    case TODOLIST:
-        delete static_cast<OPimTodoAccess*> (sourceDB);
-        delete static_cast<OPimTodoAccess*> (destDB);
-        break;
-    case DATEBOOK:
-        delete static_cast<ODateBookAccess*> (sourceDB);
-        delete static_cast<ODateBookAccess*> (destDB);
-        break;
-    default:
-        owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
-        return;
+    switch( m_selectedDatabase ) {
+        case ADDRESSBOOK:
+            delete static_cast<OPimContactAccess*> (sourceDB);
+            delete static_cast<OPimContactAccess*> (destDB);
+            break;
+        case TODOLIST:
+            delete static_cast<OPimTodoAccess*> (sourceDB);
+            delete static_cast<OPimTodoAccess*> (destDB);
+            break;
+        case DATEBOOK:
+            delete static_cast<ODateBookAccess*> (sourceDB);
+            delete static_cast<ODateBookAccess*> (destDB);
+            break;
+        default:
+            owarn << "Unknown database selected (" << m_selectedDatabase << ")" << oendl;
+            return;
     }
-
 
     owarn << "Conversion is finished and needed " << t.elapsed() << " ms !" << oendl;
 }
 
 void Converter::closeEvent( QCloseEvent *e )
 {
-
-	/* Due to the fact that we don't have multitasking here, this
-	 * critical handling don't make sense, but the future..
-	 */
-	if ( m_criticalState ){
-		e->ignore();
-		return;
-	}
-	e->accept();
+    /* Due to the fact that we don't have multitasking here, this
+     * critical handling don't make sense, but the future..
+     */
+    if ( m_criticalState ) {
+        e->ignore();
+        return;
+    }
+    e->accept();
 }
 
 bool Converter::sqliteMoveAndConvert( const QString& name, const QString& src,
                                       const QString &dest )
 {
-
     QMessageBox::information(
                     this, tr( "Pim-Converter" ),
                     tr( "<qt>Starting convert of database %1</qt>" ).arg( name )
                             );
-
 
     bool error = false;
     QString cmd;
@@ -249,48 +286,44 @@ bool Converter::sqliteMoveAndConvert( const QString& name, const QString& src,
         error = true;
     }
 
-
-    if ( error ){
+    if ( error ) {
         QMessageBox::critical( this, tr("Pim-Converter"),
                                tr("<qt>Conversion not possible: <br>"
                                   "Problem: %1</qt>").arg(cmd) );
         return error;
     }
 
-
     /*
      * Move it over
      */
     cmd = "mv " + Global::shellQuote(src) + " " + Global::shellQuote(dest);
-    if( ::system( cmd ) != 0 ){
+    if( ::system( cmd ) != 0 ) {
         error = true;
     }
-
 
     /*
      * Convert it
      */
-    if ( !error ){
+    if ( !error ) {
         cmd = "sqlite " + Global::shellQuote(dest) + " .dump | sqlite3 "
               + Global::shellQuote(src);
         if ( ::system( cmd ) != 0 )
             error = true;
     }
 
-
     /*
      * Check whether conversion really worked. If not, move old database back to
      * recover it
      */
-    if ( !QFile::exists( src ) ){
+    if ( !QFile::exists( src ) ) {
         cmd = "mv " + Global::shellQuote(dest) + " " + Global::shellQuote(src);
-        if ( ::system( cmd ) != 0 ){
+        if ( ::system( cmd ) != 0 ) {
             error = true;
             cmd = "Database-Format is not V2!?";
         }
     }
 
-    if ( error ){
+    if ( error ) {
         QMessageBox::critical( this, tr("Pim-Converter"),
                                tr("<qt>An internal error occurred: <br>"
                                   "Converting the database was impossible! <br>"
@@ -299,60 +332,59 @@ bool Converter::sqliteMoveAndConvert( const QString& name, const QString& src,
     return error;
 }
 
-
-
 void Converter::start_upgrade()
 {
-	odebug << "Start upgrading" << oendl;
-	switch( QMessageBox::warning( this, tr("Pim-Converter"),
-				      tr("<qt>Are you really sure that you "
-				      "want to convert your database from "
-				      "sqlite V2 to sqlite V3?</qt>"),
-				      QMessageBox::Ok | QMessageBox::Default,
-				      QMessageBox::Abort | QMessageBox::Escape )) {
+    odebug << "Start upgrading" << oendl;
+    switch( QMessageBox::warning( this, tr("Pim-Converter"),
+                      tr("<qt>Are you really sure that you "
+                      "want to convert your database from "
+                      "sqlite V2 to sqlite V3?</qt>"),
+                      QMessageBox::Ok | QMessageBox::Default,
+                      QMessageBox::Abort | QMessageBox::Escape )) 
+    {
+        case QMessageBox::Abort: // Abort clicked or Escape pressed
+            // abort
+            return;
+            break;
+    }
 
-	case QMessageBox::Abort: // Abort clicked or Escape pressed
-		// abort
-		return;
-		break;
-	}
-	odebug << "Checking whether sqlite is installed" << oendl;
-	if ( system( "which sqlite" ) != 0 ){
-		QMessageBox::critical( this, tr("Pim-Converter"),
-				       tr("<qt>An internal error occurred: "
-				       "sqlite was not accessible! "
-				       "Please correct the PATH or install "
-				       "this packages!</qt>") );
-		return;
-	}
-	if ( system( "which sqlite3" ) != 0 ){
-                QMessageBox::critical( this, tr("Pim-Converter"),
-                                       tr("<qt>An internal error occurred: "
-                                          "sqlite3 was not accessible! "
-                                          "Please correct the PATH or install "
-                                          "this packages!</qt>") );
-		return;
-        }
+    odebug << "Checking whether sqlite is installed" << oendl;
+    if ( system( "which sqlite" ) != 0 ) {
+        QMessageBox::critical( this, tr("Pim-Converter"),
+                       tr("<qt>An internal error occurred: "
+                       "sqlite was not accessible! "
+                       "Please correct the PATH or install "
+                       "this packages!</qt>") );
+        return;
+    }
 
-        QString src, dest;
-	bool error = false;
+    if ( system( "which sqlite3" ) != 0 ) {
+        QMessageBox::critical( this, tr("Pim-Converter"),
+                                tr("<qt>An internal error occurred: "
+                                    "sqlite3 was not accessible! "
+                                    "Please correct the PATH or install "
+                                    "this packages!</qt>") );
+        return;
+    }
 
-        src  = Global::applicationFileName("addressbook", "addressbook.db"    );
-        dest = Global::applicationFileName("addressbook", "addressbook.db_v2" );
-	error = sqliteMoveAndConvert( "Addressbook", src, dest );
+    QString src, dest;
+    bool error = false;
 
-        src  = Global::applicationFileName("datebook", "datebook.db"    );
-        dest = Global::applicationFileName("datebook", "datebook.db_v2" );
-	error = sqliteMoveAndConvert( "Datebook", src, dest );
+    src  = Global::applicationFileName("addressbook", "addressbook.db"    );
+    dest = Global::applicationFileName("addressbook", "addressbook.db_v2" );
+    error = sqliteMoveAndConvert( "Addressbook", src, dest );
 
+    src  = Global::applicationFileName("datebook", "datebook.db"    );
+    dest = Global::applicationFileName("datebook", "datebook.db_v2" );
+    error = sqliteMoveAndConvert( "Datebook", src, dest );
 
-        src  = Global::applicationFileName("todolist", "todolist.db"    );
-        dest = Global::applicationFileName("todolist", "todolist.db_v2" );
-	error = sqliteMoveAndConvert( "TodoList", src, dest );
+    src  = Global::applicationFileName("todolist", "todolist.db"    );
+    dest = Global::applicationFileName("todolist", "todolist.db_v2" );
+    error = sqliteMoveAndConvert( "TodoList", src, dest );
 
-	if ( !error ){
-		QMessageBox::information( this, tr("Pim-Converter"),
-					  tr("Conversion is finished!"),
-					  QMessageBox::Ok );
-	}
+    if ( !error ) {
+        QMessageBox::information( this, tr("Pim-Converter"),
+                      tr("Conversion is finished!"),
+                      QMessageBox::Ok );
+    }
 }
--- noncore/tools/pimconverter/converter.h
+++ noncore/tools/pimconverter/converter.h
@@ -1,3 +1,32 @@
+/*
+                             This file is part of the Opie Project
+                             Copyright (C) 2009 The Opie Team <opie-devel@handhelds.org>
+              =.             
+            .=l.
+           .>+-=
+ _;:,     .>    :=|.         This program is free software; you can
+.> <`_,   >  .   <=          redistribute it and/or  modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU Library General Public
+.="- .-=="i,     .._         License as published by the Free Software
+ - .   .-<_>     .<>         Foundation; either version 2 of the License,
+     ._= =}       :          or (at your option) any later version.
+    .%`+i>       _;_.
+    .i_,=:_.      -<s.       This program is distributed in the hope that
+     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    : ..    .:,     . . .    without even the implied warranty of
+    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=       =       ;      Library General Public License for more
+++=   -.     .`     .:       details.
+ :     =  ...= . :.=-
+ -.   .:....=;==+<;          You should have received a copy of the GNU
+  -_. . .   )=.  =           Library General Public License along with
+    --        :-=`           this library; see the file COPYING.LIB.
+                             If not, write to the Free Software Foundation,
+                             Inc., 59 Temple Place - Suite 330,
+                             Boston, MA 02111-1307, USA.
+*/
+
 #ifndef _CONVERTER_H_
 #define _CONVERTER_H_
 
@@ -7,42 +36,42 @@
 
 class Converter: public converter_base {
 public:
-        Converter(QWidget *parent, const char* name, WFlags fl);
-	static QString appName() { return QString::fromLatin1("opimconverter");}
+    Converter(QWidget *parent, const char* name, WFlags fl);
+    static QString appName() { return QString::fromLatin1("opimconverter");}
 
-	// Slots defined in the ui-description file
-	void start_conversion();
-	void start_upgrade();
-	void selectedDatabase( int num );
-	void selectedDestFormat( int num );
-	void selectedSourceFormat( int num );
+    // Slots defined in the ui-description file
+    void start_conversion();
+    void start_upgrade();
+    void selectedDatabase( int num );
+    void selectedDestFormat( int num );
+    void selectedSourceFormat( int num );
 
-	void closeEvent( QCloseEvent *e );
-	
+    void closeEvent( QCloseEvent *e );
+    
 private:
-	// Caution:
-	// The order and value of the following enums must be regarding
-	// the predefinition in the UI-File !!
-	// If you don't understand what I am talking about: Keep your fingers away!!
-	enum DataBases{
-		ADDRESSBOOK = 0,
-		TODOLIST    = 1,
-		DATEBOOK    = 2,
-	};
+    // Caution:
+    // The order and value of the following enums must be regarding
+    // the predefinition in the UI-File !!
+    // If you don't understand what I am talking about: Keep your fingers away!!
+    enum DataBases{
+        ADDRESSBOOK = 0,
+        TODOLIST    = 1,
+        DATEBOOK    = 2,
+    };
 
-	enum DbFormats{
-		XML = 0,
-		SQL = 1,
-	};
+    enum DbFormats{
+        XML = 0,
+        SQL = 1,
+    };
 
-	int m_selectedDatabase;
-	int m_selectedSourceFormat;
-	int m_selectedDestFormat;
-	bool m_criticalState;
+    int m_selectedDatabase;
+    int m_selectedSourceFormat;
+    int m_selectedDestFormat;
+    bool m_criticalState;
 
 
 private:
-	bool sqliteMoveAndConvert( const QString& name, const QString &src, const QString &destination );
+    bool sqliteMoveAndConvert( const QString& name, const QString &src, const QString &destination );
 };
 
 
