189dd9cc8668ff8a8b9f2a31426644b2a2ddca7f
Paul Eggleton <bluelightning@bluelightning.org>
2009-07-18 17:24:10 +0000

    multikey: fix code indenting

--- core/applets/multikeyapplet/multikey.cpp
+++ core/applets/multikeyapplet/multikey.cpp
@@ -28,7 +28,7 @@ Multikey::Multikey(QWidget *parent) : QLabel(parent), popupMenu(this), current("
 {
     QCopChannel* swChannel = new QCopChannel("MultiKey/Switcher", this);
     connect( swChannel, SIGNAL(received(const QCString&,const QByteArray&)),
-	     this, SLOT(message(const QCString&,const QByteArray&)));
+             this, SLOT(message(const QCString&,const QByteArray&)));
 
     QPEApplication::setStylusOperation(this, QPEApplication::RightOnHold);
     lang = 0;
@@ -41,21 +41,21 @@ Multikey::Multikey(QWidget *parent) : QLabel(parent), popupMenu(this), current("
 void Multikey::mousePressEvent(QMouseEvent *ev)
 {
     if (!sw_maps.count())
-	return;
+        return;
 
     if (ev->button() == RightButton) {
 
-	QPoint p = mapToGlobal(QPoint(0, 0));
-	QSize s = popupMenu.sizeHint();
-	int opt = popupMenu.exec(QPoint(p.x() + (width() / 2) - (s.width() / 2),
-					p.y() - s.height()), 0);
+        QPoint p = mapToGlobal(QPoint(0, 0));
+        QSize s = popupMenu.sizeHint();
+        int opt = popupMenu.exec(QPoint(p.x() + (width() / 2) - (s.width() / 2),
+                                        p.y() - s.height()), 0);
 
-	if (opt == -1)
-	    return;
-	lang = opt;
+        if (opt == -1)
+            return;
+        lang = opt;
 
-	QCopEnvelope e("MultiKey/Keyboard", "setmultikey(QString)");
-	e << sw_maps[lang];
+        QCopEnvelope e("MultiKey/Keyboard", "setmultikey(QString)");
+        e << sw_maps[lang];
         setText(labels[lang]);
     }
     QWidget::mousePressEvent(ev);
@@ -64,7 +64,7 @@ void Multikey::mousePressEvent(QMouseEvent *ev)
 void Multikey::mouseReleaseEvent(QMouseEvent *ev)
 {
     if (!sw_maps.count())
-	return;
+        return;
 
     lang = lang < sw_maps.count()-1 ? lang+1 : 0;
     QCopEnvelope e("MultiKey/Keyboard", "setmultikey(QString)");
@@ -77,60 +77,60 @@ void Multikey::message(const QCString &message, const QByteArray &data)
 {
     if ( message == "setsw(QString,QString)" ) {
 
-	QDataStream stream(data, IO_ReadOnly);
-	QString maps, current_map;
-	stream >> maps >> current_map;
-	QStringList sw = QStringList::split(QChar('|'), maps);
-	sw.append(current_map);
+        QDataStream stream(data, IO_ReadOnly);
+        QString maps, current_map;
+        stream >> maps >> current_map;
+        QStringList sw = QStringList::split(QChar('|'), maps);
+        sw.append(current_map);
 
-	QDir map_dir(QPEApplication::qpeDir() + "share/multikey/", "*.keymap");
-	lang = 0;
-	labels.clear();
-	sw_maps.clear();
-	popupMenu.clear();
+        QDir map_dir(QPEApplication::qpeDir() + "share/multikey/", "*.keymap");
+        lang = 0;
+        labels.clear();
+        sw_maps.clear();
+        popupMenu.clear();
 
-	for (uint i = 0; i < sw.count(); ++i) {
-	    QString keymap_map;
-	    if (sw[i][0] != '/') {
+        for (uint i = 0; i < sw.count(); ++i) {
+            QString keymap_map;
+            if (sw[i][0] != '/') {
 
-		keymap_map =  map_dir.absPath() + "/" + sw[i];
-	    } else {
+                keymap_map =  map_dir.absPath() + "/" + sw[i];
+            } else {
 
-		if ((map_dir.exists(QFileInfo(sw[i]).fileName(), false)
-		     && i != sw.count()-1) || !QFile::exists(sw[i])) {
-		    continue;
-		}
-		keymap_map = sw[i];
-	    }
+                if ((map_dir.exists(QFileInfo(sw[i]).fileName(), false)
+                     && i != sw.count()-1) || !QFile::exists(sw[i])) {
+                    continue;
+                }
+                keymap_map = sw[i];
+            }
 
-	    QFile map(keymap_map);
-	    if (map.open(IO_ReadOnly)) {
-		QString line;
+            QFile map(keymap_map);
+            if (map.open(IO_ReadOnly)) {
+                QString line;
 
-		map.readLine(line, 1024);
-		while (!map.atEnd()) {
+                map.readLine(line, 1024);
+                while (!map.atEnd()) {
 
-		    if (line.find(QRegExp("^sw\\s*=\\s*")) != -1) {
+                    if (line.find(QRegExp("^sw\\s*=\\s*")) != -1) {
 
-			if (i != sw.count()-1) {
-			    if (keymap_map == current_map) {
-				lang = i;
-			    }
-			    sw_maps.append(keymap_map);
-			    labels.append(line.right(line.length() - line.find(QChar('=')) - 1).stripWhiteSpace());
-			    popupMenu.insertItem(labels[labels.count()-1], labels.count()-1);
-			} else {
-			    current = line.right(line.length() - line.find(QChar('=')) - 1).stripWhiteSpace();
-			}
-			break;
-		    }
-		    map.readLine(line, 1024);
-		}
-		map.close();
-	    }
-	}
+                        if (i != sw.count()-1) {
+                            if (keymap_map == current_map) {
+                                lang = i;
+                            }
+                            sw_maps.append(keymap_map);
+                            labels.append(line.right(line.length() - line.find(QChar('=')) - 1).stripWhiteSpace());
+                            popupMenu.insertItem(labels[labels.count()-1], labels.count()-1);
+                        } else {
+                            current = line.right(line.length() - line.find(QChar('=')) - 1).stripWhiteSpace();
+                        }
+                        break;
+                    }
+                    map.readLine(line, 1024);
+                }
+                map.close();
+            }
+        }
 
-	setText(current);
+        setText(current);
     }
 }
 
