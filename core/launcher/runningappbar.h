ca130620740fd5800a2ba5dffbf5a34db1982ce7
Paul Eggleton <bluelightning@bluelightning.org>
2009-07-25 10:57:17 +0000

    Commit 1.1.1.1 (branch) version of runningappbar.h onto trunk so that it can be the default branch (and avoid problems during conversion to git)

--- core/launcher/runningappbar.h
+++ core/launcher/runningappbar.h
@@ -21,68 +21,52 @@
 #ifndef RUNNING_APP_BAR_H
 #define RUNNING_APP_BAR_H
 
+#include <qtopia/applnk.h>
+#include <qtopia/global.h>
+
 #include <qframe.h>
 #include <qlist.h>
 #include <qtimer.h>
+#include <qmap.h>
+#include <qguardedptr.h>
 
-class AppLnk;
 class AppLnkSet;
 class QCString;
 class QProcess;
 class QMessageBox;
+class TempScreenSaverMonitor;
 
 class RunningAppBar : public QFrame
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	RunningAppBar(QWidget* parent);
-	~RunningAppBar();
+    RunningAppBar(QWidget* parent);
+    ~RunningAppBar();
 
-	void addTask(const AppLnk& appLnk);
-	void removeTask(const AppLnk& appLnk);
-	void paintEvent(QPaintEvent* event);
-	void mousePressEvent(QMouseEvent*);
-	void mouseReleaseEvent(QMouseEvent*);
-	QSize sizeHint() const;
+    QSize sizeHint() const;
 
-private slots:
-	void newQcopChannel(const QString& channel);
-	void removedQcopChannel(const QString& channel);
-	void received(const QCString& msg, const QByteArray& data);
+protected:
+    void addTask(const AppLnk& appLnk);
+    void removeTask(const AppLnk& appLnk);
+    void paintEvent(QPaintEvent* event);
+    void mousePressEvent(QMouseEvent*);
+    void mouseReleaseEvent(QMouseEvent*);
 
-private:
-	AppLnkSet* m_AppLnkSet;
-	QList<AppLnk> m_AppList;
-	int m_SelectedAppIndex;
-	int spacing;
-};
-
-/**
- * Internal class that checks back in on the process when timerExpired is called
- * to make sure the process is on top.  If it's not it displays a dialog
- * box asking permission to kill it.
- */
-class AppMonitor : public QObject
-{
-	Q_OBJECT
-
-public:
-	static const int RAISE_TIMEOUT_MS;
-
-	AppMonitor(const AppLnk& app, RunningAppBar& owner);
-	~AppMonitor();
+public slots:
+    void applicationLaunched(const QString &);
+    void applicationTerminated(const QString &);
 
 private slots:
-	void timerExpired();
-	void received(const QCString& msg, const QByteArray& data);
+    void received(const QCString& msg, const QByteArray& data);
 
 private:
-	RunningAppBar& m_Owner;
-	const AppLnk& m_App;
-	QTimer m_Timer;
-	QMessageBox* m_AppKillerBox;
+    const AppLnkSet *appLnkSet;
+    QList<AppLnk> appList;
+    int selectedAppIndex;
+    int spacing;
 };
+
 
 #endif
 
