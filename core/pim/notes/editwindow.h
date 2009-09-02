9f94f2582ced11092b0df9978ab5772f9a18a7e9
Paul Eggleton <bluelightning@bluelightning.org>
2009-09-02 22:31:59 +0100

    opie-notes: add/update standard Opie source header comment

--- core/pim/notes/editwindow.h
+++ core/pim/notes/editwindow.h
@@ -1,3 +1,32 @@
+/*
+                             This file is part of the Opie Project
+
+              =.             Copyright (C) 2009 The Opie Team <opie-devel@handhelds.org>
+            .=l.             Portions copyright (C) 2002 Henning Holtschneider <hh@holtschneider.com>
+           .>+-=
+ _;:,     .>    :=|.         This program is free software; you can
+.> <`_,   >  .   <=          redistribute it and/or modify it under
+:`=1 )Y*s>-.--   :           the terms of the GNU General Public
+.="- .-=="i,     .._         License as published by the Free Software
+ - .   .-<_>     .<>         Foundation; either version 2 of the License,
+     ._= =}       :          or (at your option) any later version.
+    .%`+i>       _;_.
+    .i_,=:_.      -<s.       This program is distributed in the hope that
+     +  .  -:.       =       it will be useful,  but WITHOUT ANY WARRANTY;
+    : ..    .:,     . . .    without even the implied warranty of
+    =_        +     =;=|`    MERCHANTABILITY or FITNESS FOR A
+  _.=:.       :    :=>`:     PARTICULAR PURPOSE. See the GNU
+..}^=.=       =       ;      General Public License for more
+++=   -.     .`     .:       details.
+ :     =  ...= . :.=-
+ -.   .:....=;==+<;          You should have received a copy of the GNU
+  -_. . .   )=.  =           General Public License along with
+    --        :-=`           this file; see the file COPYING.
+                             If not, write to the Free Software Foundation,
+                             Inc., 59 Temple Place - Suite 330,
+                             Boston, MA 02111-1307, USA.
+*/
+
 #include <opie2/opimmemo.h>
 
 #include <qpe/categoryselect.h>
