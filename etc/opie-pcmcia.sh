99dfd4cbbb2b89d2ac97beb3d004263a410f2222
Paul Eggleton <bluelightning@bluelightning.org>
2013-08-26 18:24:45 +0100

    opie-cardapplet: fix udev scripts hanging udev startup
    The rules here were malformed, causing qcop to be called on every event.
    Additionally, prevent the script from doing anything if qpe isn't
    running.
    
    Signed-off-by: Paul Eggleton <bluelightning@bluelightning.org>

--- etc/opie-pcmcia.sh
+++ etc/opie-pcmcia.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Notify Opie cardapplet of pc card events
-export QTDIR=/usr/lib/opie
-export QPEDIR=/usr/lib/opie
-/usr/bin/qcop QPE/Card stabChanged\(\)
+if /bin/pidof qpe > /dev/null ; then
+    # Notify Opie cardapplet of pc card events
+    export QTDIR=/usr/lib/opie
+    export QPEDIR=/usr/lib/opie
+    /usr/bin/qcop QPE/Card stabChanged\(\)
+fi
